76ade03bacd2508aad2f46b278f10d9923db7c9e
João Cardoso <joao.fs.cardoso@gmail.com>
2010-03-30 03:36:04 +0000

    mkreloaded.sh now downloads ffp-reloaded-0.5-2.tgz

--- mkreloaded.sh
+++ mkreloaded.sh
@@ -18,11 +18,20 @@ ver=$(cut -f2 -d" " customroot/etc/Alt-F)
 
 cd reloaded
 
+if ! test -f alt-f/reloaded-2.6.12.6-arm1.ko; then
+	if ! test -f ffp-reloaded-0.5-2.tgz; then
+		wget http://www.inreto.de/dns323/ffp-reloaded/packages/ffp-reloaded-0.5-2.tgz
+	fi
+	tar --wildcards -xzf ffp-reloaded-0.5-2.tgz ./ffp/boot/reloaded-\*
+	mv ffp/boot/reloaded-* alt-f
+	rm -rf ffp
+fi
+
 cp $BLDDIR/binaries/dns323/zImage \
 	$BLDDIR/binaries/dns323/rootfs.arm.cpio-sq.lzma \
 	alt-f
 
 # don't compress, no significant space saving and much slower extraction
-rm alt-f/*~
+rm alt-f/*~ >& /dev/null
 tar --exclude-vcs -cvf Alt-F-$ver.tar alt-f
 mv Alt-F-$ver.tar $BLDDIR/binaries/dns323/
