9ead118f669b6c6db08db9e8916e20f0b88f1f82
João Cardoso <joao.fs.cardoso@gmail.com>
2010-03-30 17:38:59 +0000

    Alt-F-utils..tar.gz is now build from the source directory and copied to the download dir.

--- package/Alt-F-utils/Alt-F-utils.mk
+++ package/Alt-F-utils/Alt-F-utils.mk
@@ -15,6 +15,16 @@ ALT_F_UTILS_BINARY:=sysctrl
 ALT_F_UTILS_TARGET_BINARY:=usr/sbin/sysctrl
 ALT_F_UTILS_DEPENDENCIES:=uclibc
 
+$(DL_DIR)/$(ALT_F_UTILS_SOURCE): package/Alt-F-utils/Alt-F-utils-$(ALT_F_UTILS_VERSION)/sysctrl.c package/Alt-F-utils/Alt-F-utils-$(ALT_F_UTILS_VERSION)/dns323-fw.c
+	(cd package/Alt-F-utils/; \
+		tar --exclude-vcs -cvzf $(ALT_F_UTILS_SOURCE) Alt-F-utils-$(ALT_F_UTILS_VERSION); \
+		cp  $(ALT_F_UTILS_SOURCE) $(DL_DIR) \
+	)
+
+Alt-F-utils-source: $(DL_DIR)/$(ALT_F_UTILS_SOURCE)
+
+Alt-F-utils: Alt-F-utils-source
+
 $(eval $(call AUTOTARGETS,package,Alt-F-utils))
 
 #############################################################
