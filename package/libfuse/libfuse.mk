dd07432f8b898b6639eb7fc91eab17fd1a24c43b
João Cardoso <joao.fs.cardoso@gmail.com>
2010-04-19 21:20:08 +0000

    Don't know why, but "fusermount" is installed with crossbuild prefix, fixit

--- package/libfuse/libfuse.mk
+++ package/libfuse/libfuse.mk
@@ -18,7 +18,7 @@ $(eval $(call AUTOTARGETS,package,libfuse))
 $(LIBFUSE_TARGET_INSTALL_TARGET):
 	mkdir -p $(TARGET_DIR)/usr/lib
 	mkdir -p $(TARGET_DIR)/usr/bin
-	cp -dpf $(STAGING_DIR)/usr/bin/fusermount $(TARGET_DIR)/usr/bin/
+	cp -dpf $(STAGING_DIR)/usr/bin/arm-linux-fusermount $(TARGET_DIR)/usr/bin/fusermount
 	$(STRIPCMD) $(STRIP_STRIP_ALL) $(TARGET_DIR)/usr/bin/fusermount
 	cp -dpf $(STAGING_DIR)/usr/lib/libfuse.so* $(TARGET_DIR)/usr/lib/
 	$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/usr/lib/libfuse.so
