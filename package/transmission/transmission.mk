235ef1639752568b055dcdf73a273753c0d5be62
João Cardoso <joao.fs.cardoso@gmail.com>
2010-04-20 03:00:28 +0000

    update to version 1.92

--- package/transmission/transmission.mk
+++ package/transmission/transmission.mk
@@ -3,14 +3,14 @@
 # transmission
 #
 #############################################################
-TRANSMISSION_VERSION = 1.76
+TRANSMISSION_VERSION = 1.92
 TRANSMISSION_SOURCE = transmission-$(TRANSMISSION_VERSION).tar.bz2
 TRANSMISSION_SITE = http://download.m0k.org/transmission/files
 TRANSMISSION_AUTORECONF = NO
 TRANSMISSION_INSTALL_STAGING = NO
 TRANSMISSION_INSTALL_TARGET = YES
 TRANSMISSION_LIBTOOL_PATCH = NO
-TRANSMISSION_CONF_OPT =  --disable-nls --disable-gtk
+TRANSMISSION_CONF_OPT = --disable-nls --disable-gtk
 
 TRANSMISSION_DEPENDENCIES = uclibc libcurl openssl
 
