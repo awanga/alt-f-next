f31252f368769efa79b6e4d97a7d1726cdcf675f
João Cardoso <joao.fs.cardoso@gmail.com>
2010-05-03 17:46:44 +0000

    -initial version. Apple Filling Protocol and Appletalk

---
+++ package/netatalk/netatalk.mk
@@ -0,0 +1,37 @@
+#############################################################
+#
+# netatalk
+#
+#############################################################
+NETATALK_VERSION = 2.1
+NETATALK_SOURCE = netatalk-$(NETATALK_VERSION).tar.gz
+NETATALK_SITE = http://downloads.sourceforge.net FIXME
+NETATALK_AUTORECONF = NO
+NETATALK_INSTALL_STAGING = NO
+NETATALK_INSTALL_TARGET = YES
+NETATALK_LIBTOOL_PATCH = YES
+
+NETATALK_CONF_OPT = --with-bdb=$(STAGING_DIR)/usr \
+		--with-ssl-dir=$(STAGING_DIR)/usr \
+		--with-libiconv=$(STAGING_DIR)/usr \
+		--without-libgcrypt-dir \
+		--disable-cups \
+		--without-pam \
+		--without-gssapi \
+		--disable-tcp-wrappers \
+		--disable-static \
+		--program-prefix=""
+
+NETATALK_DEPENDENCIES = uclibc db
+
+$(eval $(call AUTOTARGETS,package,netatalk))
+
+$(NETATALK_HOOK_POST_CONFIGURE):
+	echo -e "#define rindex(a,b) strrchr((a),(b)) \n\
+	#define index(a,b) strchr((a),(b)) \n\
+	#define bcopy(a,b,c) memmove((b),(a),(c))" >> $(NETATALK_DIR)/config.h
+	touch $@
+
+$(NETATALK_HOOK_POST_INSTALL):
+	rm -rf $(TARGET_DIR)/usr/share/aclocal
+	touch $@
