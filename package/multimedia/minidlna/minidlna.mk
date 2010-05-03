f276ee236a303289f1517de8a791d4c97b076119
João Cardoso <joao.fs.cardoso@gmail.com>
2010-05-03 16:52:52 +0000

    -initial version for minidlna (for ushare usage)

---
+++ package/multimedia/minidlna/minidlna.mk
@@ -0,0 +1,21 @@
+#############################################################
+#
+# minidlna
+#
+#############################################################
+MINIDLNA_VERSION = 230410
+MINIDLNA_SOURCE = minidlna-cvs-$(MINIDLNA_VERSION).tgz
+MINIDLNA_SITE = http://minidlna.geexbox.org/releases/minidlna-$(MINIDLNA_VERSION).tar.bz2
+MINIDLNA_AUTORECONF = NO
+MINIDLNA_INSTALL_STAGING = NO
+MINIDLNA_INSTALL_TARGET = YES
+MINIDLNA_LIBTOOL_PATCH = NO
+	
+MINIDLNA_DEPENDENCIES = uclibc libexif jpeg libid3tag flac libvorbis sqlite ffmpeg
+
+$(eval $(call AUTOTARGETS,package/multimedia,minidlna))
+	
+$(MINIDLNA_HOOK_POST_EXTRACT):
+	touch $(MINIDLNA_DIR)/configure
+	chmod +x $(MINIDLNA_DIR)/configure
+	touch $@
