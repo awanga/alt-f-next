0fc6c4515161df05c177931f67322da1f55e5978
João Cardoso <joao.fs.cardoso@gmail.com>
2010-05-03 16:55:17 +0000

    -firefly initial version (not yet tested) -Damned kdesvn does't allow recursive commits?

---
+++ package/multimedia/mt-daapd/mt-daapd.mk
@@ -0,0 +1,29 @@
+#############################################################
+#
+# mt-daapd
+#
+#############################################################
+MT_DAAPD_VERSION = 0.2.4.2
+MT_DAAPD_SOURCE = mt-daapd-$(MT_DAAPD_VERSION).tar.gz
+MT_DAAPD_SITE = http://libexif.sourceforge.net/ FIXME
+MT_DAAPD_AUTORECONF = NO
+MT_DAAPD_INSTALL_STAGING = NO
+MT_DAAPD_INSTALL_TARGET = YES
+MT_DAAPD_LIBTOOL_PATCH = YES
+MT_DAAPD_CONF_ENV = CFLAGS+=-DSETPGRP_VOID
+
+MT_DAAPD_CONF_OPT = --disable-avahi --enable-oggvorbis
+	
+MT_DAAPD_DEPENDENCIES = uclibc gdbm libid3tag libvorbis
+
+$(eval $(call AUTOTARGETS,package/multimedia,mt-daapd))
+
+$(MT_DAAPD_HOOK_POST_EXTRACT):
+	sed -i 's/^AC_FUNC_SETPGRP/dnl AC_FUNC_SETPGRP/' $(MT_DAAPD_DIR)/configure.in
+	(cd $(MT_DAAPD_DIR); aclocal; autoconf; automake --add-missing)
+	touch $@
+
+#$(MT_DAAPD_HOOK_POST_INSTALL):
+#	cp $(MT_DAAPD_DIR)/contrib/mt-daapd.conf \
+#		$(MT_DAAPD_DIR)/contrib/mt-daapd.playlist $(TARGET_DIR)/etc
+#	touch $@
