a645beec2fe90775fe56cf81c3e745c7f2e47f6e
João Cardoso <joao.fs.cardoso@gmail.com>
2010-05-14 04:38:47 +0000

    -fix dependencies -help configure find headers and libraries

--- package/multimedia/fuppes/fuppes.mk
+++ package/multimedia/fuppes/fuppes.mk
@@ -9,17 +9,18 @@ FUPPES_SITE = http://downloads.sourceforge.net/project/fuppes/fuppes/SVN-$(FUPPE
 FUPPES_INSTALL_STAGING = NO
 FUPPES_INSTALL_TARGET = YES
 
-FUPPES__DEPENDENCIES = uclibc pcre libxml2 sqlite ffmpeg taglib flac libmad taglib
+FUPPES_DEPENDENCIES = uclibc host-pkgconfig pcre libxml2 sqlite ffmpeg taglib flac libvorbis libmad taglib lame
 
-FUPPES_CONF_ENV += MYSQL_CONFIG=no PKG_CONFIG_PATH=$(STAGING_DIR)/usr/lib/pkgconfig/ TAGLIB_CONFIG=$(STAGING_DIR)/usr/bin/taglib-config
+# taglib is using system header files - install host taglib-devel :-(
+# create twolame buildroot package?
+
+FUPPES_CONF_ENV += MYSQL_CONFIG=no \
+	TAGLIB_CONFIG=$(STAGING_DIR)/usr/bin/taglib-config \
+	MAD_CFLAGS=" " MAD_LIBS=-lmad
 
 FUPPES_LIBTOOL_PATCH = NO
 FUPPES_AUTORECONF = NO
 
-# searching for mad.pc config in libmad... patch/create one :-(
-# taglib is using system header files - install host taglib-devel :-(
-# create lame/twolame buildroot package
-
-FUPPES_CONF_OPT = --disable-mp4v2 --enable-transcoder-ffmpeg --enable-mad
+FUPPES_CONF_OPT = --disable-mp4v2 --enable-transcoder-ffmpeg --enable-mad --enable-lame
 
 $(eval $(call AUTOTARGETS,package/multimedia,fuppes))
