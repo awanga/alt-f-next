479cb3b138f03a2aa3f9e06ec0c737e78bd27d64
João Cardoso <joao.fs.cardoso@gmail.com>
2010-05-14 04:20:30 +0000

    -update to cups 1.4.3 -some more targets added to facilitate building debuging

--- package/cups/cups.mk
+++ package/cups/cups.mk
@@ -3,13 +3,16 @@
 # cups
 #
 ################################################################################
-CUPS_VERSION = 1.3.9
+#CUPS_VERSION = 1.3.9
+CUPS_VERSION = 1.4.3
 CUPS_NAME = cups-$(CUPS_VERSION)
 CUPS_DIR = $(BUILD_DIR)/$(CUPS_NAME)
 CUPS_SITE = http://ftp.easysw.com/pub/cups/$(CUPS_VERSION)
 CUPS_SOURCE:=$(CUPS_NAME)-source.tar.bz2
 CUPS_DESTDIR:=$(STAGING_DIR)/usr/lib
 CUPS_CAT:=$(BZCAT)
+CUPS_TARGET_BINARY:=usr/sbin/cupsd
+CUPS_DEPENDENCIES = libusb
 
 ifeq ($(BR2_PACKAGE_DBUS),y)
 	CUPS_CONF_OPT += --enable-dbus
@@ -25,7 +28,6 @@ endif
 CUPS_CONF_OPT +=	--disable-perl
 CUPS_CONF_OPT +=	--disable-java
 CUPS_CFLAGS = $(TARGET_CFLAGS)
-
 
 ifeq ($(BR2_PACKAGE_PERL),disabled)	# We do not provide perl (yet)
 	CUPS_CONF_ENV +=	ac_cv_path_perl=$(STAGING_DIR)/usr/bin/perl
@@ -73,6 +75,12 @@ $(CUPS_DIR)/.unpacked: $(DL_DIR)/$(CUPS_SOURCE)
 	$(CONFIG_UPDATE) $(CUPS_DIR)
 	touch $@
 
+CUPS_CONF_OPT = --without-perl --without-java --without-php	--without-python \
+	--disable-pam --disable-dnssd --disable-ldap --disable-gnutls --disable-gssapi \
+	--with-cups-user=cups  --with-cups-group=lpadmin \
+	--enable-openssl --enable-libusb \
+	--with-pdftops=none --with-languages=none --with-docdir=/usr/share/cups/doc
+
 $(CUPS_DIR)/.configured: $(CUPS_DIR)/.unpacked
 	cd $(CUPS_DIR) && $(AUTOCONF)
 	(cd $(CUPS_DIR) && \
@@ -88,29 +96,47 @@ $(CUPS_DIR)/.configured: $(CUPS_DIR)/.unpacked
 		--exec-prefix=/usr \
 		--sysconfdir=/etc \
 		--localstatedir=/var \
-		--with-config-file-path=/etc \
 		--disable-gnutls \
 		--disable-gssapi \
 		$(CUPS_CONF_OPT) \
 		)
 	touch $@
 
+# cups backend berkeley cgi-bin filter \
+#	locale monitor notifier pdftops scheduler systemv scripting/php \
+#	conf data doc fonts ppd templates
+
 $(CUPS_DIR)/.compiled: $(CUPS_DIR)/.configured
-	$(MAKE) CFLAGS="$(CUPS_CFLAGS)" -C $(CUPS_DIR) cups backend berkeley cgi-bin filter \
-	locale monitor notifier pdftops scheduler systemv scripting/php \
-	conf data doc fonts ppd templates
+	$(MAKE) CFLAGS="$(CUPS_CFLAGS)" -C $(CUPS_DIR)
 	touch $@
 
-$(CUPS_DIR)/.installed: $(CUPS_DIR)/.compiled
+$(TARGET_DIR)/$(CUPS_TARGET_BINARY): $(CUPS_DIR)/.compiled
+	-rm -rf $(STAGING_DIR)/usr/share/cups/mime $(TARGET_DIR)/usr/share/cups/mime
 	$(MAKE) -C $(CUPS_DIR) DESTDIR=$(STAGING_DIR) DSTROOT=$(STAGING_DIR) install
 	$(MAKE) -C $(CUPS_DIR) DESTDIR=$(TARGET_DIR) DSTROOT=$(TARGET_DIR) install
 	$(SED) "s,^prefix=.*,prefix=\'$(STAGING_DIR)/usr\',g" $(STAGING_DIR)/usr/bin/cups-config
 	$(SED) "s,^exec_prefix=.*,exec_prefix=\'$(STAGING_DIR)/usr\',g" $(STAGING_DIR)/usr/bin/cups-config
 	$(SED) "s,^includedir=.*,includedir=\'$(STAGING_DIR)/usr/include\',g" $(STAGING_DIR)/usr/bin/cups-config
 	$(SED) "s,^libdir=.*,libdir=\'$(STAGING_DIR)/usr/lib\',g" $(STAGING_DIR)/usr/bin/cups-config
+	(cd $(TARGET_DIR); \
+	rm -rf ./usr/share/applications \
+		./etc/dbus-1 ./etc/rc?.d ./etc/xinetd.d ./etc/cups/*.conf.* \
+		./etc/init.d/cups; \
+	cp -f ./usr/share/cups/charmaps/iso-8859-1.txt \
+		./usr/share/cups/charmaps/us-ascii.txt; \
+	ln -sf /usr/sbin/rcscript sbin/rccups; \
+	)
 	touch $@
 
-cups: uclibc host-autoconf $(CUPS_DEPENDENCIES) $(CUPS_DIR)/.installed
+cups: uclibc host-autoconf $(CUPS_DEPENDENCIES) $(TARGET_DIR)/$(CUPS_TARGET_BINARY)
+
+cups-install: $(TARGET_DIR)/$(CUPS_TARGET_BINARY)
+
+cups-build: $(CUPS_DIR)/.compiled
+
+cups-configure: $(CUPS_DIR)/.configured
+
+cups-extract: $(CUPS_DIR)/.unpacked
 
 cups-source: $(DL_DIR)/$(CUPS_SOURCE)
 
