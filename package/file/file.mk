b1c224d54005e8b4eb74481172d4c09a53f4b9ec
João Cardoso <joao.fs.cardoso@gmail.com>
2010-05-14 04:30:21 +0000

    -update to 5.0.4 and clean the host/target building (creating the compiled magic file needs a host build)

--- package/file/file.mk
+++ package/file/file.mk
@@ -7,63 +7,69 @@
 FILE_VERSION:=5.04
 FILE_SOURCE:=file-$(FILE_VERSION).tar.gz
 FILE_SITE:=ftp://ftp.astron.com/pub/file/
-FILE_SOURCE_DIR:=$(BUILD_DIR)/file-$(FILE_VERSION)
-FILE_DIR1:=$(TOOL_BUILD_DIR)/file-$(FILE_VERSION)-host
-FILE_DIR2:=$(BUILD_DIR)/file-$(FILE_VERSION)-target
+#FILE_SOURCE_DIR:=$(BUILD_DIR)/file-$(FILE_VERSION)
+FILE_DIR:=$(BUILD_DIR)/file-$(FILE_VERSION)
+FILE_HOST:=$(BUILD_DIR)/file-$(FILE_VERSION)-host
 FILE_CAT:=$(ZCAT)
 FILE_BINARY:=src/file
 FILE_TARGET_BINARY:=usr/bin/file
-
-$(DL_DIR)/$(FILE_SOURCE):
-	 $(call DOWNLOAD,$(FILE_SITE),$(FILE_SOURCE))
-
-file-source: $(DL_DIR)/$(FILE_SOURCE)
-
 
 #############################################################
 #
 # build file for use on the host system
 #
 #############################################################
-$(FILE_DIR1)/.configured: $(FILE_SOURCE_DIR)/.unpacked
-	mkdir -p $(FILE_DIR1)
-	(cd $(FILE_DIR1); rm -rf config.cache; \
+
+$(FILE_HOST)/.unpacked: $(DL_DIR)/$(FILE_SOURCE)
+	mkdir -p $(FILE_HOST)
+	$(FILE_CAT) $(DL_DIR)/$(FILE_SOURCE) | tar $(TAR_STRIP_COMPONENTS)=1 -C $(FILE_HOST) $(TAR_OPTIONS) -
+	toolchain/patch-kernel.sh $(FILE_HOST) package/file/ file\*.patch
+	$(CONFIG_UPDATE) $(FILE_HOST)
+	touch $@
+
+$(FILE_HOST)/.configured: $(FILE_HOST)/.unpacked
+	#$(MAKE) -C $(FILE_DIR) distclean
+	#(cd $(FILE_DIR) && rm config.guess config.sub)
+	mkdir -p $(FILE_HOST)
+	(cd $(FILE_HOST); rm -rf config.cache; \
 		CC="$(HOSTCC)" \
-		$(FILE_SOURCE_DIR)/configure \
-		--prefix=$(FILE_DIR1)/install \
+		$(FILE_HOST)/configure \
+		--prefix=$(STAGING_DIR) \
 	)
 	touch $@
 
-$(TOOL_BUILD_DIR)/bin/file: $(FILE_DIR1)/.configured
-	$(MAKE) -C $(FILE_DIR1) install
-	ln -sf $(FILE_DIR1)/install/bin/file $(TOOL_BUILD_DIR)/bin/file
+$(STAGING_DIR)/$(FILE_TARGET_BINARY): $(FILE_HOST)/.configured
+	$(MAKE) -C $(FILE_HOST) install
+	ln -sf $(STAGING_DIR)/bin/file $(TOOL_BUILD_DIR)/bin/file
 
-host-file: $(TOOL_BUILD_DIR)/bin/file
-
-host-file-clean:
-	-$(MAKE) -C $(FILE_DIR1) clean
-
-host-file-dirclean:
-	rm -rf $(FILE_DIR1)
+#host-file: $(TOOL_BUILD_DIR)/bin/file
+#
+#host-file-clean:
+#	-$(MAKE) -C $(FILE_DIR1) clean
+#
+#host-file-dirclean:
+#	rm -rf $(FILE_DIR1)
 
 #############################################################
 #
 # build file for use on the target system
 #
 #############################################################
-file-unpacked: $(FILE_SOURCE_DIR)/.unpacked
-$(FILE_SOURCE_DIR)/.unpacked: $(DL_DIR)/$(FILE_SOURCE)
+
+$(DL_DIR)/$(FILE_SOURCE):
+	 $(call DOWNLOAD,$(FILE_SITE),$(FILE_SOURCE))
+
+$(FILE_DIR)/.unpacked: $(DL_DIR)/$(FILE_SOURCE)
 	$(FILE_CAT) $(DL_DIR)/$(FILE_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	toolchain/patch-kernel.sh $(FILE_SOURCE_DIR) package/file/ file\*.patch
-	$(CONFIG_UPDATE) $(FILE_SOURCE_DIR)
+	toolchain/patch-kernel.sh $(FILE_DIR) package/file/ file\*.patch
+	$(CONFIG_UPDATE) $(FILE_DIR)
 	touch $@
 
-$(FILE_DIR2)/.configured: $(FILE_SOURCE_DIR)/.unpacked
-	mkdir -p $(FILE_DIR2)
-	(cd $(FILE_DIR2); rm -rf config.cache; \
+$(FILE_DIR)/.configured: $(FILE_DIR)/.unpacked
+	(cd $(FILE_DIR); rm -rf config.cache; \
 		$(TARGET_CONFIGURE_OPTS) \
 		$(TARGET_CONFIGURE_ARGS) \
-		$(FILE_SOURCE_DIR)/configure \
+		$(FILE_DIR)/configure \
 		--target=$(GNU_TARGET_NAME) \
 		--host=$(GNU_TARGET_NAME) \
 		--build=$(GNU_HOST_NAME) \
@@ -71,7 +77,7 @@ $(FILE_DIR2)/.configured: $(FILE_SOURCE_DIR)/.unpacked
 		--exec-prefix=/usr \
 		--bindir=/usr/bin \
 		--sbindir=/usr/sbin \
-		--libdir=/lib \
+		--libdir=/usr/lib \
 		--libexecdir=/usr/lib \
 		--sysconfdir=/etc \
 		--datadir=/usr/share/ \
@@ -80,40 +86,42 @@ $(FILE_DIR2)/.configured: $(FILE_SOURCE_DIR)/.unpacked
 		--infodir=/usr/share/info \
 		$(DISABLE_NLS) \
 		$(DISABLE_LARGEFILE) \
-		--enable-static \
+		--disable-static \
 		--disable-fsect-man5 \
 	)
 	touch $@
 
-$(FILE_DIR2)/$(FILE_BINARY): $(FILE_DIR2)/.configured $(TOOL_BUILD_DIR)/bin/file
-	ln -sf $(FILE_DIR1)/install/bin/file $(FILE_DIR2)/magic/file
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) LDFLAGS="-static" -C $(FILE_DIR2)
+$(FILE_DIR)/$(FILE_BINARY): $(FILE_DIR)/.configured
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(FILE_DIR)
 
-$(TARGET_DIR)/$(FILE_TARGET_BINARY): $(FILE_DIR2)/$(FILE_BINARY)
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) DESTDIR=$(TARGET_DIR) -C $(FILE_DIR2) install
-	-($(STRIPCMD) $(TARGET_DIR)/usr/lib/libmagic.so.*.* > /dev/null 2>&1)
-ifneq ($(BR2_HAVE_INFOPAGES),y)
-	rm -rf $(TARGET_DIR)/usr/share/info
-endif
-ifneq ($(BR2_HAVE_MANPAGES),y)
-	rm -rf $(TARGET_DIR)/usr/share/man
-endif
-	rm -rf $(TARGET_DIR)/share/locale
-	rm -rf $(TARGET_DIR)/usr/share/doc
-	mv $(TARGET_DIR)/lib/libmagic.a $(STAGING_DIR)/lib
-	rm -f $(TARGET_DIR)/lib/libmagic.la
-	mv $(TARGET_DIR)/usr/include/magic.h $(STAGING_DIR)/usr/include
+$(TARGET_DIR)/$(FILE_TARGET_BINARY): $(FILE_DIR)/$(FILE_BINARY)
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) DESTDIR=$(TARGET_DIR) -C $(FILE_DIR) install
 
-file: zlib uclibc $(TARGET_DIR)/$(FILE_TARGET_BINARY)
 
-file-configure: $(FILE_DIR2)/.configured
+file-host: $(STAGING_DIR)/$(FILE_TARGET_BINARY)
+
+file-target: $(TARGET_DIR)/$(FILE_TARGET_BINARY)
+
+file: zlib uclibc file-host file-target
+
+#$(STAGING_DIR)/$(FILE_TARGET_BINARY) 
+
+file-source: $(DL_DIR)/$(FILE_SOURCE)
+
+file-unpacked: $(FILE_DIR)/.unpacked
+
+file-configure: $(FILE_DIR)/.configured
+
+file-build: $(FILE_DIR)/$(FILE_BINARY)
+
+file-install: $(TARGET_DIR)/$(FILE_TARGET_BINARY)
 
 file-clean:
-	-$(MAKE) DESTDIR=$(TARGET_DIR) CC=$(TARGET_CC) -C $(FILE_DIR2) uninstall
-	-$(MAKE) -C $(FILE_DIR2) clean
+	-$(MAKE) DESTDIR=$(TARGET_DIR) CC=$(TARGET_CC) -C $(FILE_DIR) uninstall
+	-$(MAKE) -C $(FILE_DIR) clean
 
 file-dirclean:
-	rm -rf $(FILE_DIR2)
+	rm -rf $(FILE_DIR)
 
 #############################################################
 #
