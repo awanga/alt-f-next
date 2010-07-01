40c0203ba72b0fa048bb4cf4d440d5e2d12d6c7d
João Cardoso <joao.fs.cardoso@gmail.com>
2010-07-01 16:00:47 +0000

    -hplip initial support

---
+++ package/hplip/hplip.mk
@@ -0,0 +1,22 @@
+#############################################################
+#
+# hplip
+#
+#############################################################
+
+HPLIP_VERSION = 3.10.5
+HPLIP_SOURCE = hplip-$(HPLIP_VERSION).tar.gz
+HPLIP_SITE = http://fixme
+HPLIP_AUTORECONF = NO
+HPLIP_LIBTOOL_PATCH = YES
+HPLIP_INSTALL_STAGING = NO
+HPLIP_INSTALL_TARGET = YES
+HPLIP_DEPENDENCIES = uclibc gs jpeg tiff libpng cups libusb netsnmp sane
+
+HPLIP_CONF_OPT = --disable-qt3 --disable-qt4 --disable-gui-build --disable-dbus-build --disable-doc-build --enable-lite-build --disable-fax-build
+
+$(eval $(call AUTOTARGETS,package,hplip))
+
+$(HPLIP_HOOK_POST_INSTALL):
+	rm -rf $(TARGET_DIR)/etc/udev $(TARGET_DIR)/var/lib/hp
+	touch @
