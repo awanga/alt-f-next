6911fa1ae1d2eaf7e5aa93231256f7765a49110f
João Cardoso <joao.fs.cardoso@gmail.com>
2010-09-19 19:58:39 +0000

    -support sftp through dropbear. -ugly and quick hack! I only want sftp-server for use with dropbear, so remove everything else

--- package/openssh/openssh.mk
+++ package/openssh/openssh.mk
@@ -16,6 +16,18 @@ OPENSSH_DEPENDENCIES = zlib openssl
 
 $(eval $(call AUTOTARGETS,package,openssh))
 
+#$(OPENSSH_HOOK_POST_INSTALL):
+#	$(INSTALL) -D -m 755 package/openssh/S50sshd $(TARGET_DIR)/etc/init.d/S50sshd
+#	touch $@
+
+# ugly and quick hack, FIXME!
+# I only want sftp-server for use with dropbear, so remove eveything else
 $(OPENSSH_HOOK_POST_INSTALL):
-	$(INSTALL) -D -m 755 package/openssh/S50sshd $(TARGET_DIR)/etc/init.d/S50sshd
+	mkdir -p $(TARGET_DIR)/usr/libexec 
+	cp $(TARGET_DIR)/usr/lib/sftp-server $(TARGET_DIR)/usr/libexec
+	rmdir $(TARGET_DIR)/var/empty
+	for i in etc/moduli etc/ssh_config etc/sshd_config usr/bin/sftp \
+usr/bin/slogin usr/bin/ssh-add usr/bin/ssh-agent usr/bin/ssh-keygen \
+usr/bin/ssh-keyscan usr/lib/sftp-server usr/lib/ssh-keysign \
+usr/sbin/sshd usr/share/Ssh.bin var/empty; do rm -f $(TARGET_DIR)/$$i; done
 	touch $@
