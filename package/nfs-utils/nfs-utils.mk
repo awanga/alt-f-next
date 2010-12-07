753d05ba2a63c23b88d9b784ad279358c2abf0e8
João Cardoso <joao.fs.cardoso@gmail.com>
2010-12-07 15:31:37 +0000

    -revert to 1.2.1, as 1.2.2 and 1.2.3 has some problems. -add and fix patches

--- package/nfs-utils/nfs-utils.mk
+++ package/nfs-utils/nfs-utils.mk
@@ -4,7 +4,9 @@
 #
 #############################################################
 #NFS_UTILS_VERSION:=1.2.0
-NFS_UTILS_VERSION:=1.2.3
+NFS_UTILS_VERSION:=1.2.1
+# NFS_UTILS_VERSION:=1.2.2 uses libcap (capabilities.h), not in uclibc
+# NFS_UTILS_VERSION:=1.2.3 rpc.mountd core dumps with showmount and +1 mount
 NFS_UTILS_SOURCE:=nfs-utils-$(NFS_UTILS_VERSION).tar.bz2
 NFS_UTILS_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/nfs/
 NFS_UTILS_CAT:=$(BZCAT)
