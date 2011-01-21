b54412f4302beac69402d2ba7922fdfdf7f798bc
João Cardoso <joao.fs.cardoso@gmail.com>
2011-01-21 20:34:17 +0000

    -update to version 2.8.5

--- package/libfuse/libfuse.mk
+++ package/libfuse/libfuse.mk
@@ -3,7 +3,8 @@
 # libfuse
 #
 #############################################################
-LIBFUSE_VERSION:=2.8.1
+#LIBFUSE_VERSION:=2.8.1
+LIBFUSE_VERSION:=2.8.5
 LIBFUSE_SOURCE:=fuse-$(LIBFUSE_VERSION).tar.gz
 LIBFUSE_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/fuse/
 LIBFUSE_DIR:=$(BUILD_DIR)/fuse-$(LIBFUSE_VERSION)
