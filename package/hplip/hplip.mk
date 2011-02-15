1d02bd6b85884c400961d51d200423be8d87f0d6
João Cardoso <joao.fs.cardoso@gmail.com>
2011-02-15 15:31:44 +0000

    -bump to version 3.11.1

--- package/hplip/hplip.mk
+++ package/hplip/hplip.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-HPLIP_VERSION = 3.10.5
+HPLIP_VERSION = 3.11.1
 HPLIP_SOURCE = hplip-$(HPLIP_VERSION).tar.gz
 HPLIP_SITE = http://prdownloads.sourceforge.net/hplip/
 HPLIP_AUTORECONF = NO
@@ -13,7 +13,10 @@ HPLIP_INSTALL_STAGING = NO
 HPLIP_INSTALL_TARGET = YES
 HPLIP_DEPENDENCIES = uclibc gs jpeg tiff libpng cups libusb netsnmp sane
 
-HPLIP_CONF_OPT = --disable-qt3 --disable-qt4 --disable-gui-build --disable-dbus-build --disable-doc-build --enable-lite-build --disable-fax-build
+HPLIP_CONF_OPT = --disable-doc-build --disable-qt3 --disable-qt4 --enable-lite-build \
+--disable-fax-build --disable-dbus-build \
+--enable-network-build --enable-scan-build \
+--enable-cups-ppd-install --enable-hpcups-install
 
 $(eval $(call AUTOTARGETS,package,hplip))
 
