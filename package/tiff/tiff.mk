9024aef59397365fa417a063d290d9ee4a3b7151
João Cardoso <joao.fs.cardoso@gmail.com>
2011-02-15 15:46:47 +0000

    -bump to version 3.9.4

--- package/tiff/tiff.mk
+++ package/tiff/tiff.mk
@@ -3,25 +3,19 @@
 # tiff
 #
 #############################################################
-TIFF_VERSION:=3.8.2
-#TIFF_SITE:=ftp://ftp.remotesensing.org/pub/libtiff
-TIFF_SITE:=http://dl.maptools.org/dl/libtiff
+
+TIFF_VERSION:=3.9.4
+TIFF_SITE:=ftp://ftp.remotesensing.org/pub/libtiff
 TIFF_SOURCE:=tiff-$(TIFF_VERSION).tar.gz
 TIFF_LIBTOOL_PATCH = NO
 TIFF_INSTALL_STAGING = YES
 TIFF_INSTALL_TARGET = YES
 TIFF_CONF_OPT = \
 	--enable-shared \
-	--enable-static \
-	--disable-cxx \
+	--disable-static \
 	--without-x \
+	--program-prefix=""
 
 TIFF_DEPENDENCIES = uclibc host-pkgconfig zlib jpeg
 
 $(eval $(call AUTOTARGETS,package,tiff))
-
-$(TIFF_TARGET_INSTALL_TARGET):
-	-cp -a $(TIFF_DIR)/libtiff/.libs/libtiff.so* $(TARGET_DIR)/usr/lib/
-	$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/usr/lib/libtiff.so
-	touch $@
-
