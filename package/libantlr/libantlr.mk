398e1ef545faf85075fb319f1a3ce43aa1ee6263
João Cardoso <joao.fs.cardoso@gmail.com>
2011-02-15 15:35:22 +0000

    -libantlr initial version (needed by forked-daapd)

---
+++ package/libantlr/libantlr.mk
@@ -0,0 +1,17 @@
+#############################################################
+#
+# libantlr
+#
+#############################################################
+
+LIBANTLR_VERSION:=3.2
+LIBANTLR_SITE = http://www.antlr.org/download/C/
+LIBANTLR_SOURCE = libantlr3c-$(LIBANTLR_VERSION).tar.gz
+LIBANTLR_DIR = $(BUILD_DIR)/antlr-$(LIBANTLR_VERSION)
+LIBANTLR_LIBTOOL_PATCH = NO
+LIBANTLR_INSTALL_STAGING = YES
+LIBANTLR_INSTALL_TARGET = YES
+
+LIBANTLRL_CONF_OPT = --disable-static --enable-shared
+
+$(eval $(call AUTOTARGETS,package,libantlr))
