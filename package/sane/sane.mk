f72fe219fd2424e92b62edae68cc0364b5097443
João Cardoso <joao.fs.cardoso@gmail.com>
2011-03-11 05:04:17 +0000

    -enable IPv6 (is segfaulting?)

--- package/sane/sane.mk
+++ package/sane/sane.mk
@@ -13,6 +13,6 @@ SANE_INSTALL_STAGING = YES
 SANE_INSTALL_TARGET = YES
 SANE_DEPENDENCIES = uclibc libusb jpeg tiff
 
-SANE_CONF_OPT = --disable-latex --disable-translations --disable-ipv6 --disable-avahi --disable-rpath --with-pic --without-gphoto2
+SANE_CONF_OPT = --disable-latex --disable-translations $(DISABLE_IPV6) --disable-avahi --disable-rpath --with-pic --without-gphoto2
 
 $(eval $(call AUTOTARGETS,package,sane))
