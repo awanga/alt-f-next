54c8f80d9e9a4a54f5f67c6d65297103a740c79f
João Cardoso <joao.fs.cardoso@gmail.com>
2011-04-10 20:21:20 +0000

    -bump to version 5.8p1

--- package/openssh/openssh.mk
+++ package/openssh/openssh.mk
@@ -3,11 +3,12 @@
 # openssh
 #
 #############################################################
-OPENSSH_VERSION=5.1p1
+
+OPENSSH_VERSION=5.8p1
 OPENSSH_SITE=ftp://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
 OPENSSH_CONF_ENV = LD=$(TARGET_CC)
-OPENSSH_CONF_OPT = --libexecdir=/usr/lib --disable-lastlog --disable-utmp \
-		--disable-utmpx --disable-wtmp --disable-wtmpx --without-x
+OPENSSH_CONF_OPT = --libexecdir=/usr/lib --sysconfdir=/etc/ssh \
+	-disable-lastlog --disable-utmp --disable-utmpx --disable-wtmp --disable-wtmpx --without-x
 OPENSSH_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 OPENSSH_DEPENDENCIES = zlib openssl
 
