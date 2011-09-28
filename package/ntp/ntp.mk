b2d9411e0c76650f8db4b6bbf987be6bc6b137a0
João Cardoso <joao.fs.cardoso@gmail.com>
2011-09-28 03:25:42 +0000

    -ntp: bump to 4.2.6p3 -enable ssl -remove BSD index etc defines as uclibc now has them

--- package/ntp/ntp.mk
+++ package/ntp/ntp.mk
@@ -3,7 +3,8 @@
 # ntp
 #
 #############################################################
-NTP_VERSION:=4.2.4p5
+#NTP_VERSION:=4.2.4p5
+NTP_VERSION:=4.2.6p3
 NTP_SOURCE:=ntp-$(NTP_VERSION).tar.gz
 NTP_SITE:=http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/
 NTP_DIR:=$(BUILD_DIR)/ntp-$(NTP_VERSION)
@@ -25,11 +26,14 @@ ntp-source: $(DL_DIR)/$(NTP_SOURCE)
 $(NTP_DIR)/.patched: $(DL_DIR)/$(NTP_SOURCE)
 	$(NTP_CAT) $(DL_DIR)/$(NTP_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
 	toolchain/patch-kernel.sh $(NTP_DIR) package/ntp/ ntp\*.patch
-	$(SED) "s,^#if.*__GLIBC__.*_BSD_SOURCE.*$$,#if 0," \
-		$(NTP_DIR)/ntpd/refclock_pcf.c
-	$(SED) '/[[:space:](]index[[:space:]]*(/s/[[:space:]]*index[[:space:]]*(/ strchr(/g' $(NTP_DIR)/libisc/*.c $(NTP_DIR)/arlib/sample.c
-	$(SED) '/[[:space:](]rindex[[:space:]]*(/s/[[:space:]]*rindex[[:space:]]*(/ strrchr(/g' $(NTP_DIR)/ntpd/*.c
-	#$(SED) 's/\(^#[[:space:]]*include[[:space:]]*<sys\/var.h>\)/\/\/ \1/' $(NTP_DIR)/util/tickadj.c
+	#$(SED) "s,^#if.*__GLIBC__.*_BSD_SOURCE.*$$,#if 0," \
+	#	$(NTP_DIR)/ntpd/refclock_pcf.c
+	#$(SED) '/[[:space:](]index[[:space:]]*(/s/[[:space:]]*index[[:space:]]*(/ strchr(/g' \
+	#	$(NTP_DIR)/libisc/*.c $(NTP_DIR)/arlib/sample.c
+	#$(SED) '/[[:space:](]rindex[[:space:]]*(/s/[[:space:]]*rindex[[:space:]]*(/ strrchr(/g' \
+	#	$(NTP_DIR)/ntpd/*.c
+	#$(SED) 's/\(^#[[:space:]]*include[[:space:]]*<sys\/var.h>\)/\/\/ \1/' \
+	#	$(NTP_DIR)/util/tickadj.c
 	$(CONFIG_UPDATE) $(NTP_DIR)
 	$(CONFIG_UPDATE) $(NTP_DIR)/sntp
 	touch $@
@@ -37,6 +41,7 @@ $(NTP_DIR)/.patched: $(DL_DIR)/$(NTP_SOURCE)
 $(NTP_DIR)/.configured: $(NTP_DIR)/.patched
 	(cd $(NTP_DIR); rm -rf config.cache; \
 		$(TARGET_CONFIGURE_OPTS) \
+		$(TARGET_CONFIGURE_ENV) \
 		$(TARGET_CONFIGURE_ARGS) \
 		ac_cv_lib_md5_MD5Init=no \
 		./configure \
@@ -58,10 +63,13 @@ $(NTP_DIR)/.configured: $(NTP_DIR)/.patched
 		$(NTP_CONF_OPT) \
 		--with-shared \
 		--program-transform-name=s,,, \
-		--without-crypto \
 		--disable-tickadj \
+		--with-openssl-incdir=$(STAGING_DIR)/usr/include \
+		--with-openssl-libdir=$(STAGING_DIR)/usr/lib \
 	)
 	touch $@
+
+#		--with-crypto=openssl \
 
 $(NTP_DIR)/$(NTP_BINARY): $(NTP_DIR)/.configured
 	$(MAKE) -C $(NTP_DIR)
@@ -77,7 +85,11 @@ endif
 		install -m 644 package/ntp/ntpd.etc.default $(TARGET_DIR)/etc/default/ntpd ; \
 	fi
 
-ntp: uclibc $(TARGET_DIR)/$(NTP_TARGET_BINARY)
+ntp-configure: $(NTP_DIR)/.configured
+
+ntp-build: $(NTP_DIR)/$(NTP_BINARY)
+
+ntp: uclibc openssl $(TARGET_DIR)/$(NTP_TARGET_BINARY)
 
 ntp-clean:
 	rm -f $(TARGET_DIR)/usr/sbin/ntpd $(TARGET_DIR)/usr/bin/sntp \
