ef66c84050848adc13016e8672cd2132faf78791
João Cardoso <joao.fs.cardoso@gmail.com>
2011-09-28 01:01:35 +0000

    new package parted

---
+++ package/parted/parted.mk
@@ -0,0 +1,19 @@
+#############################################################
+#
+# parted
+#
+#############################################################
+PARTED_VERSION:=2.2
+PARTED_SOURCE:=parted-$(PARTED_VERSION).tar.gz
+PARTED_SITE:=$(GNU_MIRROR)/parted
+PARTED_DIR:=$(BUILD_DIR)/parted-$(PARTED_VERSION)
+PARTED_INSTALL_STAGING = NO
+PARTED_LIBTOOL_PATCH = NO
+
+PARTED_DEPENDENCIES = uclibc libuuid
+
+PARTED_CONF_OPT = --disable-device-mapper --disable-dynamic-loading --without-readline --disable-shared
+
+#PARTED_CONF_ENV = CFLAGS+=-Os
+
+$(eval $(call AUTOTARGETS,package,parted))
