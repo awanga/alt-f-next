7df5e5427a11485b9976dbcb2a941284dc6d0bd3
João Cardoso <joao.fs.cardoso@gmail.com>
2011-09-28 00:59:02 +0000

    new package nmap

---
+++ package/nmap/nmap.mk
@@ -0,0 +1,24 @@
+#############################################################
+#
+# nmap
+#
+#############################################################
+NMAP_VERSION:=5.51
+NMAP_SOURCE:=nmap-$(NMAP_VERSION).tar.bz2
+NMAP_SITE:=http://nmap.org/dist
+NMAP_DIR:=$(BUILD_DIR)/nmap-$(NMAP_VERSION)
+NMAP_INSTALL_STAGING = NO
+NMAP_LIBTOOL_PATCH = NO
+
+NMAP_CONF_OPT = --with-libpcap=included --with-pcap=linux --without-liblua --without-zenmap
+NMAP_CONF_ENV = ac_cv_linux_vers=$(BR2_DEFAULT_KERNEL_HEADERS)
+
+$(eval $(call AUTOTARGETS,package,nmap))
+
+$(NMAP_HOOK_POST_CONFIGURE):
+	sed -i 's/^install:/install-strip:/p' $(NMAP_DIR)/Makefile
+	touch $@
+
+$(NMAP_HOOK_POST_INSTALL):
+	rm $(TARGET_DIR)/usr/bin/ndiff
+	touch $@
