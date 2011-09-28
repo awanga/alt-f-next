f8c35fa47c307aca932010be5e1c9f5692cfbfb6
João Cardoso <joao.fs.cardoso@gmail.com>
2011-09-28 02:43:48 +0000

    lzo: bump to 2.06

--- package/lzo/lzo.mk
+++ package/lzo/lzo.mk
@@ -3,14 +3,16 @@
 # lzo
 #
 #############################################################
-LZO_VERSION:=2.03
+LZO_VERSION:=2.06
 LZO_SOURCE:=lzo-$(LZO_VERSION).tar.gz
 LZO_SITE:=http://www.oberhumer.com/opensource/lzo/download
 LZO_AUTORECONF = NO
 LZO_INSTALL_STAGING = YES
 LZO_INSTALL_TARGET = YES
+LZO_LIBTOOL_PATCH = NO
 LZO_INSTALL_STAGING_OPT = CC="$(TARGET_CC)" DESTDIR=$(STAGING_DIR) install
-LZO_CONF_ENV =
+LZO_CONF_ENV = 
+LZO_CONF_OPT = --disable-static --enable-shared
 LZO_DEPENDENCIES = uclibc
 
 $(eval $(call AUTOTARGETS,package,lzo))
