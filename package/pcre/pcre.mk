8c48d4e07b377ef72c7f846b6a5005c79c45f700
João Cardoso <joao.fs.cardoso@gmail.com>
2011-09-28 03:29:07 +0000

    -libpcre: bump to 8.13 -enable utf8

--- package/pcre/pcre.mk
+++ package/pcre/pcre.mk
@@ -3,15 +3,18 @@
 # PCRE
 #
 #############################################################
-PCRE_VERSION = 7.7
+#PCRE_VERSION = 7.7
+PCRE_VERSION = 8.13
 PCRE_SOURCE = pcre-$(PCRE_VERSION).tar.bz2
 PCRE_SITE = ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre
 PCRE_INSTALL_STAGING = YES
 PCRE_INSTALL_TARGET = YES
+PCRE_LIBTOOL_PATCH = NO
+PCRE_CONF_OPT = --enable-utf8
 
 ifneq ($(BR2_INSTALL_LIBSTDCPP),y)
 # pcre will use the host g++ if a cross version isn't available
-PCRE_CONF_OPT = --disable-cpp
+PCRE_CONF_OPT += --disable-cpp
 endif
 
 PCRE_DEPENDENCIES = uclibc
