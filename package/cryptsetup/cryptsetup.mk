7abd00511823d09a961ffae69c8d4659ba6e08c2
João Cardoso <joao.fs.cardoso@gmail.com>
2011-09-28 00:52:15 +0000

    new package cryptsetup

---
+++ package/cryptsetup/cryptsetup.mk
@@ -0,0 +1,17 @@
+#############################################################
+#
+# automatic
+#
+#############################################################
+
+CRYPTSETUP_VERSION = 1.3.1
+CRYPTSETUP_SOURCE = cryptsetup-$(CRYPTSETUP_VERSION).tar.bz2
+CRYPTSETUP_SITE = http://cryptsetup.googlecode.com/files
+CRYPTSETUP_AUTORECONF = NO
+CRYPTSETUP_INSTALL_STAGING = NO
+CRYPTSETUP_INSTALL_TARGET = YES
+CRYPTSETUP_LIBTOOL_PATCH = NO
+
+CRYPTSETUP_DEPENDENCIES = lvm2 libuuid libgcrypt popt libiconv
+
+$(eval $(call AUTOTARGETS,package,cryptsetup))
