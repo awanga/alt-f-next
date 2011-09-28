bb2d943502b77299a1359616e4c67830b71aee93
João Cardoso <joao.fs.cardoso@gmail.com>
2011-09-28 01:00:30 +0000

    new package ntfs-3g_ntfsprogs ( ntfs-3g and ntfsprogs projects merged, package ntfs3g deleted)

---
+++ package/ntfs-3g_ntfsprogs/ntfs-3g_ntfsprogs.mk
@@ -0,0 +1,32 @@
+#############################################################
+#
+# ntfs-3g_ntfsprogs
+#
+#############################################################
+
+NTFS_3G_NTFSPROGS_VERSION:=2011.4.12
+NTFS_3G_NTFSPROGS_SOURCE:=ntfs-3g_ntfsprogs-$(NTFS_3G_NTFSPROGS_VERSION).tgz
+NTFS_3G_NTFSPROGS_SITE:=http://tuxera.com/opensource
+NTFS_3G_NTFSPROGS_INSTALL_STAGING:=yes
+
+NTFS_3G_NTFSPROGS_CONF_OPT = --disable-ldconfig --disable-mtab --program-prefix=""
+
+ifeq ($(BR2_PACKAGE_NTFS_3G_NTFSPROGS_NTFSPROGS),)
+NTFS_3G_NTFSPROGS_CONF_OPT += --disable-ntfsprogs
+endif
+
+$(eval $(call AUTOTARGETS,package,ntfs-3g_ntfsprogs))
+
+ifeq ($(BR2_PACKAGE_NTFS_3G_NTFSPROGS_NTFSPROGS),)
+$(NTFS_3G_NTFSPROGS_TARGET_INSTALL_TARGET): $(NTFS_3G_NTFSPROGS_TARGET_INSTALL_STAGING)
+	cp -dpf $(STAGING_DIR)/usr/lib/libntfs-3g.so* $(TARGET_DIR)/usr/lib/
+	$(INSTALL) -m 0755 $(STAGING_DIR)/usr/bin/ntfs-3g $(TARGET_DIR)/usr/bin/
+	touch $@
+endif
+
+$(NTFS_3G_NTFSPROGS_TARGET_UNINSTALL):
+	$(call MESSAGE,"Uninstalling")
+	$(MAKE) DESTDIR=$(STAGING_DIR) -C $(NTFS_3G_NTFSPROGS_DIR) uninstall
+	rm -f $(TARGET_DIR)/usr/lib/libntfs-3g*
+	rm -f $(TARGET_DIR)/usr/bin/ntfs-3g $(TARGET_DIR)/usr/bin/ntfs-3g.probe
+	rm -f $(NTFS_3G_NTFSPROGS_TARGET_INSTALL_STAGING) $(NTFS_3G_NTFSPROGS_TARGET_INSTALL_TARGET) $(NTFS_3G_NTFSPROGS_HOOK_POST_INSTALL)
