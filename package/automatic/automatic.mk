07b41d51502a8be7874ab4c8a1381f2e79fa4933
João Cardoso <joao.fs.cardoso@gmail.com>
2011-09-28 00:46:40 +0000

    -automatic: patch added (for badly build server-side scripts) -versions 0.6.5 and 0.6.6 tried and not worked, keep using 0.6.4 -added dependencies on libiconv and openssl

--- package/automatic/automatic.mk
+++ package/automatic/automatic.mk
@@ -4,6 +4,7 @@
 #
 #############################################################
 
+# version 6.5 and 6.6 don't work. sick!
 AUTOMATIC_VERSION = 0.6.4
 AUTOMATIC_SOURCE = automatic-$(AUTOMATIC_VERSION)-src.tar.gz
 AUTOMATIC_SITE = http://kylek.is-a-geek.org:31337/files/
@@ -12,10 +13,6 @@ AUTOMATIC_INSTALL_STAGING = NO
 AUTOMATIC_INSTALL_TARGET = YES
 AUTOMATIC_LIBTOOL_PATCH = NO
 
-AUTOMATIC_DEPENDENCIES = host-pkgconfig uclibc libxml2 pcre libcurl
+AUTOMATIC_DEPENDENCIES = host-pkgconfig uclibc libxml2 pcre libcurl libiconv openssl
 
 $(eval $(call AUTOTARGETS,package,automatic))
-
-$(AUTOMATIC_HOOK_POST_INSTALL):
-	mv $(TARGET_DIR)/etc/automatic.conf-sample $(TARGET_DIR)/etc/automatic.conf
-	touch $@
