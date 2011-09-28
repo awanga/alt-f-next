77f49101ffa0e4b4ea0986706c61d3fd6cccc845
João Cardoso <joao.fs.cardoso@gmail.com>
2011-09-28 03:28:12 +0000

    -openvpn: bump to 2.2.1 -add certificates and sample scripts and config files

--- package/openvpn/openvpn.mk
+++ package/openvpn/openvpn.mk
@@ -4,7 +4,8 @@
 #
 #############################################################
 
-OPENVPN_VERSION = 2.0.9
+#OPENVPN_VERSION = 2.0.9
+OPENVPN_VERSION = 2.2.1
 OPENVPN_SOURCE = openvpn-$(OPENVPN_VERSION).tar.gz
 OPENVPN_SITE = http://openvpn.net/release
 OPENVPN_DEPENDENCIES = lzo openssl uclibc
@@ -23,14 +24,19 @@ $(OPENVPN_TARGET_INSTALL_TARGET):
 	$(call MESSAGE,"Installing")
 	$(INSTALL) -m 755 $(OPENVPN_DIR)/openvpn \
 		$(TARGET_DIR)/usr/sbin/openvpn
-	if [ ! -f $(TARGET_DIR)/etc/init.d/openvpn ]; then \
-		$(INSTALL) -m 755 -D package/openvpn/openvpn.init \
-			$(TARGET_DIR)/etc/init.d/openvpn; \
-	fi
+	#if [ ! -f $(TARGET_DIR)/etc/init.d/openvpn ]; then \
+	#	$(INSTALL) -m 755 -D package/openvpn/openvpn.init \
+	#		$(TARGET_DIR)/etc/init.d/openvpn; \
+	#fi
+	$(INSTALL) -m 755 -D package/openvpn/openvpn.init \
+		$(TARGET_DIR)/usr/share/openvpn/openvpn.init
+	mkdir -p $(TARGET_DIR)/etc/openvpn
+	mkdir -p $(TARGET_DIR)/usr/share/openvpn
+	(cd $(OPENVPN_DIR); cp -a easy-rsa sample-scripts sample-config-files $(TARGET_DIR)/usr/share/openvpn)
 	touch $@
 
-$(OPENVPN_TARGET_UNINSTALL):
-	$(call MESSAGE,"Uninstalling")
-	rm -f $(TARGET_DIR)/usr/sbin/openvpn
-	rm -f $(TARGET_DIR)/etc/init.d/openvpn
-	rm -f $(OPENVPN_TARGET_INSTALL_TARGET) $(OPENVPN_HOOK_POST_INSTALL)
+#$(OPENVPN_TARGET_UNINSTALL):
+#	$(call MESSAGE,"Uninstalling")
+#	rm -f $(TARGET_DIR)/usr/sbin/openvpn
+#	rm -f $(TARGET_DIR)/etc/init.d/openvpn
+#	rm -f $(OPENVPN_TARGET_INSTALL_TARGET) $(OPENVPN_HOOK_POST_INSTALL)
