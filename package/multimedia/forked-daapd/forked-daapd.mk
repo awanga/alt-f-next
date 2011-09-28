4dc507216aee963c68566e73df0f20431e1ce5b5
João Cardoso <joao.fs.cardoso@gmail.com>
2011-09-28 02:49:47 +0000

    -forked-daapd: bump to  0.19 -delete patch for release 13 -create patch for release 19

--- package/multimedia/forked-daapd/forked-daapd.mk
+++ package/multimedia/forked-daapd/forked-daapd.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-FORKED_DAAPD_VERSION = 0.13
+FORKED_DAAPD_VERSION = 0.19
 FORKED_DAAPD_SOURCE = forked-daapd-$(FORKED_DAAPD_VERSION).tar.gz
 FORKED_DAAPD_SITE = http://alioth.debian.org/~jblache/forked-daapd/
 FORKED_DAAPD_AUTORECONF = NO
@@ -16,6 +16,6 @@ FORKED_DAAPD_DEPENDENCIES = uclibc zlib libavl libunistring libevent libgcrypt l
 
 FORKED_DAAPD_CONF_ENV = ZLIB_LIBS="-lz" ZLIB_CFLAGS=" " LIBS="-lstdc++"
 FORKED_DAAPD_CONF_OPT = -localstatedir=/var --sysconfdir=/etc \
-	--enable-flac --enable-musepack
+	--enable-flac --enable-musepack --disable-rpath
 
 $(eval $(call AUTOTARGETS,package/multimedia,forked-daapd))
