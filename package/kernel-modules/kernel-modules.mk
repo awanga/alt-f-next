98b57cc7bb7b33c0302c538c1db904a0fc358c5a
João Cardoso <joao.fs.cardoso@gmail.com>
2011-09-29 02:07:32 +0000

    -kernel-modules: new package (a do-nothing makefile is needed, only KERNEL_MODULES_VERSION needs to be defined so 'mkpkg.sh' can verify the package version)

---
+++ package/kernel-modules/kernel-modules.mk
@@ -0,0 +1,18 @@
+#############################################################
+#
+# kernel-modules
+#
+#############################################################
+KERNEL_MODULES_VERSION:=0.1
+
+# this is a dummy target, it is here just to have BR2_PACKAGE_KERNEL_MODULES defined
+kernel-modules: uclibc
+
+#############################################################
+#
+# Toplevel Makefile options
+#
+#############################################################
+ifeq ($(BR2_PACKAGE_KERNEL_MODULES),y)
+TARGETS+=kernel-modules
+endif
