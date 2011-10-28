eb0ebe9d4464cac357b5e648d34dbbff1cb1428f
João Cardoso <joao.fs.cardoso@gmail.com>
2011-10-28 02:57:48 +0000

    -transmission: bump to 2.42 -fix mis-behaved configure

--- package/transmission/transmission.mk
+++ package/transmission/transmission.mk
@@ -4,18 +4,18 @@
 #
 #############################################################
 
-TRANSMISSION_VERSION = 2.33
+TRANSMISSION_VERSION = 2.42
 TRANSMISSION_SOURCE = transmission-$(TRANSMISSION_VERSION).tar.bz2
 TRANSMISSION_SITE = http://download.m0k.org/transmission/files
 TRANSMISSION_AUTORECONF = NO
 TRANSMISSION_INSTALL_STAGING = NO
 TRANSMISSION_INSTALL_TARGET = YES
 TRANSMISSION_LIBTOOL_PATCH = NO
-TRANSMISSION_CONF_OPT = --disable-nls --disable-gtk --disable-gconf2
+TRANSMISSION_CONF_OPT = --disable-nls --disable-gtk --disable-gconf2 --enable-utp
 
 TRANSMISSION_DEPENDENCIES = uclibc libcurl openssl libevent2 pkg-config
 
 TRANSMISSION_CONF_ENV = LIBEVENT_CFLAGS="-I$(STAGING_DIR)/libevent2/include" \
-	LIBEVENT_LIBS="-L$(STAGING_DIR)/libevent2/lib -levent2"
+	LIBEVENT_LIBS="-L$(STAGING_DIR)/libevent2/lib -levent2" ac_cv_prog_HAVE_CXX=yes
 
 $(eval $(call AUTOTARGETS,package,transmission))
