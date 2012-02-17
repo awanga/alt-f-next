6f2ece65c37dd8d282b93b6cdf9e766e14a1ebae
João Cardoso <joao.fs.cardoso@gmail.com>
2012-02-17 00:41:02 +0000

    -kexec.mk: bump to 2.0.3 -don't install kdump nor vmcore-dmesg

--- package/kexec/kexec.mk
+++ package/kexec/kexec.mk
@@ -3,7 +3,8 @@
 # kexec
 #
 #############################################################
-KEXEC_VERSION = 2.0.2
+#KEXEC_VERSION = 2.0.2
+KEXEC_VERSION = 2.0.3
 KEXEC_SOURCE = kexec-tools-$(KEXEC_VERSION).tar.bz2
 KEXEC_SITE = http://kernel.org/pub/linux/utils/kernel/kexec/
 # no install-strip/install-exec
@@ -26,4 +27,6 @@ ifneq ($(BR2_ENABLE_DEBUG),y)
 	$(STRIPCMD) $(STRIP_STRIP_ALL) $(TARGET_DIR)/usr/sbin/kdump
 endif
 	rm -rf $(TARGET_DIR)/usr/lib/kexec-tools
+	rm -f $(TARGET_DIR)/usr/sbin/kdump
+	rm -f $(TARGET_DIR)/usr/sbin/vmcore-dmesg
 	touch $@
