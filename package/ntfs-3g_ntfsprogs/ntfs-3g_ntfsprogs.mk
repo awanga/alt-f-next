18b73613b3161b27615691c59f65d683705df284
João Cardoso <joao.fs.cardoso@gmail.com>
2012-02-17 00:43:09 +0000

    -ntfs-3g_ntfsprogs.mk: bump to 2012.1.15 -set --libdir to /usr/lib otherwise /usr/lib64 will be used (32 bits target not detected)

--- package/ntfs-3g_ntfsprogs/ntfs-3g_ntfsprogs.mk
+++ package/ntfs-3g_ntfsprogs/ntfs-3g_ntfsprogs.mk
@@ -4,12 +4,12 @@
 #
 #############################################################
 
-NTFS_3G_NTFSPROGS_VERSION:=2011.4.12
+#NTFS_3G_NTFSPROGS_VERSION:=2011.4.12
+NTFS_3G_NTFSPROGS_VERSION:=2012.1.15
 NTFS_3G_NTFSPROGS_SOURCE:=ntfs-3g_ntfsprogs-$(NTFS_3G_NTFSPROGS_VERSION).tgz
 NTFS_3G_NTFSPROGS_SITE:=http://tuxera.com/opensource
 NTFS_3G_NTFSPROGS_INSTALL_STAGING:=yes
-
-NTFS_3G_NTFSPROGS_CONF_OPT = --disable-ldconfig --disable-mtab --program-prefix=""
+NTFS_3G_NTFSPROGS_CONF_OPT = --libdir=/usr/lib --disable-ldconfig --disable-mtab --program-prefix=""
 
 ifeq ($(BR2_PACKAGE_NTFS_3G_NTFSPROGS_NTFSPROGS),)
 NTFS_3G_NTFSPROGS_CONF_OPT += --disable-ntfsprogs
