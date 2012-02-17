2d7a21d7c1c49b30aaca4feef698242be20dbafc
João Cardoso <joao.fs.cardoso@gmail.com>
2012-02-17 00:25:19 +0000

    openssh.mk: use /usr/lib instead of /usr/libexec because the Filesystem Hierarchy Standard does not include any provision for libexecdir

--- package/openssh/openssh.mk
+++ package/openssh/openssh.mk
@@ -4,11 +4,11 @@
 #
 #############################################################
 
-#OPENSSH_VERSION=5.8p1
 OPENSSH_VERSION=5.9p1
 OPENSSH_SITE=ftp://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
 OPENSSH_CONF_ENV = LD=$(TARGET_CC)
-OPENSSH_CONF_OPT = --libexecdir=/usr/lib --sysconfdir=/etc/ssh \
+#OPENSSH_CONF_OPT = --libexecdir=/usr/lib --sysconfdir=/etc/ssh 
+OPENSSH_CONF_OPT = --sysconfdir=/etc/ssh \
 	-disable-lastlog --disable-utmp --disable-utmpx --disable-wtmp --disable-wtmpx --without-x
 OPENSSH_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
@@ -22,7 +22,8 @@ $(eval $(call AUTOTARGETS,package,openssh))
 ifneq ($(BR2_PACKAGE_OPENSSH_SFTP_ONLY),y)
 
 $(OPENSSH_HOOK_POST_INSTALL):
-	$(INSTALL) -D -m 755 package/openssh/S50sshd $(TARGET_DIR)/etc/init.d/S50sshd
+	#$(INSTALL) -D -m 755 package/openssh/S50sshd $(TARGET_DIR)/etc/init.d/S50sshd
+	sed 's/#.*Ciphers /Ciphers aes128-cbc,/' $(TARGET_DIR)/etc/ssh/ssh_config
 	touch $@
 
 else
@@ -32,8 +33,8 @@ else
 # and touchs the autotools dir as if a target install had been run.
 # dont touch the dependency, otherwise a full install occurs.
 $(OPENSSH_HOOK_POST_BUILD):
-	mkdir -p $(TARGET_DIR)/usr/libexec
-	$(INSTALL) -m 0755 $(OPENSSH_DIR)/sftp-server $(TARGET_DIR)/usr/libexec
+	#mkdir -p $(TARGET_DIR)/usr/libexec
+	$(INSTALL) -m 0755 $(OPENSSH_DIR)/sftp-server $(TARGET_DIR)/usr/lib
 	touch $(PROJECT_BUILD_DIR)/autotools-stamps/openssh_target_installed
 #	touch $@ # dont! and don't remove me!
 
