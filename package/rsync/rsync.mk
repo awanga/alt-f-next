faa897406b6d310587711ca07b7c1a7ccd15405c
João Cardoso <joao.fs.cardoso@gmail.com>
2012-02-17 00:44:45 +0000

    rsync.mk: bump to 3.0.9

--- package/rsync/rsync.mk
+++ package/rsync/rsync.mk
@@ -4,7 +4,8 @@
 #
 #############################################################
 
-RSYNC_VERSION:=3.0.8
+#RSYNC_VERSION:=3.0.8
+RSYNC_VERSION:=3.0.9
 RSYNC_SOURCE:=rsync-$(RSYNC_VERSION).tar.gz
 RSYNC_SITE:=http://rsync.samba.org/ftp/rsync/src
 RSYNC_AUTORECONF:=no
