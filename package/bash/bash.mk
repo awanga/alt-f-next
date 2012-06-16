a88fba8feeb1b51757552c76156f69b62e384a05
João Cardoso <joao.fs.cardoso@gmail.com>
2012-06-16 16:24:14 +0000

    bash: can't make 'install-strip' on systems where the system 'strip' program is not able to strip alien binaries -specify the strip program to use -say 'configure' to use the package bundled 'install.sh'

--- package/bash/bash.mk
+++ package/bash/bash.mk
@@ -12,6 +12,12 @@ BASH_DIR:=$(BUILD_DIR)/bash-$(BASH_VERSION)
 BASH_BINARY:=bash
 BASH_TARGET_BINARY:=bin/bash
 
+ifeq ($(BR2_ENABLE_DEBUG),y)
+BASH_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
+else
+BASH_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install-strip STRIPPROG="$(STRIPCMD)"
+endif
+
 BASH_CONF_ENV = ac_cv_func_setvbuf_reversed=no \
 		ac_cv_have_decl_sys_siglist=yes \
 		bash_cv_job_control_missing=present \
@@ -22,7 +28,8 @@ BASH_CONF_ENV = ac_cv_func_setvbuf_reversed=no \
 		bash_cv_ulimit_maxfds=yes \
 		bash_cv_getcwd_malloc=yes \
 		bash_cv_func_sigsetjmp=present \
-		bash_cv_printf_a_format=yes
+		bash_cv_printf_a_format=yes \
+		ac_cv_path_install=./support/install.sh
 
 BASH_CONF_OPT = $(DISABLE_NLS) \
 		$(DISABLE_LARGEFILE) \
