cc61bf128a4ab19a80da261c3b6704c87f494eb8
João Cardoso <joao.fs.cardoso@gmail.com>
2012-06-16 16:30:45 +0000

    libpar2: new package

---
+++ package/libpar2/libpar2.mk
@@ -0,0 +1,20 @@
+#############################################################
+#
+# libpar2
+#
+#############################################################
+
+LIBPAR2_VERSION = 0.2
+LIBPAR2_SOURCE = libpar2-$(LIBPAR2_VERSION).tar.gz
+LIBPAR2_SITE = http://sourceforge.net/projects/parchive/files/libpar2/$(LIBPAR2_VERSION)
+LIBPAR2_AUTORECONF = NO
+LIBPAR2_INSTALL_STAGING = YES
+LIBPAR2_INSTALL_TARGET = YES
+LIBPAR2_LIBTOOL_PATCH = NO
+
+LIBPAR2_DEPENDENCIES = uclibc libsigcpp
+
+$(eval $(call AUTOTARGETS,package,libpar2))
+
+$(LIBPAR2_HOOK_POST_INSTALL):
+	rm -rf $(TARGET_DIR)/usr/lib/libpar2
