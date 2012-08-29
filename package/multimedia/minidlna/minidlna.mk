8c74fa1f8ed84d7f2e6b7be3e3e99ca3dc61740f
João Cardoso <joao.fs.cardoso@gmail.com>
2012-08-29 23:55:40 +0000

    minidlna: bump to 1.0.25

--- package/multimedia/minidlna/minidlna.mk
+++ package/multimedia/minidlna/minidlna.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-MINIDLNA_VERSION = 1.0.24
+MINIDLNA_VERSION = 1.0.25
 MINIDLNA_SOURCE = minidlna_$(MINIDLNA_VERSION)_src.tar.gz
 MINIDLNA_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/minidlna
 #MINIDLNA_SITE = http://downloads.sourceforge.net/project/minidlna/minidlna/$(MINIDLNA_VERSION)
