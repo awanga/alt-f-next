57c47f244c98e6e69d2ae2aa67dba971f4ac50ca
João Cardoso <joao.fs.cardoso@gmail.com>
2012-09-14 17:06:56 +0000

    hplip: bump to 3.12.4 (using libusb-0.1)

--- package/hplip/hplip.mk
+++ package/hplip/hplip.mk
@@ -4,12 +4,12 @@
 #
 #############################################################
 
-HPLIP_VERSION = 3.11.7
-#HPLIP_VERSION = 3.12.4
+#HPLIP_VERSION = 3.11.7
+HPLIP_VERSION = 3.12.9
 HPLIP_SOURCE = hplip-$(HPLIP_VERSION).tar.gz
 HPLIP_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/project/hplip/hplip/$(HPLIP_VERSION)
 HPLIP_AUTORECONF = NO
-HPLIP_LIBTOOL_PATCH = YES
+HPLIP_LIBTOOL_PATCH = NO
 HPLIP_INSTALL_STAGING = NO
 HPLIP_INSTALL_TARGET = YES
 HPLIP_DEPENDENCIES = uclibc gs jpeg tiff libpng cups libusb netsnmp sane
@@ -17,7 +17,8 @@ HPLIP_DEPENDENCIES = uclibc gs jpeg tiff libpng cups libusb netsnmp sane
 HPLIP_CONF_OPT = --disable-doc-build --disable-qt3 --disable-qt4 --enable-lite-build \
 --disable-fax-build --disable-dbus-build \
 --enable-network-build --enable-scan-build \
---enable-cups-ppd-install --enable-hpcups-install
+--enable-cups-ppd-install --enable-hpcups-install \
+--enable-libusb01_build
 
 $(eval $(call AUTOTARGETS,package,hplip))
 
