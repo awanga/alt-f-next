e910d9e52a451a20584f8b0ac1b99069f74bd1b3
João Cardoso <joao.fs.cardoso@gmail.com>
2012-09-20 14:07:52 +0000

    minidlna: set DISABLE_NLS env var to configure and make, as libintl (gettext package) is now built and minidlna complains (is libintl too old for minidlna?). This issue migh happen for other packges as well.

--- package/multimedia/minidlna/minidlna.mk
+++ package/multimedia/minidlna/minidlna.mk
@@ -7,13 +7,14 @@
 MINIDLNA_VERSION = 1.0.25
 MINIDLNA_SOURCE = minidlna_$(MINIDLNA_VERSION)_src.tar.gz
 MINIDLNA_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/minidlna
-#MINIDLNA_SITE = http://downloads.sourceforge.net/project/minidlna/minidlna/$(MINIDLNA_VERSION)
 MINIDLNA_AUTORECONF = NO
 MINIDLNA_INSTALL_STAGING = NO
 MINIDLNA_INSTALL_TARGET = YES
 MINIDLNA_LIBTOOL_PATCH = NO
 	
 MINIDLNA_DEPENDENCIES = uclibc libexif jpeg libid3tag flac libvorbis sqlite ffmpeg
+
+MINIDLNA_CONF_ENV = DISABLE_NLS="$(DISABLE_NLS)"
 
 $(eval $(call AUTOTARGETS,package/multimedia,minidlna))
 	
