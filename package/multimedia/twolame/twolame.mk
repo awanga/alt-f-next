5929541eeea6877091603e04d0d21843dc51dff8
João Cardoso <joao.fs.cardoso@gmail.com>
2012-12-08 02:57:11 +0000

    twolame: new package

---
+++ package/multimedia/twolame/twolame.mk
@@ -0,0 +1,17 @@
+#############################################################
+#
+# twolame
+#
+#############################################################
+
+TWOLAME_VERSION = 0.3.13
+TWOLAME_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/project/twolame/twolame/$(TWOLAME_VERSION)
+TWOLAME_SOURCE = twolame-$(TWOLAME_VERSION).tar.gz
+
+TWOLAME_AUTORECONF = NO
+TWOLAME_INSTALL_STAGING = YES
+TWOLAME_INSTALL_TARGET = YES
+TWOLAME_LIBTOOL_PATCH = NO
+
+$(eval $(call AUTOTARGETS,package,twolame))
+
