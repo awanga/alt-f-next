bc3a347ca6f80a96fd3577d154a02ffd480c3f9b
João Cardoso <joao.fs.cardoso@gmail.com>
2012-12-16 04:45:40 +0000

    rpcbind: depends on tirpc; patch for missing err.h; patch link for missing nss.h

--- package/rpcbind/rpcbind.mk
+++ package/rpcbind/rpcbind.mk
@@ -4,6 +4,10 @@
 #
 #############################################################
 
+# not compiling, depends on, at least, libtirpc
+# also, see this patch:
+# http://lists.uclibc.org/pipermail/uclibc/2010-February/043569.html
+
 RPCBIND_VERSION = 0.2.0
 RPCBIND_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/project/rpcbind/rpcbind/$(RPCBIND_VERSION)
 RPCBIND_SOURCE = rpcbind-$(RPCBIND_VERSION).tar.bz2
@@ -15,3 +19,15 @@ RPCBIND_LIBTOOL_PATCH = NO
 
 $(eval $(call AUTOTARGETS,package,rpcbind))
 
+$(RPCBIND_HOOK_POST_CONFIGURE):
+	(echo '#include <errno.h>'; \
+	echo '#define err(exitcode, format, args...) \
+		errx(exitcode, format ": %s", ## args, strerror(errno))'; \
+	echo '#define errx(exitcode, format, args...) \
+		{ warnx(format, ## args); exit(exitcode); }'; \
+	echo '#define warn(format, args...) \
+		warnx(format ": %s", ## args, strerror(errno))'; \
+	echo '#define warnx(format, args...) \
+		fprintf(stderr, format "\n", ## args)'; \
+	) > $(RPCBIND_DIR)/src/err.h
+	touch $@
