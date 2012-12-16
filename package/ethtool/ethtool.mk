1b9b2d2b534fca0e16dffb35db122a29b89763f5
João Cardoso <joao.fs.cardoso@gmail.com>
2012-12-16 02:12:43 +0000

    ethtool: bump to 3.7

--- package/ethtool/ethtool.mk
+++ package/ethtool/ethtool.mk
@@ -4,7 +4,8 @@
 #
 #############################################################
 
-ETHTOOL_VERSION:=2.6.36
+#ETHTOOL_VERSION:=2.6.36
+ETHTOOL_VERSION:=3.7
 ETHTOOL_SOURCE:=ethtool-$(ETHTOOL_VERSION).tar.gz
 ETHTOOL_SITE:=http://www.kernel.org/pub/software/network/ethtool
 ETHTOOL_AUTORECONF:=no
