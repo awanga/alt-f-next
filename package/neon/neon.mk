eb5e73ee26e353ffacc0ba53e069a7f30906a218
João Cardoso <joao.fs.cardoso@gmail.com>
2012-12-19 04:49:13 +0000

    neon: fix staging-dir neon-config for correct davfs2 configure usage

--- package/neon/neon.mk
+++ package/neon/neon.mk
@@ -52,5 +52,7 @@ ifeq ($(BR2_ENABLE_DEBUG),)
 $(NEON_HOOK_POST_INSTALL): $(NEON_TARGET_INSTALL_TARGET)
 	$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/usr/lib/libneon.so
 	rm -f $(TARGET_DIR)/usr/bin/neon-config
+	sed -i 's|includedir=.*|includedir=$(STAGING_DIR)/usr/include|' \
+		$(STAGING_DIR)/usr/bin/neon-config
 	touch $@
 endif
