fc1e84495d0587bc08eab30782abecf9c17eb3ee
João Cardoso <joao.fs.cardoso@gmail.com>
2013-04-02 20:03:36 +0000

    mc: remove --with-ncurses-includes and --with-ncurses-libs from configure options,  as that makes it to fail. ncurses.h is found without those options

--- package/mc/mc.mk
+++ package/mc/mc.mk
@@ -16,8 +16,6 @@ MC_LIBTOOL_PATCH = NO
 MC_DEPENDENCIES = libglib2
 
 MC_CONF_ENV = fu_cv_sys_stat_statfs2_bsize=yes
-MC_CONF_OPT = --disable-doxygen-doc --with-screen=ncurses \
-	--with-ncurses-includes=$(STAGING)/usr \
-	--with-ncurses-libs=$(STAGING)/usr
+MC_CONF_OPT = --disable-doxygen-doc --with-screen=ncurses
 
 $(eval $(call AUTOTARGETS,package,mc))
