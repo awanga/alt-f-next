0a3df8bcc242483f0c615821cde3da4d9f3ac8a0
João Cardoso <joao.fs.cardoso@gmail.com>
2013-05-10 19:34:45 +0000

    news: handle news announcements from sourceforge

--- customroot/usr/bin/news.sh
+++ customroot/usr/bin/news.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# script to execute once a week,
-# updates news.log with new files since NEWS_CHK
+# script to execute once/twice a week,
+# updates news.log with new files/announcements since NEWS_CHK
 # update NEWS_CHK in misc.conf
 #
 # Status checks for news.log, and if its date is more recent than NEWS_ACK show the warning.
@@ -9,16 +9,9 @@
 
 MISCC=/etc/misc.conf
 LOGF=/var/log/news.log
-NEWS_URL="http://sourceforge.net/api/file/index/project-id/796573/crtime/asc/rss"
 
-if test -s $MISCC; then
-	. $MISCC
-fi
-
-if test -z "$NEWS_CHK"; then
-	first=yes
-	NEWS_CHK=$(date +%s)
-fi
+FILES_URL=http://sourceforge.net/api/file/index/project-id/796573/crtime/asc/rss
+NEWS_URL=http://sourceforge.net/p/alt-f/news/feed
 
 # RSS file format:
 # <item>
@@ -35,15 +28,22 @@ fi
 #   </media:content>
 # </item>
 
-last_rel=$(wget -q -O - $NEWS_URL | tee /root/rss | awk -v last_chk="$NEWS_CHK" -v logf="$LOGF" '
+# $1-rss url, $2-most recent item date
+parse_rss() {
+	wget -q -O - "$1" | \
+	sed -e 's/<item>/\n<item>/' -e 's/<title>/\n<title>/' -e 's/<pubDate>/\n<pubDate>/' | \
+	awk -v last_chk="$2" -v logf="$LOGF" '
 	BEGIN {
 		w["Jan"]="01"; w["Feb"]="02"; w["Mar"]="03"; w["Apr"]="04"; w["May"]="05"; w["Jun"]="06";
 		w["Jul"]="07"; w["Aug"]="08"; w["Sep"]="09"; w["Oct"]=10; w["Nov"]=11; w["Dec"]=12;
-		#printf("current:%s\n", strftime("%Y-%b-%e %H:%M:%S", systime()))
+#printf("current:%s %s\n", strftime("%Y-%b-%d %H:%M:%S", systime()), systime())  > "/dev/stderr"
 	}
 	/<item>/,/<\/item>/{
-		if (match($0,"<title>"))
-			item = gensub("<title><!\\[CDATA\\[(.*)\\]\\]></title>","\\1","g");
+		if (match($0,"<title>")) {
+			item = gensub(".*<title>(.*)</title>.*","\\1","g");
+			if (match(item,"CDATA"))
+				item = gensub("<!\\[CDATA\\[(.*)\\]\\]>","\\1","g",item);
+		}
 		if (match($0,"filesize=\"\"")) { # skip itens with no size (directories)
 			while (! match($0,"</item>"))
 				getline
@@ -57,18 +57,45 @@ last_rel=$(wget -q -O - $NEWS_URL | tee /root/rss | awk -v last_chk="$NEWS_CHK" 
 			ts = mktime(sprintf("%s %s %s %s %s %s", a[4], w[a[3]], a[2], h[1], h[2], h[3]));
 		}
 		if (match($0,"</item>")) {
-			if (ts > last_chk)
-				printf("%s %s\n",dt, item) >> logf
-			if (ts > last_rel)
+#printf("%s %s %s\n", dt, ts, item) > "/dev/stderr"
+			if (ts > last_chk) { # log everything published since the last check
+#printf("NEW: %s %s\n", dt, item)
+				printf("%s %s\n", dt, item) >> logf
+			}
+			if (ts > last_rel) # items can be out of order, so record the latest date
 				last_rel = ts;
 		}
 	}
 	END {
 		print last_rel
-	}
-')
+	}'
+}
 
-if test -n "$first" -o "$last_rel" -gt "$NEWS_CHK"; then
+if test -s $MISCC; then
+	. $MISCC
+fi
+
+if test -z "$NEWS_CHK" -o -z "$FILES_CHK"; then
+	echo NEWS_CHK=$(date +%s) >> $MISCC
+	echo FILES_CHK=$(date +%s) >> $MISCC
+	exit 0
+fi
+
+last_files=$(parse_rss $FILES_URL $FILES_CHK)
+#last_files=$(parse_rss /root/rss $FILES_CHK)
+#echo last_files=$last_files
+
+# update last check date with date of most recent item
+if test "$last_files" -gt "$FILES_CHK"; then
+	sed -i '/^FILES_CHK=/d' $MISCC
+	echo FILES_CHK=$last_files >> $MISCC
+fi
+
+last_news=$(parse_rss $NEWS_URL $NEWS_CHK)
+#last_news=$(parse_rss /root/feed $NEWS_CHK)
+#echo last_news=$last_news
+
+if test "$last_news" -gt "$NEWS_CHK"; then
 	sed -i '/^NEWS_CHK=/d' $MISCC
-	echo NEWS_CHK=$last_rel >> $MISCC
+	echo NEWS_CHK=$last_news >> $MISCC
 fi
