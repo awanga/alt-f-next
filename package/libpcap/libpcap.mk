4fd0bc6f0fb9d5b6988cfd4353b8e8aa34069610
João Cardoso <joao.fs.cardoso@gmail.com>
2013-05-24 13:47:51 +0000

    libpcap: new package

--- package/libpcap/libpcap.mk
+++ package/libpcap/libpcap.mk
@@ -3,14 +3,16 @@
 # libpcap
 #
 #############################################################
-LIBPCAP_VERSION:=1.1.1
+#LIBPCAP_VERSION:=1.1.1
+LIBPCAP_VERSION:=1.4.0
 LIBPCAP_SOURCE:=libpcap-$(LIBPCAP_VERSION).tar.gz
 LIBPCAP_SITE:=http://www.tcpdump.org/release
 LIBPCAP_DIR:=$(BUILD_DIR)/libpcap-$(LIBPCAP_VERSION)
-LIBPCAP_INSTALL_STAGING = NO
+LIBPCAP_INSTALL_STAGING = YES
 
 LIBPCAP_CONF_OPT = --disable-yydebug --with-pcap=linux
 LIBPCAP_CONF_ENV = ac_cv_linux_vers=$(BR2_DEFAULT_KERNEL_HEADERS)
+LIBPCAP_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
 $(eval $(call AUTOTARGETS,package,libpcap))
 
