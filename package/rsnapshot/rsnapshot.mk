6429fff00c4df015f81dee2865a8fad273f625de
João Cardoso <joao.fs.cardoso@gmail.com>
2013-05-24 17:14:19 +0000

    rsnapshot: new package

---
+++ package/rsnapshot/rsnapshot.mk
@@ -0,0 +1,21 @@
+#############################################################
+#
+# rsnapshot
+#
+#############################################################
+
+RSNAPSHOT_VERSION = 1.3.1
+RSNAPSHOT_SOURCE = rsnapshot-$(RSNAPSHOT_VERSION).tar.gz
+RSNAPSHOT_SITE = http://www.rsnapshot.org/downloads
+RSNAPSHOT_AUTORECONF = NO
+RSNAPSHOT_INSTALL_STAGING = NO
+RSNAPSHOT_INSTALL_TARGET = YES
+RSNAPSHOT_LIBTOOL_PATCH = NO
+
+RSNAPSHOT_DEPENDENCIES = uclibc rsync perl
+
+$(eval $(call AUTOTARGETS,package,rsnapshot))
+
+$(RSNAPSHOT_HOOK_POST_INSTALL):
+	mv $(TARGET_DIR)/etc/rsnapshot.conf.default $(TARGET_DIR)/etc/rsnapshot.conf
+	touch $@
