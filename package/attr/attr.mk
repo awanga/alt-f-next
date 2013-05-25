903cc767274bed007592dff19c61ebf42d4ff323
João Cardoso <joao.fs.cardoso@gmail.com>
2013-05-25 21:49:45 +0000

    attr: new package

---
+++ package/attr/attr.mk
@@ -0,0 +1,16 @@
+#############################################################
+#
+# attr
+#
+#############################################################
+ATTR_VERSION:=2.4.47
+ATTR_SOURCE:=attr-$(ATTR_VERSION).src.tar.gz
+ATTR_SITE:=http://download.savannah.nongnu.org/releases/attr
+ATTR_LIBTOOL_PATCH = NO
+ATTR_INSTALL_STAGING=YES
+
+ATTR_INSTALL_STAGING_OPT = DESTDIR="$(STAGING_DIR)" install install-dev install-lib
+ATTR_INSTALL_TARGET_OPT = DESTDIR="$(TARGET_DIR)" install install-lib
+ATTR_CONF_OPT = --disable-gettext
+
+$(eval $(call AUTOTARGETS,package,attr))
