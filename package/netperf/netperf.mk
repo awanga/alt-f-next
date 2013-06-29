7308893e13aca1494ffdbec0a316fb00062079e4
João Cardoso <joao.fs.cardoso@gmail.com>
2013-06-29 16:47:09 +0000

    netperf: new package

---
+++ package/netperf/netperf.mk
@@ -0,0 +1,19 @@
+#############################################################
+#
+# netperf
+#
+#############################################################
+
+NETPERF_VERSION:=2.6.0
+NETPERF_SOURCE:=netperf-$(NETPERF_VERSION).tar.gz
+NETPERF_SITE:=ftp://ftp.netperf.org/netperf
+NETPERF_AUTORECONF:=NO
+NETPERF_INSTALL_STAGING:=NO
+NETPERF_INSTALL_TARGET:=YES
+
+NETPERF_CONF_ENV:=ac_cv_func_setpgrp_void=yes
+NETPERF_CONF_OPT:=--program-prefix=""
+
+NETPERF_DEPENDENCIES:=uclibc
+
+$(eval $(call AUTOTARGETS,package,netperf))
