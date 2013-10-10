18473b95d40e8fecf83c59b7d0f7b626b035d61f
João Cardoso <joao.fs.cardoso@gmail.com>
2013-10-10 15:59:16 +0000

    uboot-mkimage: new package

---
+++ package/uboot-mkimage/uboot-mkimage.mk
@@ -0,0 +1,18 @@
+#############################################################
+#
+# uboot-mkimage for the host
+#
+#############################################################
+
+UBOOT_MKIMAGE_VERSION = 0.4
+UBOOT_MKIMAGE_SOURCE = uboot-mkimage_$(UBOOT_MKIMAGE_VERSION).tar.gz
+UBOOT_MKIMAGE_SITE = http://ftp.de.debian.org/debian/pool/main/u/uboot-mkimage
+
+UBOOT_MKIMAGE_DEPENDENCIES = uclibc 
+
+$(eval $(call AUTOTARGETS_HOST,package,uboot-mkimage))
+
+$(UBOOT_MKIMAGE_HOST_HOOK_POST_EXTRACT):
+	echo -e "#!/bin/sh\ntrue" > $(UBOOT_MKIMAGE_HOST_DIR)/configure
+	chmod +x $(UBOOT_MKIMAGE_HOST_DIR)/configure
+	touch $@
