0f9745620a247794a0754e7cfb67b887a098dda7
João Cardoso <joao.fs.cardoso@gmail.com>
2013-10-10 14:49:46 +0000

    htop: new package

---
+++ package/htop/htop.mk
@@ -0,0 +1,20 @@
+#############################################################
+#
+# htop
+#
+#############################################################
+
+HTOP_VERSION:=1.0.2
+HTOP_SOURCE:=htop-$(HTOP_VERSION).tar.gz
+HTOP_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/htop/htop/$(HTOP_VERSION)
+HTOP_LIBTOOL_PATCH = NO
+
+HTOP_DEPENDENCIES = ncurses
+HTOP_CONF_OPT = --disable-unicode --program-prefix=""
+
+$(eval $(call AUTOTARGETS,package,htop))
+
+$(HTOP_HOOK_POST_INSTALL):
+	-rm -rf $(TARGET_DIR)/usr/share/applications
+	-rm -rf $(TARGET_DIR)/usr/share/pixmaps
+	touch $@
