c71ef2240371ede77e3182b8674633bb62149482
João Cardoso <joao.fs.cardoso@gmail.com>
2013-10-10 15:05:13 +0000

    automatic: -bump to 0.8.3 -addapt to github odd way of distributing tarballs

--- package/automatic/automatic.mk
+++ package/automatic/automatic.mk
@@ -4,10 +4,15 @@
 #
 #############################################################
 
-#AUTOMATIC_VERSION = 0.6.7
-AUTOMATIC_VERSION = 0.7.2
-AUTOMATIC_SOURCE = automatic-$(AUTOMATIC_VERSION)-src.tar.gz
-AUTOMATIC_SITE = http://kylek.is-a-geek.org:31337/files/
+#AUTOMATIC_VERSION = 0.7.2
+#AUTOMATIC_SOURCE = automatic-$(AUTOMATIC_VERSION)-src.tar.gz
+#AUTOMATIC_SITE = http://kylek.is-a-geek.org:31337/files/
+
+# moved to github: https://github.com/1100101/Automatic/archive/v0.8.3.tar.gz
+AUTOMATIC_VERSION = 0.8.3
+AUTOMATIC_SOURCE = automatic-$(AUTOMATIC_VERSION).tar.gz
+AUTOMATIC_SITE = https://github.com/1100101/Automatic/archive
+
 AUTOMATIC_AUTORECONF = NO
 AUTOMATIC_INSTALL_STAGING = NO
 AUTOMATIC_INSTALL_TARGET = YES
@@ -16,3 +21,13 @@ AUTOMATIC_LIBTOOL_PATCH = NO
 AUTOMATIC_DEPENDENCIES = host-pkgconfig uclibc libxml2 pcre libcurl libiconv openssl
 
 $(eval $(call AUTOTARGETS,package,automatic))
+
+$(AUTOMATIC_TARGET_SOURCE):
+	$(call DOWNLOAD,$(AUTOMATIC_SITE),v$(AUTOMATIC_VERSION).tar.gz)
+	(cd $(DL_DIR); ln -sf v$(AUTOMATIC_VERSION).tar.gz automatic-$(AUTOMATIC_VERSION).tar.gz )
+	mkdir -p $(BUILD_DIR)/automatic-$(AUTOMATIC_VERSION)
+	touch $@
+
+$(AUTOMATIC_HOOK_POST_EXTRACT):
+	(cd $(AUTOMATIC_DIR); ./autogen.sh)
+	touch $@
