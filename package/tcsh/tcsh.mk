77900f0da21246ba74bfb1085b823df3168ee025
João Cardoso <joao.fs.cardoso@gmail.com>
2013-10-10 14:50:47 +0000

    tcsh: new package (thanks to John-Paul)

---
+++ package/tcsh/tcsh.mk
@@ -0,0 +1,13 @@
+#############################################################
+#
+# tcsh
+#
+#############################################################
+
+TCSH_VERSION:=6.18.01
+TCSH_SOURCE:=tcsh-$(TCSH_VERSION).tar.gz
+TCSH_SITE:=ftp://ftp.astron.com/pub/tcsh/
+
+TCSH_DEPENDENCIES = ncurses libiconv uclibc
+
+$(eval $(call AUTOTARGETS,package,tcsh))
