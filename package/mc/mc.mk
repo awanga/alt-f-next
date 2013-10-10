c152b13d4bff50041532939ddd0f78ff5e45862a
João Cardoso <joao.fs.cardoso@gmail.com>
2013-10-10 15:30:29 +0000

    mc: -host system ncurses was being used, fix -mc requires ncurses.h under ncurses include directory, so fool it!

--- package/mc/mc.mk
+++ package/mc/mc.mk
@@ -8,14 +8,26 @@ MC_VERSION = 4.8.1.6
 
 MC_SOURCE = mc-$(MC_VERSION).tar.bz2
 MC_SITE = http://www.midnight-commander.org/downloads
-MC_AUTORECONF = NO 
+MC_AUTORECONF = NO
 MC_INSTALL_STAGING = NO
 MC_INSTALL_TARGET = YES
 MC_LIBTOOL_PATCH = NO
 
-MC_DEPENDENCIES = libglib2
+MC_DEPENDENCIES = libglib2 ncurses
 
 MC_CONF_ENV = fu_cv_sys_stat_statfs2_bsize=yes
-MC_CONF_OPT = --disable-doxygen-doc --with-screen=ncurses
+MC_CONF_OPT = --disable-doxygen-doc --without-x --with-screen=ncurses \
+	--with-ncurses-includes=$(STAGING_DIR)/usr/include \
+	--with-ncurses-libs=$(STAGING_DIR)/usr/lib
 
 $(eval $(call AUTOTARGETS,package,mc))
+
+# mc requires ncurses.h under ncurses include directory, so fool it! 
+$(MC_HOOK_POST_EXTRACT):
+	( \
+	cd $(STAGING_DIR)/usr/include; \
+	mkdir ncurses; cd ncurses; \
+	ln -sf ../ncurses.h ncurses.h; \
+	ln -sf ../term.h term.h; \
+	)
+	touch $@
