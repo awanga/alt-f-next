91ed358b107a5a251d7108a3440530ad3e2c8033
João Cardoso <joao.fs.cardoso@gmail.com>
2013-10-10 15:56:36 +0000

    squashfs: new package

---
+++ package/squashfs/squashfs.mk
@@ -0,0 +1,45 @@
+#############################################################
+#
+# squashfs
+#
+#############################################################
+
+SQUASHFS_VERSION = 4.2
+SQUASHFS_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/squashfs
+SQUASHFS_SOURCE = squashfs$(SQUASHFS_VERSION).tar.gz
+
+SQUASHFS_AUTORECONF = NO
+SQUASHFS_INSTALL_STAGING = NO
+SQUASHFS_INSTALL_TARGET = YES
+SQUASHFS_LIBTOOL_PATCH = NO
+SQUASHFS_SUBDIR = squashfs-tools
+
+ifeq ($(BR2_PACKAGE_SQUASHFS_TARGET),y)
+SQUASHFS_DEPENDENCIES = uclibc xz
+SQUASHFS_MAKE_OPT = CC="$(TARGET_CC) $(TARGET_CFLAGS)"
+SQUASHFS_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR)/usr/bin install
+$(eval $(call AUTOTARGETS,package,squashfs))
+$(SQUASHFS_HOOK_POST_EXTRACT):
+	echo -e "#!/bin/sh\ntrue" > $(SQUASHFS_DIR)/$(SQUASHFS_SUBDIR)/configure
+	chmod +x $(SQUASHFS_DIR)/$(SQUASHFS_SUBDIR)/configure
+	sed -i -e 's/#XZ_SUPPORT = 1/XZ_SUPPORT = 1/' \
+	-e 's/#LZMA_XZ_SUPPORT = .*/LZMA_XZ_SUPPORT = 1/' \
+	-e 's/INSTALL_DIR/DESTDIR/' \
+	$(SQUASHFS_DIR)/$(SQUASHFS_SUBDIR)/Makefile
+	touch $@
+endif
+
+ifeq ($(BR2_PACKAGE_SQUASHFS_HOST),y)
+SQUASHFS_DEPENDENCIES = uclibc xz-host
+SQUASHFS_HOST_MAKE_OPT = EXTRA_LDFLAGS="-L$(HOST_DIR)/usr/lib" CC="$(HOSTCC) $(HOST_CFLAGS)"
+SQUASHFS_HOST_INSTALL_OPT = DESTDIR=$(HOST_DIR)/usr/bin install
+$(eval $(call AUTOTARGETS_HOST,package,squashfs))
+$(SQUASHFS_HOST_HOOK_POST_EXTRACT):
+	echo -e "#!/bin/sh\ntrue" > $(SQUASHFS_HOST_DIR)/$(SQUASHFS_SUBDIR)/configure
+	chmod +x $(SQUASHFS_HOST_DIR)/$(SQUASHFS_SUBDIR)/configure
+	sed -i -e 's/#XZ_SUPPORT = 1/XZ_SUPPORT = 1/' \
+	-e 's/#LZMA_XZ_SUPPORT = .*/LZMA_XZ_SUPPORT = 1/' \
+	-e 's/INSTALL_DIR/DESTDIR/' \
+	$(SQUASHFS_HOST_DIR)/$(SQUASHFS_SUBDIR)/Makefile
+	touch $@
+endif
