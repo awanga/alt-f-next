fa61d90a81c6700e2731b23093b1b8acc2effeda
João Cardoso <joao.fs.cardoso@gmail.com>
2013-10-10 15:00:54 +0000

    dropbear: -bump to  2013.58 -configure with cipher-none

--- package/dropbear/dropbear.mk
+++ package/dropbear/dropbear.mk
@@ -4,9 +4,9 @@
 #
 #############################################################
 
-#DROPBEAR_VERSION = 2011.54
-DROPBEAR_VERSION = 2012.55
-DROPBEAR_SOURCE = dropbear-$(DROPBEAR_VERSION).tar.gz
+#DROPBEAR_VERSION = 2012.55
+DROPBEAR_VERSION = 2013.58
+DROPBEAR_SOURCE = dropbear-$(DROPBEAR_VERSION).tar.bz2
 DROPBEAR_SITE = http://matt.ucc.asn.au/dropbear/releases
 DROPBEAR_DEPENDENCIES = uclibc zlib
 DROPBEAR_TARGET_BINS = dbclient dropbearkey dropbearconvert scp ssh
@@ -22,8 +22,9 @@ endif
 $(eval $(call AUTOTARGETS,package,dropbear))
 
 $(DROPBEAR_HOOK_POST_EXTRACT):
-	$(SED) 's,^#define XAUTH_COMMAND.*/xauth,#define XAUTH_COMMAND "/usr/bin/xauth,g' \
-	-e 's,^#define SFTPSERVER_PATH.*,#define SFTPSERVER_PATH "/usr/lib/sftp-server",g' \
+	$(SED) 's|^#define XAUTH_COMMAND.*/xauth|#define XAUTH_COMMAND "/usr/bin/xauth|g' \
+	-e 's|^#define SFTPSERVER_PATH.*|#define SFTPSERVER_PATH "/usr/lib/sftp-server"|g' \
+	-e 's|.*#define DROPBEAR_NONE_CIPHER.*|#define DROPBEAR_NONE_CIPHER|' \
 	$(DROPBEAR_DIR)/options.h
 	touch $@
 
@@ -36,15 +37,15 @@ $(DROPBEAR_TARGET_INSTALL_TARGET):
 	ln -snf ../sbin/dropbear $(TARGET_DIR)/usr/bin/dropbearconvert
 	ln -snf ../sbin/dropbear $(TARGET_DIR)/usr/bin/scp
 	ln -snf ../sbin/dropbear $(TARGET_DIR)/usr/bin/ssh
-	if [ ! -f $(TARGET_DIR)/etc/init.d/S50dropbear ]; then \
-		$(INSTALL) -m 0755 -D package/dropbear/S50dropbear $(TARGET_DIR)/etc/init.d/S50dropbear; \
-	fi
+	#if [ ! -f $(TARGET_DIR)/etc/init.d/S50dropbear ]; then \
+	#	$(INSTALL) -m 0755 -D package/dropbear/S50dropbear $(TARGET_DIR)/etc/init.d/S50dropbear; \
+	#fi
 	touch $@
 
 $(DROPBEAR_TARGET_UNINSTALL):
 	$(call MESSAGE,"Uninstalling")
 	rm -f $(TARGET_DIR)/usr/sbin/dropbear
 	rm -f $(addprefix $(TARGET_DIR)/usr/bin/, $(DROPBEAR_TARGET_BINS))
-	rm -f $(TARGET_DIR)/etc/init.d/S50dropbear
+	#rm -f $(TARGET_DIR)/etc/init.d/S50dropbear
 	rm -f $(DROPBEAR_TARGET_INSTALL_TARGET) $(DROPBEAR_HOOK_POST_INSTALL)
 
