a469c4c4ec1286f0c9fb76eb8aaccbcf9b27eeb4
João Cardoso <joao.fs.cardoso@gmail.com>
2013-10-10 15:15:21 +0000

    ipkg: use alternate download site (main site is down)

--- package/ipkg/ipkg.mk
+++ package/ipkg/ipkg.mk
@@ -6,7 +6,10 @@
 
 IPKG_VERSION = 0.99.163
 IPKG_SOURCE = ipkg-$(IPKG_VERSION).tar.gz
-IPKG_SITE = http://www.handhelds.org/download/packages/ipkg/
+# original is down
+# IPKG_SITE = http://www.handhelds.org/download/packages/ipkg/
+IPKG_SITE = http://ftp.gwdg.de/linux/handhelds/packages/ipkg
+
 IPKG_AUTORECONF = NO
 IPKG_INSTALL_STAGING = YES
 IPKG_INSTALL_TARGET = YES
