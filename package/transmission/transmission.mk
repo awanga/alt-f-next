f4d2acc36c1c476cb30aa8bf98036e131529381d
João Cardoso <joao.fs.cardoso@gmail.com>
2013-10-10 15:57:52 +0000

    transmission: bump to 2.80

--- package/transmission/transmission.mk
+++ package/transmission/transmission.mk
@@ -4,9 +4,8 @@
 #
 #############################################################
 
-#TRANSMISSION_VERSION = 2.76
+
 TRANSMISSION_VERSION = 2.80
-#TRANSMISSION_SOURCE = transmission-$(TRANSMISSION_VERSION).tar.bz2
 TRANSMISSION_SOURCE = transmission-$(TRANSMISSION_VERSION).tar.xz
 TRANSMISSION_SITE = http://download.m0k.org/transmission/files
 TRANSMISSION_AUTORECONF = NO
