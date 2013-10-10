391c54e77e28031a4babdc2d2fdf37d571a858db
João Cardoso <joao.fs.cardoso@gmail.com>
2013-10-10 16:01:13 +0000

    xz: new package

---
+++ package/xz/xz.mk
@@ -0,0 +1,25 @@
+#############################################################
+#
+# xz
+#
+#############################################################
+
+XZ_VERSION = 5.0.5
+XZ_SITE = http://tukaani.org/xz
+XZ_SOURCE = xz-$(XZ_VERSION).tar.bz2
+
+XZ_INSTALL_STAGING = YES
+XZ_INSTALL_TARGET = YES
+XZ_AUTORECONF = NO
+XZ_LIBTOOL_PATCH = NO
+XZ_DEPENDENCIES = uclibc 
+
+ifeq ($(BR2_PACKAGE_XZ_HOST),y)
+$(eval $(call AUTOTARGETS_HOST,package,xz))
+endif
+
+ifeq ($(BR2_PACKAGE_XZ_TARGET),y)
+XZ_INSTALL_STAGING = YES
+XZ_INSTALL_TARGET = YES
+$(eval $(call AUTOTARGETS,package,xz))
+endif
