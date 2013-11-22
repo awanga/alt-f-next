8a989f189447c638f9cb9fa45c849acdaf9e9e5a
João Cardoso <joao.fs.cardoso@gmail.com>
2013-11-22 15:56:28 +0000

    ntfs-3g_ntfsprogs: build everything, remove from target dir what is not configured

--- package/ntfs-3g_ntfsprogs/ntfs-3g_ntfsprogs.mk
+++ package/ntfs-3g_ntfsprogs/ntfs-3g_ntfsprogs.mk
@@ -4,29 +4,28 @@
 #
 #############################################################
 
-#NTFS_3G_NTFSPROGS_VERSION:=2011.4.12
 NTFS_3G_NTFSPROGS_VERSION:=2012.1.15
 NTFS_3G_NTFSPROGS_SOURCE:=ntfs-3g_ntfsprogs-$(NTFS_3G_NTFSPROGS_VERSION).tgz
 NTFS_3G_NTFSPROGS_SITE:=http://tuxera.com/opensource
-NTFS_3G_NTFSPROGS_INSTALL_STAGING:=yes
+
 NTFS_3G_NTFSPROGS_CONF_OPT = --libdir=/usr/lib --disable-ldconfig --disable-mtab --program-prefix=""
 
-ifeq ($(BR2_PACKAGE_NTFS_3G_NTFSPROGS_NTFSPROGS),)
-NTFS_3G_NTFSPROGS_CONF_OPT += --disable-ntfsprogs
-endif
+NTFS_3G_NTFSPROGS_EXTRA_BIN = ntfscat ntfscluster ntfscmp ntfsfix ntfsinfo ntfsls \
+	lowntfs-3g ntfs-3g.secaudit ntfs-3g.usermap ntfs-3g.probe
+NTFS_3G_NTFSPROGS_EXTRA_SBIN = mkntfs ntfsclone ntfscp ntfslabel ntfsresize ntfsundelete
+NTFS_3G_NTFSPROGS_NO = mkfs.ntfs mount.lowntfs-3g mount.ntfs-3g
 
 $(eval $(call AUTOTARGETS,package,ntfs-3g_ntfsprogs))
 
+$(NTFS_3G_NTFSPROGS_HOOK_POST_INSTALL):
 ifeq ($(BR2_PACKAGE_NTFS_3G_NTFSPROGS_NTFSPROGS),)
-$(NTFS_3G_NTFSPROGS_TARGET_INSTALL_TARGET): $(NTFS_3G_NTFSPROGS_TARGET_INSTALL_STAGING)
-	cp -dpf $(STAGING_DIR)/usr/lib/libntfs-3g.so* $(TARGET_DIR)/usr/lib/
-	$(INSTALL) -m 0755 $(STAGING_DIR)/usr/bin/ntfs-3g $(TARGET_DIR)/usr/bin/
-	touch $@
+	( cd $(TARGET_DIR); \
+	for i in $(NTFS_3G_NTFSPROGS_EXTRA_BIN); do rm usr/bin/$$i; done; \
+	for i in $(NTFS_3G_NTFSPROGS_EXTRA_SBIN); do rm usr/sbin/$$i; done; \
+	)
 endif
+	( cd $(TARGET_DIR); \
+	for i in $(NTFS_3G_NTFSPROGS_NO); do rm sbin/$$i; done; \
+	)
+	touch $@
 
-$(NTFS_3G_NTFSPROGS_TARGET_UNINSTALL):
-	$(call MESSAGE,"Uninstalling")
-	$(MAKE) DESTDIR=$(STAGING_DIR) -C $(NTFS_3G_NTFSPROGS_DIR) uninstall
-	rm -f $(TARGET_DIR)/usr/lib/libntfs-3g*
-	rm -f $(TARGET_DIR)/usr/bin/ntfs-3g $(TARGET_DIR)/usr/bin/ntfs-3g.probe
-	rm -f $(NTFS_3G_NTFSPROGS_TARGET_INSTALL_STAGING) $(NTFS_3G_NTFSPROGS_TARGET_INSTALL_TARGET) $(NTFS_3G_NTFSPROGS_HOOK_POST_INSTALL)
