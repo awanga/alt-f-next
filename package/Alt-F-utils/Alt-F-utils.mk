8cbec71c988b7f7810f9c5f40e73b9af64e50be8
João Cardoso <joao.fs.cardoso@gmail.com>
2013-12-11 21:00:44 +0000

    Alt-F-utils: -bump to 0.1.7 -build for the host

--- package/Alt-F-utils/Alt-F-utils.mk
+++ package/Alt-F-utils/Alt-F-utils.mk
@@ -1,37 +1,41 @@
 #############################################################
 #
-# ALT_F_UTILS
+# Alt-F-utils
 #
 #############################################################
-ALT_F_UTILS_VERSION = 0.1.6
+ALT_F_UTILS_VERSION = 0.1.7
 ALT_F_UTILS_SOURCE = Alt-F-utils-$(ALT_F_UTILS_VERSION).tar.gz
-ALT_F_UTILS_SITE = http://localhost/~jcard
-ALT_F_UTILS_DIR:=$(BUILD_DIR)/Alt-F-utils-$(ALT_F_UTILS_VERSION)
-ALT_F_UTILS_AUTORECONF:=NO
-ALT_F_UTILS_INSTALL_STAGING:=NO
-ALT_F_UTILS_INSTALL_TARGET:=YES
-ALT_F_UTILS_CAT:=$(BZCAT)
-ALT_F_UTILS_BINARY:=sysctrl
-ALT_F_UTILS_TARGET_BINARY:=usr/sbin/sysctrl
-ALT_F_UTILS_DEPENDENCIES:=uclibc
+ALT_F_UTILS_SITE = file://$(DL_DIR)
+
+ALT_F_UTILS_AUTORECONF := NO
+ALT_F_UTILS_INSTALL_STAGING := NO
+ALT_F_UTILS_INSTALL_TARGET := YES
+ALT_F_UTILS_DEPENDENCIES := uclibc
 
 $(DL_DIR)/$(ALT_F_UTILS_SOURCE): package/Alt-F-utils/Alt-F-utils-$(ALT_F_UTILS_VERSION)/sysctrl.c package/Alt-F-utils/Alt-F-utils-$(ALT_F_UTILS_VERSION)/dns323-fw.c
 	(cd package/Alt-F-utils/; \
-		tar --exclude-vcs -cvzf $(ALT_F_UTILS_SOURCE) Alt-F-utils-$(ALT_F_UTILS_VERSION); \
+		rm *~; \
+		$(TAR) --exclude-vcs -cvzf $(ALT_F_UTILS_SOURCE) Alt-F-utils-$(ALT_F_UTILS_VERSION); \
 		cp  $(ALT_F_UTILS_SOURCE) $(DL_DIR) \
 	)
 
-Alt-F-utils-source: $(DL_DIR)/$(ALT_F_UTILS_SOURCE)
-
-Alt-F-utils: Alt-F-utils-source
-
+ifeq ($(BR2_PACKAGE_ALT_F_UTILS_TARGET),y)
+ALT_F_UTILS_MAKE_ENV := CC="$(TARGET_CC) $(TARGET_CFLAGS)" STRIP="$(TARGET_STRIP)"
 $(eval $(call AUTOTARGETS,package,Alt-F-utils))
-
-#############################################################
-#
-# Toplevel Makefile options
-#
-#############################################################
-ifeq ($(BR2_PACKAGE_ALT_F_UTILS),y)
-TARGETS+=Alt-F-utils
 endif
+
+ifeq ($(BR2_PACKAGE_ALT_F_UTILS_HOST),y)
+ALT_F_UTILS_HOST_MAKE_ENV := STRIP=true
+$(eval $(call AUTOTARGETS_HOST,package,Alt-F-utils))
+
+$(ALT_F_UTILS_HOST_HOOK_POST_INSTALL):
+	mkdir -p $(HOST_DIR)/usr/bin
+	( cd $(HOST_DIR)/usr/sbin/; \
+		rm rpcinfo sysctrl; \
+		mv dns323-fw ../bin \
+	)
+	touch $@
+
+endif
+
+$(ALT_F_UTILS_TARGET_SOURCE): $(DL_DIR)/$(ALT_F_UTILS_SOURCE)
