73a4e5cb074cee73c5e4d3ae60288af27492e33d
João Cardoso <joao.fs.cardoso@gmail.com>
2013-12-19 02:45:58 +0000

    rsync mk: add support for building with ACL/XATTR (not currently used)

--- package/rsync/rsync.mk
+++ package/rsync/rsync.mk
@@ -4,7 +4,6 @@
 #
 #############################################################
 
-#RSYNC_VERSION:=3.0.8
 RSYNC_VERSION:=3.0.9
 RSYNC_SOURCE:=rsync-$(RSYNC_VERSION).tar.gz
 RSYNC_SITE:=http://rsync.samba.org/ftp/rsync/src
@@ -12,16 +11,23 @@ RSYNC_AUTORECONF:=no
 RSYNC_USE_CONFIG_CACHE:=no
 RSYNC_INSTALL_STAGING:=NO
 RSYNC_INSTALL_TARGET:=YES
+
 ifeq ($(BR2_ENABLE_DEBUG),y)
 RSYNC_INSTALL_TARGET_OPT:=DESTDIR=$(TARGET_DIR) INSTALLCMD='./install-sh -c' \
-			  install
+	install
 else
 RSYNC_INSTALL_TARGET_OPT:=DESTDIR=$(TARGET_DIR) INSTALLCMD='./install-sh -c' \
-			  STRIPPROG="$(TARGET_STRIP)" install-strip
+	STRIPPROG="$(TARGET_STRIP)" install-strip
 endif
-#RSYNC_CONF_OPT:=--with-included-popt $(DISABLE_IPV6)
-RSYNC_CONF_OPT:=$(DISABLE_IPV6)
 
 RSYNC_DEPENDENCIES:=uclibc popt
+RSYNC_CONF_OPT:=$(DISABLE_IPV6)
+
+ifeq ($(BR2_PACKAGE_RSYNC_ACL),y)
+RSYNC_DEPENDENCIES += acl
+RSYNC_CONF_OPT += --enable-acl-support --enable-xattr-support
+else
+RSYNC_CONF_OPT += --disable-acl-support --disable-xattr-support
+endif
 
 $(eval $(call AUTOTARGETS,package,rsync))
