99d2640e4e15b3034b8c19ee84d90142a847b4a9
João Cardoso <joao.fs.cardoso@gmail.com>
2014-03-01 17:58:14 +0000

    jpeg: add jpeg-libs ipkg package

--- package/jpeg/jpeg.mk
+++ package/jpeg/jpeg.mk
@@ -5,12 +5,22 @@
 #############################################################
 
 JPEG_VERSION:=8c
-JPEG_DIR=$(BUILD_DIR)/jpeg-$(JPEG_VERSION)
 JPEG_SITE:=http://www.ijg.org/files/
 JPEG_SOURCE=jpegsrc.v$(JPEG_VERSION).tar.gz
+
+JPEG_DIR=$(BUILD_DIR)/jpeg-$(JPEG_VERSION)
+
 JPEG_LIBTOOL_PATCH = NO
 JPEG_INSTALL_STAGING = YES
+
+JPEG_PROGS = cjpeg djpeg jpegtran rdjpgcom wrjpgcom 
 
 JPEG_CONF_OPT = --disable-static --program-prefix=""
 
 $(eval $(call AUTOTARGETS,package,jpeg))
+
+$(JPEG_HOOK_POST_INSTALL):
+ifneq ($(BR2_PACKAGE_JPEG_PROGS),y)
+	(cd $(TARGET_DIR)/usr/bin; rm -f $(JPEG_PROGS))
+endif
+	touch $@
