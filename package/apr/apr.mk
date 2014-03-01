8c77fa57128f90b1f2979bd293ee95291db9f10f
João Cardoso <joao.fs.cardoso@gmail.com>
2014-03-01 17:11:24 +0000

    apr: bump devel version; remove stray files on rootfs; fix apr-config and libapr.la

--- package/apr/apr.mk
+++ package/apr/apr.mk
@@ -7,8 +7,10 @@
 APR_VERSION = 1.4.6
 APR_SITE = http://archive.apache.org/dist/apr
 APR_SOURCE = apr-$(APR_VERSION).tar.gz
+
 APR_INSTALL_STAGING = YES
 APR_LIBTOOL_PATCH = NO
+
 APR_DEPENDENCIES = libuuid
 
 APR_CONF_ENV = \
@@ -28,14 +30,22 @@ APR_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) install
 
 $(eval $(call AUTOTARGETS,package,apr))
 
-# cross-compile/stagind-dir HACKS!
+$(APR_HOOK_POST_INSTALL):
+	rm -rf $(TARGET_DIR)/usr/bin/apr-1-config \
+		$(TARGET_DIR)/usr/lib/apr.exp \
+		$(TARGET_DIR)/usr/share/apr-1
+	touch $@
+
 $(APR_TARGET_INSTALL_STAGING):
 	$(MAKE) DESTDIR=$(STAGING_DIR) -C $(APR_DIR) install
-	sed -i 's|="/usr|="$(STAGING_DIR)/usr|' $(STAGING_DIR)/usr/bin/apr-1-config
-	sed -i 's|=/usr|=$(STAGING_DIR)/usr|' $(STAGING_DIR)/usr/lib/pkgconfig/apr-1.pc
-	sed -i "s|^libdir=.*|libdir='$(STAGING_DIR)/usr/lib'|" $(STAGING_DIR)/usr/lib/libapr-1.la
-	sed -i -e 's|apr_builddir=/|apr_builddir=$(STAGING_DIR)/|' \
+	$(SED) "s|^prefix=.*|prefix=\'$(STAGING_DIR)/usr\'|g" \
+		-e "s|^exec_prefix=.*|exec_prefix=\'$(STAGING_DIR)/usr\'|g" \
+		-e "s|^libdir=.*|libdir=\'$(STAGING_DIR)/usr/lib\'|g" \
+		-e "s|^installbuilddir=\"\(.*\)\"|installbuilddir=\'$(STAGING_DIR)\1\'|g" \
+		$(STAGING_DIR)/usr/bin/apr-1-config
+	$(SED) 's|apr_builddir=/|apr_builddir=$(STAGING_DIR)/|' \
 		-e 's|top_builddir=/|top_builddir=$(STAGING_DIR)/|' \
 		-e 's|apr_builders=/|apr_builders=$(STAGING_DIR)/|' \
 		$(STAGING_DIR)/$(APR_INSTALL_BUILD)/apr_rules.mk
+	$(SED) "s|^libdir=.*|libdir='$(STAGING_DIR)/usr/lib'|" $(STAGING_DIR)/usr/lib/libapr-1.la
 	touch $@
