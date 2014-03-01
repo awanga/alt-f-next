b10316668978de150a477be9d0c3de5c6f8b5b54
João Cardoso <joao.fs.cardoso@gmail.com>
2014-03-01 18:00:00 +0000

    kernel-modules: bump to 3.10.32

--- package/kernel-modules/kernel-modules.mk
+++ package/kernel-modules/kernel-modules.mk
@@ -3,7 +3,8 @@
 # kernel-modules
 #
 #############################################################
-KERNEL_MODULES_VERSION:=2.6.35.14
+
+KERNEL_MODULES_VERSION:=3.10.32
 
 # this is a dummy target, it is here just to have BR2_PACKAGE_KERNEL_MODULES defined
 kernel-modules: uclibc
