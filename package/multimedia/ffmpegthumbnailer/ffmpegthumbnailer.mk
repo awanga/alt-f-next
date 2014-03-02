df7cbd8e8be04c716c148301c0a81bdc3b5c2665
João Cardoso <joao.fs.cardoso@gmail.com>
2014-03-02 15:53:00 +0000

    ffmpegthumbnailer: bump to 2.0.8

--- package/multimedia/ffmpegthumbnailer/ffmpegthumbnailer.mk
+++ package/multimedia/ffmpegthumbnailer/ffmpegthumbnailer.mk
@@ -3,15 +3,20 @@
 # ffmpegthumbnailer
 #
 #############################################################
-FFMPEGTHUMBNAILER_VERSION = 2.0.2
+
+FFMPEGTHUMBNAILER_VERSION = 2.0.8
 FFMPEGTHUMBNAILER_SOURCE = ffmpegthumbnailer-$(FFMPEGTHUMBNAILER_VERSION).tar.gz
 FFMPEGTHUMBNAILER_SITE = http://ffmpegthumbnailer.googlecode.com/files
+
 FFMPEGTHUMBNAILER_AUTORECONF = NO
 FFMPEGTHUMBNAILER_LIBTOOL_PATCH = NO
+
 FFMPEGTHUMBNAILER_INSTALL_STAGING = YES
 FFMPEGTHUMBNAILER_INSTALL_TARGET = YES
+
 FFMPEGTHUMBNAILER_DEPENDENCIES = uclibc ffmpeg jpeg libpng
+
 FFMPEGTHUMBNAILER_CONF_OPT = --disable-static --enable-png --enable-jpeg
-FFMPEGTHUMBNAILER_CONF_ENV = PKG_CONFIG_PATH=$(STAGING_DIR)//usr/lib/pkgconfig/
+FFMPEGTHUMBNAILER_CONF_ENV = PKG_CONFIG_PATH=$(STAGING_DIR)/usr/lib/pkgconfig/
 
 $(eval $(call AUTOTARGETS,package/multimedia,ffmpegthumbnailer))
