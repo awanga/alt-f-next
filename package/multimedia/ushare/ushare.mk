709699ae26b64f1ca6d24a35beed5d7c72f655bd
João Cardoso <joao.fs.cardoso@gmail.com>
2014-03-02 16:31:16 +0000

    ushare: add ipkg preint script; bump devel version

--- package/multimedia/ushare/ushare.mk
+++ package/multimedia/ushare/ushare.mk
@@ -3,18 +3,23 @@
 # uShare
 #
 #############################################################
+
 USHARE_VERSION = 1.1a
 USHARE_SOURCE = ushare-$(USHARE_VERSION).tar.bz2
 USHARE_SITE = http://ushare.geexbox.org/releases/
+
 USHARE_AUTORECONF = NO
+USHARE_LIBTOOL_PATCH = NO
+
 USHARE_INSTALL_STAGING = NO
 USHARE_INSTALL_TARGET = YES
-USHARE_LIBTOOL_PATCH = NO
+
 USHARE_DEPENDENCIES = uclibc libupnp libdlna
+
 USHARE_CONF_OPT = --enable-dlna \
 	--disable-static \
 	--cross-compile \
-	--cross-prefix=$(STAGING_DIR)/usr/bin/arm-linux-uclibcgnueabi- \
+	--cross-prefix=arm-linux-uclibcgnueabi- \
 	--with-libdlna-dir=$(STAGING_DIR)/usr/ \
 	--with-libupnp-dir=$(STAGING_DIR)/usr/
 
