10733ed99c4e7002efa6d80d8d3f80512a957f19
João Cardoso <joao.fs.cardoso@gmail.com>
2014-03-02 16:09:22 +0000

    fuppes: disable building with ffmpeg (not compatible with ffmpeg recent version), flac/vorbis/mad/taglib/lame/... most external libs

--- package/multimedia/fuppes/fuppes.mk
+++ package/multimedia/fuppes/fuppes.mk
@@ -3,30 +3,24 @@
 # fuppes
 #
 #############################################################
-#FUPPES_VERSION = 689
+
 FUPPES_VERSION = 660
 FUPPES_SOURCE = fuppes-0.$(FUPPES_VERSION).tar.gz
 FUPPES_SITE = http://downloads.sourceforge.net/project/fuppes/fuppes/SVN-$(FUPPES_VERSION)
+
 FUPPES_INSTALL_STAGING = NO
 FUPPES_INSTALL_TARGET = YES
+
 FUPPES_LIBTOOL_PATCH = NO
 FUPPES_AUTORECONF = NO
 
-FUPPES_DEPENDENCIES = uclibc pkg-config pcre libxml2 sqlite ffmpeg taglib flac libvorbis libmad taglib lame ffmpegthumbnailer
+FUPPES_DEPENDENCIES = uclibc pkg-config pcre libxml2 sqlite
 
 FUPPES_CONF_ENV += MYSQL_CONFIG=no \
- TAGLIB_CONFIG=$(STAGING_DIR)/usr/bin/taglib-config \
- MAD_CFLAGS=" " MAD_LIBS=-lmad \
- PKG_CONFIG_PATH=$(STAGING_DIR)/usr/lib/pkgconfig \
- FFMPEGTHUMBNAILER_CFLAGS=-I$(STAGING_DIR)/usr/include/libffmpegthumbnailer
+	PKG_CONFIG_PATH=$(STAGING_DIR)/usr/lib/pkgconfig
 
-#FUPPES_CONF_ENV += MYSQL_CONFIG=no \
-# TAGLIB_CFLAGS=-I$(STAGING_DIR)/usr/include/taglib \
-# MAD_CFLAGS=" " MAD_LIBS=-lmad \
-# PKG_CONFIG_PATH=$(STAGING_DIR)/usr/lib/pkgconfig
-
-FUPPES_CONF_OPT = --disable-mp4v2 --disable-ffmpegthumbnailer --enable-transcoder-ffmpeg --enable-mad --enable-lame  
-
-# 689: --localstatedir=/var
+FUPPES_CONF_OPT = --disable-mp4v2 --disable-transcoder-ffmpeg --disable-magickwand \
+	--disable-ffmpegthumbnailer --disable-libavformat --disable-lame \
+	--disable-flac --disable-vorbis --disable-mad --disable-taglib --enable-dlna 
 
 $(eval $(call AUTOTARGETS,package/multimedia,fuppes))
