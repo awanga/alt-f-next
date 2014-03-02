880a699d42b21b5f8aad8a32948281f69a8ed437
João Cardoso <joao.fs.cardoso@gmail.com>
2014-03-02 16:10:53 +0000

    lame: bump to 3.99.5

--- package/multimedia/lame/lame.mk
+++ package/multimedia/lame/lame.mk
@@ -3,12 +3,17 @@
 # lame
 #
 #############################################################
-LAME_VERSION = 3.98.4
+
+LAME_VERSION = 3.99.5
 LAME_SOURCE = lame-$(LAME_VERSION).tar.gz
 LAME_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/lame/
+
 LAME_AUTORECONF = NO
+LAME_LIBTOOL_PATCH = NO
+
 LAME_INSTALL_STAGING = YES
 LAME_INSTALL_TARGET = YES
+
 LAME_DEPENDENCIES = uclibc
 LAME_CONF_OPT = --disable-static 
 
