955bd10601f11d92072d87fb47d6b8ac63899f10
João Cardoso <joao.fs.cardoso@gmail.com>
2014-03-02 16:16:28 +0000

    libdlna: fix ffmpeg includes patches; bump devel version

--- package/multimedia/libdlna/libdlna.mk
+++ package/multimedia/libdlna/libdlna.mk
@@ -12,12 +12,12 @@ LIBDLNA_INSTALL_TARGET = YES
 LIBDLNA_LIBTOOL_PATCH = NO
 LIBDLNA_CONF_OPT = --disable-static \
 	--cross-compile \
-	--cross-prefix=$(STAGING_DIR)/usr/bin/arm-linux-uclibcgnueabi- \
-	--with-ffmpeg-dir=$(STAGING_DIR)/usr/
-	
+	--cross-prefix=arm-linux-uclibcgnueabi-
+
 LIBDLNA_DEPENDENCIES = uclibc ffmpeg
 
 $(eval $(call AUTOTARGETS,package/multimedia,libdlna))
+
 
 $(LIBDLNA_TARGET_INSTALL_TARGET):
 	$(MAKE) DESTDIR=$(TARGET_DIR) -C $(LIBDLNA_DIR) install
