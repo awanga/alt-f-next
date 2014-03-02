2c5a35f583873566cf2d7b42435ecc5c991c15a5
João Cardoso <joao.fs.cardoso@gmail.com>
2014-03-02 16:21:35 +0000

    libogg: bump to 1.3.1

--- package/multimedia/libogg/libogg.mk
+++ package/multimedia/libogg/libogg.mk
@@ -3,10 +3,12 @@
 # libogg
 #
 #############################################################
-LIBOGG_VERSION = 1.1.4
+
+LIBOGG_VERSION = 1.3.1
 LIBOGG_SOURCE = libogg-$(LIBOGG_VERSION).tar.gz
 LIBOGG_SITE = http://downloads.xiph.org/releases/ogg
 LIBOGG_AUTORECONF = NO
+LIBOGG_LIBTOOL_PATCH = NO
 LIBOGG_INSTALL_STAGING = YES
 LIBOGG_INSTALL_TARGET = YES
 
