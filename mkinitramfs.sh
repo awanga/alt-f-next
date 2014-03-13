ffa9990d682e4a231d40ad987c311979511c6979
João Cardoso <joao.fs.cardoso@gmail.com>
2014-03-13 14:43:29 +0000

    mkinitramfs.sh: handle pre-installed packages ipkg data; add xz support; handle sqsplit fw format for DNS-320/325;

--- mkinitramfs.sh
+++ mkinitramfs.sh
@@ -1,5 +1,51 @@
 #!/bin/bash
 
+#set -x
+
+# recursively find packages that <package> depends on: 
+rdeps() {
+	echo $1 $(awk '/Version:/{print $2}' $CWD/ipkgfiles/$1.control)
+	deps=$(awk '/Depends:/{for (i=2; i<=NF; i++) print $i}' $CWD/ipkgfiles/$1.control)
+	for i in $deps; do
+		p=${i%%,}
+		if ! test "$p" = "ipkg"; then rdeps $p; fi
+	done
+}
+
+# preinst.status needed at runtime to avoid installing pre installed packages
+# when they are dependencies of a new package being installed
+deps_status() {
+	grep -E '(Package:|Version:|Depends:|Architecture:)' $CWD/ipkgfiles/$1.control
+	echo "Status: install user installed"
+	echo "Installed-Time: $(date +%s)"
+					
+	if test -f $CWD/ipkgfiles/$1.conffiles; then
+		echo Conffiles: 
+		for j in $(cat $CWD/ipkgfiles/$1.conffiles); do
+			echo "/Alt-F/$j $(md5sum $BLDDIR/project_build_arm/$board/root/$j | cut -d" " -f1)"
+		done
+	fi  
+	echo
+}
+
+deps_check() {
+	if ! ( cd $CWD; ./mkpkg.sh -check $1 >& /dev/null); then
+		echo "WARNING: Package $1 does not contains all files, might or not be OK!" > $(tty)
+	fi
+}
+
+beroot() {
+	if test $(whoami) != "root"; then
+		sudo -E $0 $TYPE $COMP
+		exit $?
+	fi
+}
+
+usage() {
+	echo "Usage: mkinitramfs [type] (cpio|squsr|sqall*|sqsplit) [compression] (gz|lzma|xz*)"
+	exit 1
+}
+
 if test "$(dirname $0)" != "."; then
 	echo "This script must be run in the root of the tree, exiting."
 	exit 1;
@@ -15,9 +61,29 @@ if test -z "$BLDDIR"; then
 	exit 1
 fi
 
-if test "$#" != 1; then
-	echo "Usage: mkinitramfs gz | lzma | sqfs | sqmtd"
-	exit
+if test "$#" = 0; then
+	TYPE=sqall
+	COMP=xz
+elif test "$#" = 1; then
+	if test "$1" = "cpio" -o "$1" = "squsr" -o "$1" = "sqall" -o "$1" = "sqsplit"; then
+		TYPE=$1
+		COMP=xz
+	elif test "$1" = "gz" -o "$1" = "lzma" -o "$1" = "xz"; then
+		TYPE=sqall
+		COMP=$1
+	else
+		usage
+	fi
+elif test "$#" = 2; then
+	TYPE=$1
+	COMP=$2
+else
+	usage
+fi
+
+if test $TYPE != "cpio" -a $TYPE != "squsr" -a $TYPE != "sqall" -a "$1" != "sqsplit" \
+	-a $COMP != "gz" -a $COMP != "lzma" -a $COMP != "xz"; then
+	usage
 fi
 
 if test -z "$ME" -o -z "$MG"; then
@@ -25,81 +91,175 @@ if test -z "$ME" -o -z "$MG"; then
 	export MG=$(id -gn)
 fi
 
-if test $(whoami) != "root"; then
-	sudo -E $0 $1
-	exit
-fi
-
 PATH=$(pwd)/bin:$PATH
-cd ${BLDDIR}/binaries/dns323
 
-if ! test -d tmp; then
-	mkdir tmp
+. .config 2> /dev/null
+board=$BR2_PROJECT
+
+EXT=$COMP
+if test "$COMP" = "xz"; then
+	cmd="xz --check=crc32 -z -6"
+elif test "$COMP" = "lzma"; then
+	cmd="lzma -6"
+elif test "$COMP" = "gz"; then
+	cmd="gzip -9"
+	COMP=gzip
+else
+	echo "mkinitramfs.sh: unknown compressor."
+	exit 1
 fi
 
-if test "$1" = "gz"; then
+# SQFSBLK: squashfs compression block sizes: 131072 262144 524288 1048576
+
+# FIXME: shouldn't this be in .config instead? to diminish redundancy and missing dependencies?
+# fw_pkgs: pre-installed packages in base firmware
+# sq_pkgs: pre-installed packages on sqimage 
+# base_pkgs/base_pkgs2 contains all packages for the base firmware but uClibc and busybox.
+# Other packages often don't explicitly depends on them, so we have to list them all here.
+base_pkgs="alt-f-utils mdadm e2fsprogs dosfstools dropbear rsync portmap nfs-utils kexec gptfdisk-sgdisk sfdisk ntfs-3g zlib libiconv popt"
+base_pkgs2="inadyn-mt smartmontools at dnsmasq ntp samba-small openssl openssh-sftp vsftpd wget msmtp stunnel"
+
+case $board in
+	dns323)
+		SQFSBLK=262144
+		fw_pkgs="$base_pkgs $base_pkgs2"
+		;;
+	dns321)
+		SQFSBLK=131072
+		#SQFSBLK=262144
+		fw_pkgs="$base_pkgs $base_pkgs2 minidlna"
+		;;
+	dns325)
+		SQFSBLK=131072
+		fw_pkgs="$base_pkgs gptfdisk mtd-utils"
+		sq_pkgs="$base_pkgs2 ntfs-3g-ntfsprogs quota-tools minidlna netatalk forked-daapd transmission iscsitarget"
+		;;
+esac
+
+CWD=$PWD
+
+if test "$TYPE" = "cpio"; then # standard initramfs
+	beroot
+
+	cd ${BLDDIR}/binaries/$board
+	mkdir -p tmp
+
 	mount -o ro,loop rootfs.arm.ext2 tmp
 	cd tmp
-	find . | cpio --quiet -o -H newc | gzip -9 > ../rootfs.arm.cpio.gz
+	find . | cpio --quiet -o -H newc | $cmd > ../rootfs.arm.$TYPE.$EXT
 	cd ..
 	umount tmp
-	chown $ME:$MG rootfs.arm.cpio.gz
+	rmdir tmp
 
-elif test "$1" = "lzma"; then
-	mount -o ro,loop rootfs.arm.ext2 tmp
-	cd tmp
-	find . | cpio --quiet -o -H newc | lzma e -si -so > ../rootfs.arm.cpio.lzma
-	cd ..
-	umount tmp
-	chown $ME:$MG rootfs.arm.cpio.lzma
+	chown $ME:$MG rootfs.arm.$TYPE.$EXT
 
-elif test "$1" = "sqfs"; then
-	# only /usr
+elif test "$TYPE" = "squsr"; then # standard initramfs with /usr squashed
+	beroot
+
+	cd ${BLDDIR}/binaries/$board
+	mkdir -p tmp
+
 	cp rootfs.arm.ext2 rootfs.arm.ext2.tmp
 	mount -o loop rootfs.arm.ext2.tmp tmp
 
-	# lzma or gzip
-	COMP=lzma
-
-	# block sizes: 131072 262144 524288 1048576
-	mksquashfs tmp/usr/ usr.squashfs -comp $COMP -b 262144 \
-		-always-use-fragments -keep-as-directory
+	mksquashfs tmp/usr/ usr.squashfs -comp $COMP -b $SQFSBLK \
+		-always-use-fragments -keep-as-directory -all-root
 	rm -rf tmp/usr/*
 	mv usr.squashfs tmp
 	cd tmp
 
-	# there is little space savings (100KB) trying to compress with lzma
-	# when /usr is gzip compressed, and boot times are looonger
-	if test "$COMP" = "lzma"; then 
-		find . | cpio --quiet -o -H newc | lzma e -si -so  > ../rootfs.arm.cpio-sq.lzma
-	else
-		find . | cpio --quiet -o -H newc | gzip > ../rootfs.arm.cpio-sq.gz
-	fi
+	find . | cpio --quiet -o -H newc | $cmd > ../rootfs.arm.$TYPE.$EXT
 	cd ..
 	umount tmp
+	rmdir tmp
 	rm rootfs.arm.ext2.tmp
 
-	chown $ME:$MG rootfs.arm.cpio-sq.lzma
+	chown $ME:$MG rootfs.arm.$TYPE.$EXT
 	
-elif test "$1" = "sqmtd"; then
-	# everything squasehed
-	mount -o ro,loop rootfs.arm.ext2 tmp
+elif test "$TYPE" = "sqall"; then # squashfs initrd, everything squashed
 
-	# lzma or gzip
-	COMP=lzma
+	cd ${BLDDIR}/project_build_arm/$board/
 
-	# block sizes: 131072 262144 524288 1048576
-	mksquashfs tmp rootfs.arm.sqmtd -comp $COMP -noappend -b 262144 \
-		-always-use-fragments
+	fw_pkgs_deps=$(for i in $fw_pkgs; do rdeps $i; done | sort -u)
 
-	umount tmp
-	chown $ME:$MG rootfs.arm.sqmtd
+	# create ipkg status file stating which packages are pre installed
+	echo "$fw_pkgs_deps" | sort -u > root/etc/preinst
+	rm -f root/etc/preinst.status
+	for i in $(echo "$fw_pkgs_deps" | cut -d' ' -f1); do
+		deps_check $i
+		deps_status $i
+	done >> root/etc/preinst.status
+
+	rm -f root/dev/null root/dev/console # mksquashfs can create device nodes
+	mksquashfs root rootfs.arm.$TYPE.$EXT -comp $COMP -noappend -b $SQFSBLK \
+		-always-use-fragments -all-root -pf $CWD/mksquashfs.pf
+
+	mv rootfs.arm.sqall.$EXT ${BLDDIR}/binaries/$board
+
+elif test "$TYPE" = "sqsplit"; then # as 'sqall' above but also create sqimage with extra files
+
+# BUG: FIXME kernel-modules is needed for the dns-325 because of forked-daapd
+# (quota-tools doesn't needs it also?)
+# but cryptodev and iscsitarget are not build, so no kernel modules for them exists!
+# the easy solution is to also build those packages for the 325, although seldom used
+# note: cryptodev is a kernel module, set at package config time
+
+	if test "$board" != "dns325"; then
+		echo "initramfs: FIXME: ERROR, this mode is only for a dns325"
+		exit 1
+	fi
+
+	cd ${BLDDIR}/project_build_arm/$board/
+
+	fw_pkgs_deps=$(for i in $fw_pkgs; do rdeps $i; done | sort -u)
+	sq_pkgs_deps=$(for i in $sq_pkgs; do rdeps $i; done | sort -u)
+
+	echo -e "$fw_pkgs_deps" "\n$sq_pkgs_deps" | sort -u > root/etc/preinst
+
+	# create ipkg status file stating which packages are pre installed
+	rm -f root/etc/preinst.status
+	for i in $(echo "$fw_pkgs_deps" | cut -d' ' -f1); do
+		deps_check $i
+		deps_status $i
+	done >> root/etc/preinst.status
+
+	# create sqimage pkgs file list and ipkg status file stating which packages are pre installed
+	TF=$(mktemp)
+	for i in $(echo "$sq_pkgs_deps" | cut -d' ' -f1); do
+		deps_check $i
+		deps_status $i
+		cat $CWD/ipkgfiles/$i.lst >> $TF
+	done >> root/etc/preinst.status 
+
+	# sqimage files list, to be removed from base and present only on sqimage
+	sqimagefiles=$(cat $TF | sort -u)
+	rm $TF
+
+	cp -a root image
+	mkdir -p sqimage
+	cd image
+	# create dirs first, as packages often don't have dirs name on it
+	# and its permission needs to be preserved
+	find . -type d | cpio -p ../sqimage 
+	echo "$sqimagefiles" | cpio -pu ../sqimage
+	rm -f $sqimagefiles >& /dev/null
+	cd ..
+	# remove empty dirs on sqimage (image itself *has* to have them)
+	find sqimage -depth -type d -empty -exec rmdir {} \;
 	
+	rm image/dev/null image/dev/console # mksquashfs can create device nodes
+	mksquashfs image rootfs.arm.sqall.$EXT -comp $COMP -noappend -b $SQFSBLK \
+		-always-use-fragments -all-root -pf $CWD/mksquashfs.pf
+
+	mksquashfs sqimage rootfs.arm.sqimage.$EXT -comp $COMP -noappend -b $SQFSBLK \
+		-always-use-fragments -all-root
+
+	rm -rf image sqimage
+	mv rootfs.arm.sqall.$EXT rootfs.arm.sqimage.$EXT ${BLDDIR}/binaries/$board
+
 else
-	echo "Usage: mkinitramfs gz | lzma | sqfs | sqmtd"
+	usage
 fi
-
-rmdir tmp
 
 
 
