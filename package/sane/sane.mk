d7b19beca30aa24ab1786b98a0bcbb3f1cd57ec3
João Cardoso <joao.fs.cardoso@gmail.com>
2014-03-14 16:57:08 +0000

    sane: update download site; add SANE_WGET_OPTS because download site CA is not recognized

--- package/sane/sane.mk
+++ package/sane/sane.mk
@@ -6,11 +6,16 @@
 
 SANE_VERSION = 1.0.21
 SANE_SOURCE = sane-backends-$(SANE_VERSION).tar.gz
-SANE_SITE = http://alioth.debian.org/frs/download.php/3258/
+SANE_SITE = http://alioth.debian.org/frs/download.php/file/3258
+
+SANE_WGET_OPTS = --no-check-certificate
+
 SANE_AUTORECONF = NO
 SANE_LIBTOOL_PATCH = YES
+
 SANE_INSTALL_STAGING = YES
 SANE_INSTALL_TARGET = YES
+
 SANE_DEPENDENCIES = uclibc libusb jpeg tiff
 
 SANE_CONF_OPT = --disable-latex --disable-translations $(DISABLE_IPV6) --disable-avahi --disable-rpath --with-pic --without-gphoto2
