3cb1024010feb23c03e6447d57d936cc543c3bda
João Cardoso <joao.fs.cardoso@gmail.com>
2014-03-14 16:47:38 +0000

    mpd: update download site

--- package/multimedia/mpd/mpd.mk
+++ package/multimedia/mpd/mpd.mk
@@ -4,9 +4,12 @@
 #
 #############################################################
 
+MPD_MAJOR_VER = 0.17
+MPD_MINOR_VER = 2
 MPD_VERSION = 0.17.2
-MPD_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/project/musicpd/mpd/$(MPD_VERSION)
-MPD_SOURCE = mpd-$(MPD_VERSION).tar.bz2
+
+MPD_SITE = http://www.musicpd.org/download/mpd/$(MPD_MAJOR_VER)
+MPD_SOURCE = mpd-$(MPD_MAJOR_VER).$(MPD_MINOR_VER).tar.bz2
 
 MPD_AUTORECONF = NO
 MPD_INSTALL_STAGING = NO
@@ -14,8 +17,5 @@ MPD_INSTALL_TARGET = YES
 MPD_LIBTOOL_PATCH = NO
 
 MPD_DEPENDENCIES = uclibc flac sqlite libvorbis libogg libmad ffmpeg libcurl libid3tag alsa-lib lame twolame libglib2 avahi
-
-#MPD_CONF_OPT = --with-zeroconf=avahi
-#MPD_CONF_ENV = AVAHI_LIBS="-lavahi-common -lavahi-client" AVAHI_CFLAGS=" "
 
 $(eval $(call AUTOTARGETS,package,mpd))
