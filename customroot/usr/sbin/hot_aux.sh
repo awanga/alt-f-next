e303bff4cf53743f26205ab885f4e0afa90d5c1d
João Cardoso <joao.fs.cardoso@gmail.com>
2014-04-15 15:32:30 +0000

    hot_aux: don't paralelize fsck on disks, as paging takes over and makes fsck slower (FIXME: does this apply to the DNS-320/325 also? They have 128/256MB of memory)

--- customroot/usr/sbin/hot_aux.sh
+++ customroot/usr/sbin/hot_aux.sh
@@ -9,31 +9,17 @@ if test -n "$debug"; then
 	env	
 fi
 
+# don't do paralell fsck (assume at most one fsck is running)
 check() {
-	if test "${MDEV:0:2}" = "md"; then
-		we=$(ls /sys/block/$MDEV/slaves/ | tr -d [0-9]'\n' | \
-			sed -n 's/\(...\)\(...\)/\(\1\|\2\)/p')
-	else
-		we=${MDEV:0:3}
-	fi
-
-	while true; do
-		inuse=""
-		inclean=$(ls /tmp/check-sd[a-z][1-9] /tmp/check-md[0-9] 2> /dev/null | grep -oE '(sd[a-z].|md[0-9])')
-		if test -z "$inclean"; then break; fi
-		for i in "$inclean"; do
-			if test "${i:0:2}" = "md"; then
-				inuse="$inuse $(ls /sys/block/$i/slaves)"
-			else
-				inuse="$inuse $i"
-			fi
-		done
-
-		if ! echo "$inuse" | grep -q -E "$we"; then
-			break
+	while ls /tmp/check-* >& /dev/null; do
+		if kill -0 $(cat /tmp/check-*.pid) 2> /dev/null; then
+			echo hot_aux "$MDEV waiting to be fscked"
+			sleep 10
+		else
+			while ! mkdir /tmp/fsck_lock; do sleep 1; done
+			rm -f /tmp/check-*
+			return
 		fi
-		logger -t hot_aux "$MDEV waiting to be fscked"
-		sleep 10
 	done
 }
 
@@ -49,23 +35,22 @@ mopts=$3
 lbl=$4
 fstype=$5
 
-if test -f $MISCC; then
-	. $MISCC
-fi
+if test -f $MISCC; then . $MISCC; fi
 
 if test "$fsckcmd" != "echo"; then
 	
 	trap "" 1
 
 	check
-	logger -t hot_aux "Start fscking $MDEV"
+	logger -st hot_aux "Start fscking $MDEV"
 
 	xf=/tmp/check-$MDEV
 	logf=${xf}.log
 	pidf=${xf}.pid
 
 	touch $xf
-	echo $$ > $pidf 
+	echo $$ > $pidf
+	rmdir /tmp/fsck_lock 2> /dev/null
 
 	if test "$fsopt" = "-"; then
 		fsopt=""
@@ -80,7 +65,7 @@ if test "$fsckcmd" != "echo"; then
 	else
 		emsg="Finish fscking $MDEV: $res"
 	fi
-	logger -t hot_aux "$emsg"
+	logger -st hot_aux "$emsg"
 	rm -f $xf $logf $pidf 
 
 	if test -z "$(ls /tmp/check-* 2>/dev/null)"; then
@@ -88,7 +73,7 @@ if test "$fsckcmd" != "echo"; then
 	fi
 
 else
-	logger -t hot_aux "No fsck command for $fstype, $MDEV not fscked."
+	logger -st hot_aux "No fsck command for $fstype, $MDEV not fscked."
 fi
 
 # record fstab date, don't change it
@@ -103,7 +88,7 @@ rm /tmp/fstab_date
 
 # don't mount if noauto is present in mount options
 if echo "$mopts" | grep -q noauto; then
-	logger -t hot_aux "Not auto-mounting $lbl as 'noauto' is present in the mount options."
+	logger -st hot_aux "Not auto-mounting $lbl as 'noauto' is present in the mount options."
 	exit 0
 fi
 
@@ -111,32 +96,28 @@ mount $PWD/$MDEV
 
 if test -f /usr/sbin/quotaon; then
 	if echo "$mopts" | grep -qE '(grpjquota|usrjquota)'; then
-		logger -t hot_aux "Activating quotas on $MDEV"
+		logger -st hot_aux "Activating quotas on $MDEV"
 		quotaon -ug $PWD/$MDEV
 	fi
 fi
 
-if test -f "/mnt/$lbl/alt-f.fail"; then
-	rm -f "/mnt/$lbl/alt-f.fail"
-fi
-
 if test -d "/mnt/$lbl/Users"; then
 	if ! test -h /home -a -d "$(readlink -f /home)" ; then
-		logger -t hot_aux "Users directory found in $lbl"
+		logger -st hot_aux "Users directory found in $lbl"
 		ln -s "/mnt/$lbl/Users" /home
 	fi
 fi
 
 if test -d "/mnt/$lbl/Public"; then
 	if ! test -h /Public -a -d "$(readlink -f /Public)" ; then
-		logger -t hot_aux "Public directory found in $lbl"
+		logger -st hot_aux "Public directory found in $lbl"
 		ln -s "/mnt/$lbl/Public" /Public
 	fi
 fi
 
 if test -d "/mnt/$lbl/Backup"; then
 	if ! test -h /Backup -a -d "$(readlink -f /Backup)" ; then
-		logger -t hot_aux "Backup directory found in $lbl"
+		logger -st hot_aux "Backup directory found in $lbl"
 		ln -s "/mnt/$lbl/Backup" /Backup
 	fi
 fi
@@ -144,17 +125,17 @@ fi
 if test -n "$USER_SCRIPT" -a ! -f $USERLOCK; then
 	if test "/mnt/$lbl" = "$(dirname $USER_SCRIPT)" -a -x "/mnt/$lbl/$(basename $USER_SCRIPT)"; then
 		touch $USERLOCK
-		logger -t hot_aux "Executing \"$USER_SCRIPT start\" in background"
+		logger -st hot_aux "Executing \"$USER_SCRIPT start\" in background"
 		$USER_SCRIPT start &
 	fi
 fi
 
 if test -d "/mnt/$lbl/ffp"; then
 	if ! test -h /ffp -a -d "$(readlink -f /ffp)" ; then
-		logger -t hot_aux "ffp directory found in $lbl"
+		logger -st hot_aux "ffp directory found in $lbl"
 		ln -s "/mnt/$lbl/ffp" /ffp
 		if test $? = 0 -a -x /etc/init.d/S??ffp; then
-				/etc/init.d/S??ffp start
+			rcffp start
 		fi
 	fi
 fi
@@ -162,7 +143,7 @@ fi
 if test -d /mnt/$lbl/Alt-F; then
 	if test "$mopts" != "ro"; then
 		if ! test -h /Alt-F -a -d "$(readlink -f /Alt-F)"; then
-			logger -t hot_aux "Alt-F directory found in $lbl"
+			logger -st hot_aux "Alt-F directory found in $lbl"
 			rm -f /mnt/$lbl/Alt-F/Alt-F /mnt/$lbl/Alt-F/ffp /mnt/$lbl/Alt-F/home
 			ln -s /mnt/$lbl/Alt-F /Alt-F
 			echo "DON'T ADD, REMOVE OR MODIFY ANY FILE IN THIS DIRECTORY
@@ -183,6 +164,6 @@ OR ANY OF ITS SUB-DIRECTORIES, OR THE SYSTEM MIGHT HANG!" > /Alt-F/README.txt
 			done
 		fi
 	else
-		logger -t hot_aux "Alt-F directory found in $lbl but not used, as fs is read-only!"
+		logger -st hot_aux "Alt-F directory found in $lbl but not used, as fs is read-only!"
 	fi
 fi
