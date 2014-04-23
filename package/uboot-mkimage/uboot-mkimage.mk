71b5a255dab0a88ea9c765e1233b44b86c5e94c9
João Cardoso <joao.fs.cardoso@gmail.com>
2014-04-23 16:38:26 +0000

    uboot-mkimage: build zlib-host first, as it is needed, might be missing on the host or be of a different version

--- package/uboot-mkimage/uboot-mkimage.mk
+++ package/uboot-mkimage/uboot-mkimage.mk
@@ -8,7 +8,8 @@ UBOOT_MKIMAGE_VERSION = 0.4
 UBOOT_MKIMAGE_SOURCE = uboot-mkimage_$(UBOOT_MKIMAGE_VERSION).tar.gz
 UBOOT_MKIMAGE_SITE = http://ftp.de.debian.org/debian/pool/main/u/uboot-mkimage
 
-UBOOT_MKIMAGE_DEPENDENCIES = uclibc 
+UBOOT_MKIMAGE_HOST_DEPENDENCIES = uclibc zlib-host
+UBOOT_MKIMAGE_HOST_MAKE_ENV = CC="$(HOSTCC) $(HOST_CFLAGS)"
 
 $(eval $(call AUTOTARGETS_HOST,package,uboot-mkimage))
 
