b5c3b8940334c39ab37fddec1a00e8784b312a61
João Cardoso <joao.fs.cardoso@gmail.com>
2014-04-23 16:41:33 +0000

    git: make it depend on perl-host and pcre

--- package/git/git.mk
+++ package/git/git.mk
@@ -8,12 +8,22 @@ GIT_SOURCE = git-$(GIT_VERSION).tar.gz
 GIT_SITE = http://git-core.googlecode.com/files/
 GIT_INSTALL_STAGING = NO
 GIT_INSTALL_TARGET = YES
-GIT_DEPENDENCIES = uclibc openssl libcurl expat perl
+GIT_DEPENDENCIES = uclibc openssl libcurl expat pcre perl-host
 
-GIT_CONF_ENV = ac_cv_c_c99_format=no ac_cv_fread_reads_directories=yes ac_cv_snprintf_returns_bogus=yes 
+GIT_CONF_ENV = ac_cv_fread_reads_directories=yes \
+	ac_cv_snprintf_returns_bogus=yes 
 
-GIT_CONF_OPT = --without-python --without-tcltk
+GIT_CONF_OPT = --without-python --without-tcltk \
+	-with-libpcre --with-perl=$(HOST_DIR)/usr/bin/perl
+
+#GIT_MAKE_ENV = NO_PERL=no
 
 GIT_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
 $(eval $(call AUTOTARGETS,package,git))
+
+$(GIT_HOOK_POST_INSTALL):
+	for i in $$(grep -lr "$(HOST_DIR)/usr/bin/perl" $(TARGET_DIR)/usr/lib/git-core); do \
+		sed -i "s|$(HOST_DIR)/usr/bin/perl|/usr/bin/perl|" $$i; \
+	done
+	touch $@
