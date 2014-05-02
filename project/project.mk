86d4aec359dc7068c3340d660877d2a1eb102abe
João Cardoso <joao.fs.cardoso@gmail.com>
2014-05-02 16:03:30 +0000

    project.mk: don't edit project config files (busybox/uclibc) if they don't exist

--- project/project.mk
+++ project/project.mk
@@ -25,22 +25,19 @@ saveconfig:
 	if [ -f $(LINUX26_DIR)/.config ]; then \
 		cp $(LINUX26_DIR)/.config \
 			$(LOCAL)/$(PROJECT)/linux-$(LINUX26_VERSION).config; \
-		$(SED) '/BR2_PACKAGE_LINUX_KCONFIG/d' $(PROJECT_FILE); \
-		echo "BR2_PACKAGE_LINUX_KCONFIG=\"$(LOCAL)/$(PROJECT)/linux-$(LINUX26_VERSION).config\"" >> $(PROJECT_FILE); \
-		$(SED) '/BR2_BOARD_PATH/d' $(PROJECT_FILE); \
-		echo "BR2_BOARD_PATH=\"$(LOCAL)/$(PROJECT)\"" >> $(PROJECT_FILE); \
+		$(SED) 's|BR2_PACKAGE_LINUX_KCONFIG=.*|BR2_PACKAGE_LINUX_KCONFIG=\"$(LOCAL)/$(PROJECT)/linux-$(LINUX26_VERSION).config\"|' $(PROJECT_FILE); \
+		#$(SED) '/BR2_BOARD_PATH/d' $(PROJECT_FILE); \
+		#echo "BR2_BOARD_PATH=\"$(LOCAL)/$(PROJECT)\"" >> $(PROJECT_FILE); \
 	fi
-	if [ -f $(BUSYBOX_DIR)/.config ]; then \
+	if [ -f $(BUSYBOX_DIR)/.config -a -f $(LOCAL)/$(PROJECT)/busybox-$(BUSYBOX_VERSION).config ]; then \
 		cp $(BUSYBOX_DIR)/.config \
 			$(LOCAL)/$(PROJECT)/busybox-$(BUSYBOX_VERSION).config; \
-		$(SED) '/BR2_PACKAGE_BUSYBOX_CONFIG/d' $(PROJECT_FILE); \
-		echo "BR2_PACKAGE_BUSYBOX_CONFIG=\"$(LOCAL)/$(PROJECT)/busybox-$(BUSYBOX_VERSION).config\"" >> $(PROJECT_FILE); \
+		$(SED) 's|BR2_PACKAGE_BUSYBOX_CONFIG=.*|BR2_PACKAGE_BUSYBOX_CONFIG=\"$(LOCAL)/$(PROJECT)/busybox-$(BUSYBOX_VERSION).config\"|' $(PROJECT_FILE); \
 	fi
-	if [ -f $(UCLIBC_DIR)/.config ]; then \
+	if [ -f $(UCLIBC_DIR)/.config -a -f $(LOCAL)/$(PROJECT)/uclibc-$(UCLIBC_VER).config ]; then \
 		cp $(UCLIBC_DIR)/.config \
 			$(LOCAL)/$(PROJECT)/uclibc-$(UCLIBC_VER).config; \
-		$(SED) '/BR2_UCLIBC_CONFIG/d' $(PROJECT_FILE); \
-		echo "BR2_UCLIBC_CONFIG=\"$(LOCAL)/$(PROJECT)/uclibc-$(UCLIBC_VER).config\"" >> $(PROJECT_FILE); \
+		$(SED) 's|BR2_UCLIBC_CONFIG=.*|BR2_UCLIBC_CONFIG=\"$(LOCAL)/$(PROJECT)/uclibc-$(UCLIBC_VER).config\"|' $(PROJECT_FILE); \
 	fi
 	if [ -f $(UBOOT_DIR)/include/configs/$(PROJECT).h ]; then \
 		mkdir -p $(LOCAL)/$(PROJECT)/u-boot; \
