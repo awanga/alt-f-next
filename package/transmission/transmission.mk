5810819edd2241b0cca13fafa0704185b155f4b6
João Cardoso <joao.fs.cardoso@gmail.com>
2014-05-21 14:37:49 +0000

    transmission: bump to 2.83

--- package/transmission/transmission.mk
+++ package/transmission/transmission.mk
@@ -4,17 +4,17 @@
 #
 #############################################################
 
-TRANSMISSION_VERSION = 2.82
+TRANSMISSION_VERSION = 2.83
 TRANSMISSION_SOURCE = transmission-$(TRANSMISSION_VERSION).tar.xz
 TRANSMISSION_SITE = http://download.m0k.org/transmission/files
+
 TRANSMISSION_AUTORECONF = NO
 TRANSMISSION_INSTALL_STAGING = NO
 TRANSMISSION_INSTALL_TARGET = YES
 TRANSMISSION_LIBTOOL_PATCH = NO
-TRANSMISSION_CONF_OPT = --disable-nls --disable-gtk --disable-gconf2 --enable-utp
 
 TRANSMISSION_DEPENDENCIES = uclibc libcurl openssl libevent2 pkg-config
-
+TRANSMISSION_CONF_OPT = --disable-nls --disable-gtk --disable-gconf2 --enable-utp
 TRANSMISSION_CONF_ENV = LIBEVENT_CFLAGS="-I$(STAGING_DIR)/libevent2/include" \
 	LIBEVENT_LIBS="-L$(STAGING_DIR)/libevent2/lib -levent2" ac_cv_prog_HAVE_CXX=yes
 
