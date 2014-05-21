61ca342bf6c723f5c4c09ac355ed3dfb2746bb23
João Cardoso <joao.fs.cardoso@gmail.com>
2014-05-21 18:18:16 +0000

    texinfo: new host package. Version 4.13 is REQUIRED by the toolchain (gcc-4.3.3). As some distros ship texinfo 5.x, this is a prerequisite, and HOST_DIR is put ahead of PATH in order to override system-installed versions

---
+++ package/texinfo/texinfo.mk
@@ -0,0 +1,16 @@
+#############################################################
+#
+# texinfo
+#
+#############################################################
+
+# version 4.13 is REQUIRED by the toolchain (gcc-4.3.3)
+
+TEXINFO_VERSION = 4.13a
+TEXINFO_SITE = http://ftp.gnu.org/gnu/texinfo
+TEXINFO_SOURCE = texinfo-$(TEXINFO_VERSION).tar.gz
+
+TEXINFO_AUTORECONF = NO
+TEXINFO_LIBTOOL_PATCH = NO
+
+$(eval $(call AUTOTARGETS_HOST,package,texinfo))
