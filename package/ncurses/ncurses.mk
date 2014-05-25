a9e581f1d11a4469ec52fda9bfe57081d4680eb6
João Cardoso <joao.fs.cardoso@gmail.com>
2014-05-25 17:06:12 +0000

    ncurses-host: use shared libs

--- package/ncurses/ncurses.mk
+++ package/ncurses/ncurses.mk
@@ -152,6 +152,8 @@ $(TARGET_DIR)/usr/lib/libncurses.a: $(NCURSES_DIR)/lib/libncurses.a
 	-$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/usr/lib/libncurses.so.$(NCURSES_VERSION)
 	touch -c $@
 
+NCURSES_HOST_CONF_OPT = --with-shared
+
 $(eval $(call AUTOTARGETS_HOST,package,ncurses))
 
 ncurses: ncurses-host $(TARGET_DIR)/usr/lib/libncurses.so.$(NCURSES_VERSION)
