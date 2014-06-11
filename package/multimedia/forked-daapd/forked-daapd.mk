c49a610183876225b377df65a7d65551fa313ce6
João Cardoso <joao.fs.cardoso@gmail.com>
2014-06-11 01:37:00 +0000

    forked-daapd: add gperf-host dependency

--- package/multimedia/forked-daapd/forked-daapd.mk
+++ package/multimedia/forked-daapd/forked-daapd.mk
@@ -19,7 +19,7 @@ FORKED_DAAPD_LIBTOOL_PATCH = NO
 FORKED_DAAPD_INSTALL_STAGING = NO
 FORKED_DAAPD_INSTALL_TARGET = YES
 
-FORKED_DAAPD_DEPENDENCIES = uclibc zlib libavl libunistring libevent libgcrypt libconfuse mxml alsa-lib libantlr sqlite avahi ffmpeg taglib flac
+FORKED_DAAPD_DEPENDENCIES = uclibc zlib libavl libunistring libevent libgcrypt libconfuse mxml alsa-lib libantlr sqlite avahi ffmpeg taglib flac gperf-host
 
 FORKED_DAAPD_CONF_ENV = ZLIB_LIBS="-lz" ZLIB_CFLAGS=" " LIBS="-lstdc++"
 FORKED_DAAPD_CONF_OPT = -localstatedir=/var --sysconfdir=/etc \
