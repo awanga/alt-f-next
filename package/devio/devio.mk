92d45ed35737cde191d72817e0f4c92d7e0c4352
João Cardoso <joao.fs.cardoso@gmail.com>
2014-06-11 01:29:50 +0000

    devio: fix download URL

--- package/devio/devio.mk
+++ package/devio/devio.mk
@@ -5,7 +5,7 @@
 #############################################################
 
 DEVIO_VERSION = 1.2
-DEVIO_SITE = $(BR2_SOURCEFORGE_MIRROR)/devio
+DEVIO_SITE = $(BR2_SOURCEFORGE_MIRROR)/project/devio/devio/devio-$(DEVIO_VERSION)
 DEVIO_SOURCE = devio-$(DEVIO_VERSION).tar.gz
 
 DEVIO_DEPENDENCIES = uclibc 
