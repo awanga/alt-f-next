88a2af7d08b8c95473a2c1befddcce2fcb2bdf70
João Cardoso <joao.fs.cardoso@gmail.com>
2014-06-11 01:41:20 +0000

    quota-tools: use --enable-strip-binaries=no, as it tries to use the system 'strip', not arm-linix-strip

--- package/quota-tools/quota-tools.mk
+++ package/quota-tools/quota-tools.mk
@@ -15,7 +15,7 @@ QUOTA_TOOLS_LIBTOOL_PATCH = NO
 QUOTA_TOOLS_DEPENDENCIES = e2fsprogs
 
 QUOTA_TOOLS_INSTALL_TARGET_OPT =  ROOTDIR=$(TARGET_DIR) install
-QUOTA_TOOLS_CONF_OPT = --enable-nls=no 
+QUOTA_TOOLS_CONF_OPT = --enable-nls=no --enable-strip-binaries=no
 QUOTA_TOOLS_MAKE_ENV = CC="$(TARGET_CC)"
 
 $(eval $(call AUTOTARGETS,package,quota-tools))
