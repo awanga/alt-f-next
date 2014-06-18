076960e5327d549135c25533023bcc00438c8482
João Cardoso <joao.fs.cardoso@gmail.com>
2014-06-18 18:21:46 +0000

    a2ps: add gperf-host dependency

--- package/a2ps/a2ps.mk
+++ package/a2ps/a2ps.mk
@@ -8,7 +8,7 @@ A2PS_SOURCE = a2ps-$(A2PS_VERSION).tar.gz
 A2PS_SITE = $(BR2_GNU_MIRROR)/a2ps
 A2PS_INSTALL_STAGING = YES
 A2PS_INSTALL_TARGET = YES
-A2PS_DEPENDENCIES = uclibc file
+A2PS_DEPENDENCIES = uclibc gperf-host file
 
 A2PS_CONF_ENV = ac_cv_prog_EMACS=no ac_cv_path_file_prog=/usr/bin/file
 
