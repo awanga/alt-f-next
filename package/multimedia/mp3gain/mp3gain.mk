d2d9707e1426b37f375681d13c984e5aed00ba27
João Cardoso <joao.fs.cardoso@gmail.com>
2014-06-18 20:30:07 +0000

    mp3gain: check unzip existence

--- package/multimedia/mp3gain/mp3gain.mk
+++ package/multimedia/mp3gain/mp3gain.mk
@@ -5,10 +5,9 @@
 ################################################################################
 
 MP3GAIN_VERSION = 1.5.2
-MP3GAIN_VERSION2 = 1_5_2_r2
-#MP3GAIN_DIR = $(MP3GAIN_VERSION2)
-MP3GAIN_SOURCE = mp3gain-$(MP3GAIN_VERSION2)-src.zip
+MP3GAIN_SOURCE = mp3gain-$(MP3GAIN_VERSION2)_r2-src.zip
 MP3GAIN_SITE = $(BR2_SOURCEFORGE_MIRROR)/project/mp3gain/mp3gain/$(MP3GAIN_VERSION)
+
 MP3GAIN_AUTORECONF = NO
 MP3GAIN_INSTALL_TARGET = YES
 MP3GAIN_INSTALL_STAGING = NO
@@ -16,6 +15,7 @@ MP3GAIN_INSTALL_STAGING = NO
 $(eval $(call AUTOTARGETS,package/multimedia,mp3gain))
 
 $(MP3GAIN_TARGET_EXTRACT):
+	if ! which unzip; then /bin/echo -e "\n\nYou must install 'unzip' on your build machine\n"; fi
 	unzip $(DL_DIR)/$(MP3GAIN_SOURCE) -d $(MP3GAIN_DIR)
 	touch $@
 
@@ -29,6 +29,3 @@ $(MP3GAIN_TARGET_BUILD):
 $(MP3GAIN_TARGET_INSTALL_TARGET):
 	$(MAKE) CC=$(TARGET_CC) CFLAGS="$(TARGET_CFLAGS)" INSTALL_PATH="$(TARGET_DIR)/usr/bin" -C $(MP3GAIN_DIR) install
 	touch $@
-
-#$(MP3GAIN_HOOK_POST_INSTALL):
-#	touch $@
