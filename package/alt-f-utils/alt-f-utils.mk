db29a4866b480b3b192869bc2d8d5d78762192d4
João Cardoso <joao.fs.cardoso@gmail.com>
2014-06-24 15:44:53 +0000

    alt-f-utils.mk: build fixes/dependencies

--- package/alt-f-utils/alt-f-utils.mk
+++ package/alt-f-utils/alt-f-utils.mk
@@ -16,30 +16,33 @@ ALT_F_UTILS_INSTALL_TARGET := YES
 
 ALT_F_UTILS_DEPENDENCIES := uclibc
 
-$(DL_DIR)/$(ALT_F_UTILS_SOURCE): package/alt-f-utils/alt-f-utils-$(ALT_F_UTILS_VERSION)/sysctrl.c package/alt-f-utils/alt-f-utils-$(ALT_F_UTILS_VERSION)/dns323-fw.c
+ALT_F_UTILS_MAKE_ENV := CC="$(TARGET_CC) $(TARGET_CFLAGS) -D_GNU_SOURCE" STRIP="$(TARGET_STRIP)"
+ALT_F_UTILS_HOST_MAKE_ENV := STRIP=true
+
+$(DL_DIR)/$(ALT_F_UTILS_SOURCE): package/alt-f-utils/alt-f-utils-$(ALT_F_UTILS_VERSION)/sysctrl.c \
+		package/alt-f-utils/alt-f-utils-$(ALT_F_UTILS_VERSION)/dns323-fw.c \
+		package/alt-f-utils/alt-f-utils-$(ALT_F_UTILS_VERSION)/dns320l-daemon.c
 	(cd package/alt-f-utils/; \
-		rm *~; \
+		rm alt-f-utils-$(ALT_F_UTILS_VERSION)/*~; \
 		$(TAR) --exclude-vcs -cvzf $(ALT_F_UTILS_SOURCE) alt-f-utils-$(ALT_F_UTILS_VERSION); \
 		cp  $(ALT_F_UTILS_SOURCE) $(DL_DIR) \
 	)
+	-make alt-f-utils-dirclean alt-f-utils-host-dirclean
+	touch $@
 
-ifeq ($(BR2_PACKAGE_ALT_F_UTILS_TARGET),y)
-ALT_F_UTILS_MAKE_ENV := CC="$(TARGET_CC) $(TARGET_CFLAGS)" STRIP="$(TARGET_STRIP)"
 $(eval $(call AUTOTARGETS,package,alt-f-utils))
-endif
 
-ifeq ($(BR2_PACKAGE_ALT_F_UTILS_HOST),y)
-ALT_F_UTILS_HOST_MAKE_ENV := STRIP=true
 $(eval $(call AUTOTARGETS_HOST,package,alt-f-utils))
 
 $(ALT_F_UTILS_HOST_HOOK_POST_INSTALL):
 	mkdir -p $(HOST_DIR)/usr/bin
 	( cd $(HOST_DIR)/usr/sbin/; \
-		rm rpcinfo sysctrl; \
+		rm rpcinfo sysctrl dns320l-daemon; \
 		mv dns323-fw ../bin \
 	)
 	touch $@
 
-endif
+$(ALT_F_UTILS_TARGET_CONFIGURE) $(ALT_F_UTILS_HOST_CONFIGURE):
+	touch $@
 
-$(ALT_F_UTILS_TARGET_SOURCE): $(DL_DIR)/$(ALT_F_UTILS_SOURCE)
+$(ALT_F_UTILS_TARGET_SOURCE) $(ALT_F_UTILS_HOST_SOURCE): $(DL_DIR)/$(ALT_F_UTILS_SOURCE)
