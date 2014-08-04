aeaf23b9deb4e7c6053d8b3e66f33ffdee33f755
João Cardoso <joao.fs.cardoso@gmail.com>
2014-08-04 13:44:51 +0000

    mt-daapd: supply right configure env instead of patching source

--- package/multimedia/mt-daapd/mt-daapd.mk
+++ package/multimedia/mt-daapd/mt-daapd.mk
@@ -3,22 +3,19 @@
 # mt-daapd
 #
 #############################################################
+
 MT_DAAPD_VERSION = 0.2.4.2
 MT_DAAPD_SOURCE = mt-daapd-$(MT_DAAPD_VERSION).tar.gz
 MT_DAAPD_SITE = $(BR2_SOURCEFORGE_MIRROR)/project/mt-daapd/mt-daapd/$(MT_DAAPD_VERSION)
+
 MT_DAAPD_AUTORECONF = NO
 MT_DAAPD_INSTALL_STAGING = NO
 MT_DAAPD_INSTALL_TARGET = YES
 MT_DAAPD_LIBTOOL_PATCH = YES
-MT_DAAPD_CONF_ENV = CFLAGS+=-DSETPGRP_VOID
 
-MT_DAAPD_CONF_OPT = --disable-avahi --enable-oggvorbis
-	
 MT_DAAPD_DEPENDENCIES = uclibc gdbm libid3tag libvorbis
 
-$(eval $(call AUTOTARGETS,package/multimedia,mt-daapd))
+MT_DAAPD_CONF_ENV = ac_cv_func_setpgrp_void=yes
+MT_DAAPD_CONF_OPT = --disable-avahi --enable-oggvorbis
 
-$(MT_DAAPD_HOOK_POST_EXTRACT):
-	sed -i 's/^AC_FUNC_SETPGRP/dnl AC_FUNC_SETPGRP/' $(MT_DAAPD_DIR)/configure.in
-	(cd $(MT_DAAPD_DIR); aclocal; autoconf; automake --add-missing)
-	touch $@
+$(eval $(call AUTOTARGETS,package/multimedia,mt-daapd))
