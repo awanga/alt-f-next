96cbac2f5b0799f9299ae08b7b525d6d32b50d0e
João Cardoso <joao.fs.cardoso@gmail.com>
2014-08-11 01:18:17 +0000

    parted: bump to 3.2

--- package/parted/parted.mk
+++ package/parted/parted.mk
@@ -3,15 +3,21 @@
 # parted
 #
 #############################################################
-PARTED_VERSION:=2.2
-PARTED_SOURCE:=parted-$(PARTED_VERSION).tar.gz
+
+PARTED_VERSION:=3.2
+PARTED_SOURCE:=parted-$(PARTED_VERSION).tar.xz
 PARTED_SITE:=$(BR2_GNU_MIRROR)/parted
+
 PARTED_DIR:=$(BUILD_DIR)/parted-$(PARTED_VERSION)
+
 PARTED_INSTALL_STAGING = NO
 PARTED_LIBTOOL_PATCH = NO
 
-PARTED_DEPENDENCIES = uclibc libuuid
+PARTED_DEPENDENCIES = uclibc libuuid libiconv
 
-PARTED_CONF_OPT = --disable-device-mapper --disable-dynamic-loading --without-readline --disable-shared
+PARTED_CONF_OPT = --disable-device-mapper --without-readline \
+	--disable-shared --disable-dynamic-loading 
+	
+PARTED_CONF_ENV = LIBS=-liconv
 
 $(eval $(call AUTOTARGETS,package,parted))
