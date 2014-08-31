206a74e8ed25a686dcb4b6ac940dfe4c7fa2d925
João Cardoso <joao.fs.cardoso@gmail.com>
2014-08-31 22:07:06 +0000

    nano: bump to 2.2.6; make it depends on groff-host

--- package/editors/nano/nano.mk
+++ package/editors/nano/nano.mk
@@ -4,12 +4,12 @@
 #
 #############################################################
 
-NANO_VERSION:=2.2.4
+NANO_VERSION:=2.2.6
 NANO_SOURCE:=nano-$(NANO_VERSION).tar.gz
 NANO_SITE:=http://www.nano-editor.org/dist/v2.2/
 
 NANO_CAT:=$(ZCAT)
-NANO_DEPENDENCIES = ncurses
+NANO_DEPENDENCIES = ncurses groff-host
 
 NANO_CONF_OPT = --program-prefix=''
 
