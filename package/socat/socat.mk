45c9e831759ad4af5085baf49bc0a92a2bfc97f4
João Cardoso <joao.fs.cardoso@gmail.com>
2014-09-22 14:33:36 +0000

    socat: new package -- Multipurpose socket relay program closes ticket 33 https://sourceforge.net/p/alt-f/packagerequest/33/

--- package/socat/socat.mk
+++ package/socat/socat.mk
@@ -4,104 +4,38 @@
 #
 #############################################################
 
-#SOCAT_MAJOR_VERSION=1.4
-#SOCAT_MINOR_VERSION=.0.3
-SOCAT_MAJOR_VERSION=2.0.0-b2
-SOCAT_MINOR_VERSION=
+SOCAT_VERSION=2.0.0-b7
+SOCAT_SOURCE=socat-$(SOCAT_VERSION).tar.bz2
+SOCAT_SITE=http://www.dest-unreach.org/socat/download
 
-# Don't alter below this line unless you (think) you know
-# what you are doing! Danger, Danger!
+SOCAT_DEPENDENCIES = host-autoconf
 
-SOCAT_SOURCE=socat-$(SOCAT_MAJOR_VERSION)$(SOCAT_MINOR_VERSION).tar.bz2
-SOCAT_CAT:=$(BZCAT)
-SOCAT_SITE=http://www.dest-unreach.org/socat/download/
-#SOCAT_DIR=$(BUILD_DIR)/${shell basename $(SOCAT_SOURCE) .tar.bz2}
-SOCAT_DIR=$(BUILD_DIR)/socat-$(SOCAT_MAJOR_VERSION)
-#SOCAT_WORKDIR=$(BUILD_DIR)/socat_workdir
-SOCAT_WORKDIR=$(SOCAT_DIR)
+SOCAT_CONF_ENV = sc_cv_termios_ispeed=no \
+		 sc_cv_sys_crdly_shift=9 \
+		 sc_cv_sys_tabdly_shift=11 \
+		 sc_cv_sys_csize_shift=4
 
-# SOCAT Configure opts taken from Config/config.Linux-2.6.24.h
-CRDLY_SHIFT := $(strip $(subst ",,$(BR2_PACKAGE_SOCAT_PREDEF_CRDLY_SHIFT)))
-#"))
-TABDLY_SHIFT := $(strip $(subst ",,$(BR2_PACKAGE_SOCAT_PREDEF_TABDLY_SHIFT)))
-#"))
-CSIZE_SHIFT := $(strip $(subst ",,$(BR2_PACKAGE_SOCAT_PREDEF_CSIZE_SHIFT)))
-#"))
+SOCAT_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
-SOCAT_CONFIGURE_OPTS :=
-ifneq ($(CRDLY_SHIFT),)
-SOCAT_CONFIGURE_OPTS += sc_cv_sys_crdly_shift=$(CRDLY_SHIFT)
-endif
-ifneq ($(TABDLY_SHIFT),)
-SOCAT_CONFIGURE_OPTS += sc_cv_sys_tabdly_shift=$(TABDLY_SHIFT)
-endif
-ifneq ($(CRDLY_SHIFT),)
-SOCAT_CONFIGURE_OPTS += sc_cv_sys_csize_shift=$(CSIZE_SHIFT)
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+	SOCAT_DEPENDENCIES += openssl
+else
+	SOCAT_CONF_OPT += --disable-openssl
 endif
 
-$(DL_DIR)/$(SOCAT_SOURCE):
-	$(call DOWNLOAD,$(SOCAT_SITE),$(SOCAT_SOURCE))
+ifeq ($(BR2_PACKAGE_READLINE),y)
+	SOCAT_DEPENDENCIES += readline
+else
+	SOCAT_CONF_OPT += --disable-readline
+endif
 
-$(SOCAT_DIR)/.unpacked: $(DL_DIR)/$(SOCAT_SOURCE)
-	$(SOCAT_CAT) $(DL_DIR)/$(SOCAT_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	toolchain/patch-kernel.sh $(SOCAT_DIR) package/socat/ socat-$(SOCAT_MAJOR_VERSION)\*.patch
-	touch $(SOCAT_DIR)/.unpacked
+$(eval $(call AUTOTARGETS,package,socat))
 
-$(SOCAT_WORKDIR)/Makefile: $(SOCAT_DIR)/.unpacked
-	rm -f $(SOCAT_WORKDIR)/Makefile
-	mkdir -p $(SOCAT_WORKDIR)
-	(cd $(SOCAT_WORKDIR); rm -rf config.cache; \
-		$(TARGET_CONFIGURE_OPTS) \
-		$(TARGET_CONFIGURE_ARGS) \
-		$(SOCAT_CONFIGURE_OPTS) \
-		$(SOCAT_DIR)/configure \
-		--target=$(GNU_TARGET_NAME) \
-		--host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) \
-		--prefix=/usr \
-		--exec-prefix=/usr \
-		--bindir=/usr/bin \
-		--sbindir=/usr/sbin \
-		--libdir=/lib \
-		--libexecdir=/usr/lib \
-		--sysconfdir=/etc \
-		--datadir=/usr/share \
-		--localstatedir=/var \
-		--mandir=/usr/man \
-		--infodir=/usr/info \
-		--disable-termios \
-		$(DISABLE_NLS); \
-	$(SED) 's/#define HAVE_TERMIOS_ISPEED 1/#undef HAVE_TERMIOS_ISPEED/g' config.h; \
-	)
+# We need to run autoconf to regenerate the configure script, in order
+# to ensure that the test checking linux/ext2_fs.h works
+# properly. However, the package only uses autoconf and not automake,
+# so we can't use the normal autoreconf logic.
 
-#	cp $(SOCAT_WORKDIR)/Config/config.Linux-2-6*.h $(SOCAT_WORKDIR)/config.h ; \
-
-
-$(SOCAT_WORKDIR)/socat: $(SOCAT_WORKDIR)/Makefile
-	rm -f $@
-	$(MAKE) -C $(SOCAT_WORKDIR)
-
-$(TARGET_DIR)/usr/bin/socat: $(SOCAT_WORKDIR)/socat
-	mkdir -p $(TARGET_DIR)/usr/man/man1
-	$(MAKE) -C $(SOCAT_WORKDIR) install prefix=$(TARGET_DIR)/usr DESTDIR=$(TARGET_DIR)
+$(SOCAT_HOOK_POST_EXTRACT):
+	(cd $(SOCAT_DIR); $(HOST_DIR)/usr/bin/autoconf)
 	touch $@
-
-socat: uclibc $(TARGET_DIR)/usr/bin/socat
-
-socat-source: $(DL_DIR)/$(SOCAT_SOURCE)
-
-socat-clean:
-	-$(MAKE) -C $(SOCAT_WORKDIR) clean
-	rm -f $(TARGET_DIR)/usr/bin/{socat,filan,procan}
-
-socat-dirclean:
-	rm -rf $(SOCAT_DIR) $(SOCAT_WORKDIR)
-
-#############################################################
-#
-# Toplevel Makefile options
-#
-#############################################################
-ifeq ($(BR2_PACKAGE_SOCAT),y)
-TARGETS+=socat
-endif
