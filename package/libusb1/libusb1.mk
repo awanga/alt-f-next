134102fb623a53002d200a7d2e7ba4a19b7ad75b
João Cardoso <joao.fs.cardoso@gmail.com>
2014-10-21 17:30:44 +0000

    libusb1: new package -- the 1.0 version of the Userspace library for accessing USB devices

---
+++ package/libusb1/libusb1.mk
@@ -0,0 +1,17 @@
+#############################################################
+#
+# libusb1
+#
+#############################################################
+
+LIBUSB1_VERSION:=1.0.19
+LIBUSB1_SOURCE:=libusb-$(LIBUSB1_VERSION).tar.bz2
+LIBUSB1_SITE:= $(BR2_SOURCEFORGE_MIRROR)/project/libusb/libusb-1.0/libusb-$(LIBUSB1_VERSION)
+
+LIBUSB1_LIBTOOL_PATCH = NO
+LIBUSB1_INSTALL_STAGING = YES
+
+LIBUSB1_CONF_OPT = --disable-udev --disable-static --enable-system-log
+LIBUSB1_CONF_NV = CFLAGS
+
+$(eval $(call AUTOTARGETS,package,libusb1))
