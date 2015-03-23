b5162c7c1430310a4e0da23a17c4f6fc0337b86b
João Cardoso <joao.fs.cardoso@gmail.com>
2015-03-23 19:01:57 +0000

    librsync: new package

---
+++ package/librsync/librsync.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# librsync
+#
+################################################################################
+
+LIBRSYNC_VERSION = 0.9.7
+LIBRSYNC_SOURCE = librsync-$(LIBRSYNC_VERSION).tar.gz
+LIBRSYNC_SITE = $(BR2_SOURCEFORGE_MIRROR)/project/librsync/librsync/$(LIBRSYNC_VERSION)
+
+LIBRSYNC_INSTALL_STAGING = YES
+LIBRSYNC_INSTALL_TARGET = YES
+
+LIBRSYNC_DEPENDENCIES = zlib bzip2 popt
+LIBRSYNC_CONF_OPT = --disable-static --enable-shared
+
+$(eval $(call AUTOTARGETS,package,librsync))
+
+$(LIBRSYNC_HOOK_POST_INSTALL):
+	$(RM) $(TARGET_DIR)/usr/bin/rdiff
+	$(RM) $(STAGING_DIR)/usr/bin/rdiff
+	touch $@
