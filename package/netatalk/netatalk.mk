6648d8d14f55c94e4f7f1eaac9c41977f5b14e1d
João Cardoso <joao.fs.cardoso@gmail.com>
2015-06-19 16:50:04 +0000

    netatalk: bump to 3.1.7; use libevent2 new location; make it depends on acl

--- package/netatalk/netatalk.mk
+++ package/netatalk/netatalk.mk
@@ -4,42 +4,33 @@
 #
 #############################################################
 
-NETATALK_VERSION = 3.1.2
+NETATALK_VERSION = 3.1.7
 NETATALK_SOURCE = netatalk-$(NETATALK_VERSION).tar.bz2
 NETATALK_SITE = $(BR2_SOURCEFORGE_MIRROR)/project/netatalk/netatalk/$(NETATALK_VERSION)
 
 NETATALK_LIBTOOL_PATCH = NO
 NETATALK_AUTORECONF = NO
 
-NETATALK_INSTALL_STAGING = NO
-NETATALK_INSTALL_TARGET = YES
-        
 NETATALK_CONF_OPT = \
-		--localstatedir=/var/lib \
-		--with-sysroot=$(STAGING_DIR)/usr \
-		--with-ssl-dir=$(STAGING_DIR)/usr \
-		--with-libiconv=$(STAGING_DIR)/usr \
-		--with-libgcrypt-dir=$(STAGING_DIR)/usr \
-		--with-bdb=$(STAGING_DIR)/usr \
-		--with-libevent-header=$(STAGING_DIR)/libevent2/include \
-		--with-libevent-lib=$(STAGING_DIR)/libevent2/lib \
-		--enable-zeroconf \
-		--disable-cups \
-		--without-acls \
-		--without-pam \
-		--without-kerberos \
-		--without-gssapi \
-		--without-ldap \
-		--disable-tcp-wrappers \
-		--disable-static \
-		--program-prefix=""
+	--with-bdb=$(STAGING_DIR)/usr \
+	--with-ssl-dir=$(STAGING_DIR)/usr \
+	--localstatedir=/var/lib \
+	--enable-zeroconf \
+	--disable-cups \
+	--without-pam \
+	--without-kerberos \
+	--without-gssapi \
+	--without-ldap \
+	--without-libevent \
+	--disable-tcp-wrappers \
+	--disable-static \
+	--program-prefix=""
 
-NETATALK_DEPENDENCIES = uclibc libgcrypt db avahi libevent2
+NETATALK_DEPENDENCIES = uclibc libgcrypt db avahi libevent2 acl openssl libiconv
 
 $(eval $(call AUTOTARGETS,package,netatalk))
 
 $(NETATALK_HOOK_POST_CONFIGURE):
-	sed -i 's/-levent/-levent2/' $(NETATALK_DIR)/etc/netatalk/Makefile
 	# setting --enable-fhs on configure changes install folders to /bin/, /lib, etc
 	# as only /var/run/netatalk.pid is desired (include/atalk/paths.h), hack:
 	sed -i 's/.*FHS_COMPATIBILITY.*/#define FHS_COMPATIBILITY 1/' $(NETATALK_DIR)/config.h
