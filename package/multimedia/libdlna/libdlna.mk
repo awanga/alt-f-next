17458c6331bb5cced1b00d707764693f10961f0c
João Cardoso <joao.fs.cardoso@gmail.com>
2015-06-19 18:05:16 +0000

    libdlna: bump to 0.2.4

--- package/multimedia/libdlna/libdlna.mk
+++ package/multimedia/libdlna/libdlna.mk
@@ -3,21 +3,21 @@
 # libdlna
 #
 #############################################################
-LIBDLNA_VERSION = 0.2.3
+
+LIBDLNA_VERSION = 0.2.4
 LIBDLNA_SOURCE = libdlna-$(LIBDLNA_VERSION).tar.bz2
 LIBDLNA_SITE = http://libdlna.geexbox.org/releases/
+
 LIBDLNA_AUTORECONF = NO
-LIBDLNA_INSTALL_STAGING = YES
-LIBDLNA_INSTALL_TARGET = YES
 LIBDLNA_LIBTOOL_PATCH = NO
-LIBDLNA_CONF_OPT = --disable-static \
-	--cross-compile \
-	--cross-prefix=arm-linux-uclibcgnueabi-
+
+LIBDLNA_INSTALL_STAGING = YES
 
 LIBDLNA_DEPENDENCIES = uclibc ffmpeg
 
-$(eval $(call AUTOTARGETS,package/multimedia,libdlna))
+LIBDLNA_CONF_OPT = --disable-static --cross-compile --cross-prefix=arm-linux-uclibcgnueabi-
 
+$(eval $(call AUTOTARGETS,package/multimedia,libdlna))
 
 $(LIBDLNA_TARGET_INSTALL_TARGET):
 	$(MAKE) DESTDIR=$(TARGET_DIR) -C $(LIBDLNA_DIR) install
