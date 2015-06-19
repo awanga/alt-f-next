4928ea15508c87e52abfdfd595fa2f52c5d9a6b7
João Cardoso <joao.fs.cardoso@gmail.com>
2015-06-19 16:41:21 +0000

    transmission: adapt to libevent2 new location

--- package/transmission/transmission.mk
+++ package/transmission/transmission.mk
@@ -9,13 +9,9 @@ TRANSMISSION_SOURCE = transmission-$(TRANSMISSION_VERSION).tar.xz
 TRANSMISSION_SITE = http://download.m0k.org/transmission/files
 
 TRANSMISSION_AUTORECONF = NO
-TRANSMISSION_INSTALL_STAGING = NO
-TRANSMISSION_INSTALL_TARGET = YES
 TRANSMISSION_LIBTOOL_PATCH = NO
 
 TRANSMISSION_DEPENDENCIES = uclibc libcurl openssl libevent2 pkg-config
 TRANSMISSION_CONF_OPT = --disable-nls --disable-gtk --disable-gconf2 --enable-utp
-TRANSMISSION_CONF_ENV = LIBEVENT_CFLAGS="-I$(STAGING_DIR)/libevent2/include" \
-	LIBEVENT_LIBS="-L$(STAGING_DIR)/libevent2/lib -levent2" ac_cv_prog_HAVE_CXX=yes
 
 $(eval $(call AUTOTARGETS,package,transmission))
