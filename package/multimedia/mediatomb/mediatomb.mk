dd354e96c2f1125f90918b2c6b4cd50f3c16cf64
João Cardoso <joao.fs.cardoso@gmail.com>
2015-06-19 18:15:18 +0000

    mediatomb: bump dev version; support mysql

--- package/multimedia/mediatomb/mediatomb.mk
+++ package/multimedia/mediatomb/mediatomb.mk
@@ -7,10 +7,9 @@
 MEDIATOMB_VERSION = 0.12.1
 MEDIATOMB_SOURCE = mediatomb-$(MEDIATOMB_VERSION).tar.gz
 MEDIATOMB_SITE = $(BR2_SOURCEFORGE_MIRROR)/project/mediatomb/MediaTomb/$(MEDIATOMB_VERSION)
-MEDIATOMB_INSTALL_STAGING = NO
-MEDIATOMB_INSTALL_TARGET = YES
+
 MEDIATOMB_LIBTOOL_PATCH = NO
-MEDIATOMB_DEPENDENCIES = uclibc sqlite expat file taglib ffmpeg libcurl
+MEDIATOMB_DEPENDENCIES = uclibc sqlite mysql expat file taglib ffmpeg libcurl
 
 MEDIATOMB_CONF_OPT = --enable-sighup --enable-inotify
 
