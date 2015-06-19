649ea3bd5c5c97fb7a823aa5cd6d93b2d5c5280a
João Cardoso <joao.fs.cardoso@gmail.com>
2015-06-19 18:17:22 +0000

    fuppes: bump dev version (ffmpeg)

--- package/multimedia/fuppes/fuppes.mk
+++ package/multimedia/fuppes/fuppes.mk
@@ -8,16 +8,12 @@ FUPPES_VERSION = 660
 FUPPES_SOURCE = fuppes-0.$(FUPPES_VERSION).tar.gz
 FUPPES_SITE = $(BR2_SOURCEFORGE_MIRROR)/project/fuppes/fuppes/SVN-$(FUPPES_VERSION)
 
-FUPPES_INSTALL_STAGING = NO
-FUPPES_INSTALL_TARGET = YES
-
 FUPPES_LIBTOOL_PATCH = NO
-FUPPES_AUTORECONF = NO
+FUPPES_AUTORECONF = YES
 
 FUPPES_DEPENDENCIES = uclibc pkg-config pcre libxml2 sqlite
 
-FUPPES_CONF_ENV += MYSQL_CONFIG=no \
-	PKG_CONFIG_PATH=$(STAGING_DIR)/usr/lib/pkgconfig
+FUPPES_CONF_ENV += MYSQL_CONFIG=no
 
 FUPPES_CONF_OPT = --disable-mp4v2 --disable-transcoder-ffmpeg --disable-magickwand \
 	--disable-ffmpegthumbnailer --disable-libavformat --disable-lame \
