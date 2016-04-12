4cabbe8feb21364bc1f67024cb48d6900bd4e817
João Cardoso <joao.fs.cardoso@gmail.com>
2016-04-12 15:13:33 +0000

    libevent: update download URL

--- package/libevent2/libevent2.mk
+++ package/libevent2/libevent2.mk
@@ -7,7 +7,7 @@
 LIBEVENT2_MAJOR:=2.1
 LIBEVENT2_VERSION:=2.1.5
 LIBEVENT2_SOURCE:=libevent-$(LIBEVENT2_VERSION)-beta.tar.gz
-LIBEVENT2_SITE:= $(BR2_SOURCEFORGE_MIRROR)/project/levent/libevent/libevent-$(LIBEVENT2_MAJOR)
+LIBEVENT2_SITE:=https://github.com/libevent/libevent/releases/download/release-2.1.5-beta
 
 LIBEVENT2_LIBTOOL_PATCH = NO
 LIBEVENT2_INSTALL_STAGING = YES
