c3d6b02010c1c53057297eace4d7d24beeb805bb
João Cardoso <joao.fs.cardoso@gmail.com>
2016-04-12 17:56:50 +0000

    alt-f-utils: add DNS-327L support to dns323-fw

--- package/alt-f-utils/alt-f-utils-0.1.7/dns323-fw.c
+++ package/alt-f-utils/alt-f-utils-0.1.7/dns323-fw.c
@@ -141,7 +141,8 @@ sig_t signatures[] = {
 	{3, 128, 5242880, 5242880, 106954752, 5242880, "\x55\xAA" "DNS-325" "\x00\x55\xAA"},	/* DNS-325-A1A2 */
 	{4, 128, 5242880, 5242880, 106954752, 5242880, "\x55\xAA" "DNS323D1" "\x55\xAA"},	/* DNS-320-A1A2 */
 	{5, 128, 5242880, 5242880, 104857600, 5242880, "\x55\xAA" "DNS320B" "\x00\x55\xAA"},	/* DNS-320-B1 */
-	{6, 128, 5242880, 5242880, 104857600, 5242880, "\x55\xAA" "DNS320L" "\x00\x55\xAA"}	/* DNS-320L */
+	{6, 128, 5242880, 5242880, 104857600, 5242880, "\x55\xAA" "DNS320L" "\x00\x55\xAA"},	/* DNS-320L */
+	{7, 128, 5242880, 5242880, 81788928, 10485760, "\x55\xAA" "DNS327L" "\x00\x55\xAA"}	/* DNS-327L */
 };
 
 int nsig = sizeof(signatures)/sizeof(sig_t);
@@ -510,7 +511,7 @@ void usage() {
 		"\tdns323-fw -m [-q (quiet)]\n"
 		"\t-k kernel_file -i initramfs_file [-a sqimage_file] [-d defaults_file]\n"
 		"\t[-p product_id]  [-c custom_id] [-l model_id ] [-u sub_id] [-v new_version]\n"
-		"\t[-t type (0-FrodoII, 1-Chopper, 2-Gandolf, 3-DNS-325-A1A2, 4-DNS-320-A1A2, 5-DNS-320-B 6-DNS-320L] firmware_file\n");
+		"\t[-t type (0-FrodoII, 1-Chopper, 2-Gandolf, 3-DNS-325-A1A2, 4-DNS-320-A1A2, 5-DNS-320-B 6-DNS-320L, 7-DNS-327L] firmware_file\n");
   
 	fprintf(stderr, "-Print information from a firmware file:\n"
 		"\tdns323-fw -f firmware_file\n");
