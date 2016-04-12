9315d012381b5272d425ddd55190c485c7f67e01
João Cardoso <joao.fs.cardoso@gmail.com>
2016-04-12 17:09:33 +0000

    uboot-mkimage: update download URL

--- package/uboot-mkimage/uboot-mkimage.mk
+++ package/uboot-mkimage/uboot-mkimage.mk
@@ -6,14 +6,13 @@
 
 UBOOT_MKIMAGE_VERSION = 0.4
 UBOOT_MKIMAGE_SOURCE = uboot-mkimage_$(UBOOT_MKIMAGE_VERSION).tar.gz
-UBOOT_MKIMAGE_SITE = $(BR2_DEBIAN_MIRROR)/debian/pool/main/u/uboot-mkimage
+#UBOOT_MKIMAGE_SITE = $(BR2_DEBIAN_MIRROR)/debian/pool/main/u/uboot-mkimage
+UBOOT_MKIMAGE_SITE = http://archive.debian.org/debian/pool/main/u/uboot-mkimage
 
 UBOOT_MKIMAGE_HOST_DEPENDENCIES = uclibc zlib-host
 UBOOT_MKIMAGE_HOST_MAKE_ENV = CC="$(HOSTCC) $(HOST_CFLAGS)"
 
 $(eval $(call AUTOTARGETS_HOST,package,uboot-mkimage))
 
-$(UBOOT_MKIMAGE_HOST_HOOK_POST_EXTRACT):
-	echo -e "#!/bin/sh\ntrue" > $(UBOOT_MKIMAGE_HOST_DIR)/configure
-	chmod +x $(UBOOT_MKIMAGE_HOST_DIR)/configure
+$(UBOOT_MKIMAGE_HOST_CONFIGURE):
 	touch $@
