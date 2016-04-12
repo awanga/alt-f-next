8d6315ccaaec4e8111ab5b915fc5851615dd1188
João Cardoso <joao.fs.cardoso@gmail.com>
2016-04-12 15:17:57 +0000

    msmtp: bump to 1.6.4

--- package/msmtp/msmtp.mk
+++ package/msmtp/msmtp.mk
@@ -4,8 +4,12 @@
 #
 #############################################################
 
-MSMTP_VERSION = 1.4.31
-MSMTP_SOURCE = msmtp-$(MSMTP_VERSION).tar.bz2
+#MSMTP_VERSION = 1.4.31
+MSMTP_VERSION = 1.6.4
+
+#MSMTP_SOURCE = msmtp-$(MSMTP_VERSION).tar.bz2
+MSMTP_SOURCE = msmtp-$(MSMTP_VERSION).tar.xz
+
 MSMTP_SITE = $(BR2_SOURCEFORGE_MIRROR)/project/msmtp/msmtp/$(MSMTP_VERSION)
 
 MSMTP_INSTALL_STAGING = NO
@@ -13,7 +17,8 @@ MSMTP_INSTALL_TARGET = YES
 MSMTP_BINARY:=src/msmtp
 MSMTP_TARGET_BINARY:=usr/bin/msmtp
 
-MSMTP_CONF_OPT = --program-prefix="" --with-ssl=openssl --without-gnome-keyring --without-libidn
+MSMTP_CONF_OPT = --program-prefix="" --with-ssl=openssl \
+		--without-gnome-keyring --without-libidn --disable-gai-idn
 
 MSMTP_DEPENDENCIES = uclibc openssl host-pkgconfig
 
