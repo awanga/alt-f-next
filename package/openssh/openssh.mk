e278f5e1168a7b6e9e6820c4f6b70ad88cf34f34
João Cardoso <joao.fs.cardoso@gmail.com>
2016-04-12 16:00:42 +0000

    openssh: bump to 7.1p2 -- update might affect users and other progs; remove unneeded patch; support initscript and webUI

--- package/openssh/openssh.mk
+++ package/openssh/openssh.mk
@@ -4,7 +4,9 @@
 #
 #############################################################
 
-OPENSSH_VERSION=6.4p1
+#OPENSSH_VERSION=6.9p1
+OPENSSH_VERSION=7.1p2
+
 OPENSSH_SITE=ftp://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
 
 OPENSSH_CONF_ENV = LD=$(TARGET_CC)
@@ -15,7 +17,7 @@ OPENSSH_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
 OPENSSH_SFTP_STAMP = $(PROJECT_BUILD_DIR)/autotools-stamps/openssh-sftp_target_installed
 
-# The bellow dependency on dropbear is a fake. Dropbear installs ssh, scp, etc as a link
+# The bellow dependency on dropbear is a fake. Dropbear installs ssh and scp as a link
 # to dropbear, so if it is installed after openssh it will override those binaries
 
 OPENSSH_DEPENDENCIES = zlib openssl dropbear
@@ -29,6 +31,9 @@ ifneq ($(BR2_PACKAGE_OPENSSH_SFTP),y)
 
 $(OPENSSH_HOOK_POST_INSTALL):
 	sed -i 's/#.*Ciphers /Ciphers aes128-cbc,/' $(TARGET_DIR)/etc/ssh/ssh_config
+	sed -i -e '/Subsystem/s/libexec/lib/' \
+		-e 's/#PermitRootLogin no/PermitRootLogin yes/' \
+		$(TARGET_DIR)/etc/ssh/sshd_config
 	touch $@
 
 else
