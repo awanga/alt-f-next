33e6483338b53e4f784704ddf6a895e69367d34e
João Cardoso <joao.fs.cardoso@gmail.com>
2016-04-13 15:41:26 +0000

    forked_daapd: bump to 23.4; remove taglib, flac, libavl dependencies

--- package/multimedia/forked-daapd/forked-daapd.mk
+++ package/multimedia/forked-daapd/forked-daapd.mk
@@ -4,19 +4,19 @@
 #
 #############################################################
 
-FORKED_DAAPD_VERSION = 23.3
+FORKED_DAAPD_VERSION = 23.4
 FORKED_DAAPD_SOURCE = forked-daapd-$(FORKED_DAAPD_VERSION).tar.gz
 FORKED_DAAPD_SITE = https://github.com/ejurgensen/forked-daapd/archive
 
 FORKED_DAAPD_AUTORECONF = YES
 FORKED_DAAPD_LIBTOOL_PATCH = YES
 
-FORKED_DAAPD_DEPENDENCIES = uclibc zlib libavl libunistring libevent2 libgcrypt libconfuse mxml alsa-lib libantlr sqlite avahi ffmpeg taglib flac gperf-host host-automake
+FORKED_DAAPD_DEPENDENCIES = uclibc zlib libunistring libevent2 libgcrypt libconfuse mxml alsa-lib libantlr sqlite avahi ffmpeg gperf-host host-automake
 
-FORKED_DAAPD_CONF_ENV = ZLIB_LIBS="-lz" ZLIB_CFLAGS=" "
+FORKED_DAAPD_CONF_ENV = ZLIB_LIBS="-L$(STAGING_DIR)/usr/lib -lz" ZLIB_CFLAGS="-I$(STAGING_DIR)/usr/include"
+# FIXME: zlib: install zlib.pc
 
-FORKED_DAAPD_CONF_OPT = --localstatedir=/var --sysconfdir=/etc \
-	--enable-flac --enable-musepack --disable-rpath
+FORKED_DAAPD_CONF_OPT = --localstatedir=/var --sysconfdir=/etc --enable-mpd --disable-rpath
 
 $(eval $(call AUTOTARGETS,package/multimedia,forked-daapd))
 
@@ -28,7 +28,6 @@ $(FORKED_DAAPD_TARGET_SOURCE):
 
 $(FORKED_DAAPD_HOOK_POST_EXTRACT):
 	sed -i 's/@CONFUSE_CFLAGS@ @TAGLIB_CFLAGS@/& @LIBEVENT_CFLAGS@/' $(FORKED_DAAPD_DIR)/src/Makefile.am 
-	sed -i 's/_EVENT_/EVENT__/' $(FORKED_DAAPD_DIR)/src/evrtsp/rtsp-libevent20.c
 	find $(FORKED_DAAPD_DIR) -name \*.c | xargs sed -i 's/<config.h>/<config2.h>/'
 	##aclocal -I ./m4
 	##libtoolize --copy
