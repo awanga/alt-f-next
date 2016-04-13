3cb629a0f13311587bff3fa474656975bce68d55
João Cardoso <joao.fs.cardoso@gmail.com>
2016-04-13 15:38:07 +0000

    ffmpegthumbnailer: link with pthreads; add some notes on download URL, versions, compile requirements

--- package/multimedia/ffmpegthumbnailer/ffmpegthumbnailer.mk
+++ package/multimedia/ffmpegthumbnailer/ffmpegthumbnailer.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-# requires cmake
+# 2.0.9 and later requires cmake and a -std=c++11 gcc compiler
 #https://github.com/dirkvdb/ffmpegthumbnailer/releases/download/2.0.10/ffmpegthumbnailer-2.0.10.tar.bz2
 # FFMPEGTHUMBNAILER_VERSION = 2.0.10
 # FFMPEGTHUMBNAILER_SOURCE = ffmpegthumbnailer-$(FFMPEGTHUMBNAILER_VERSION).tar.bz2
@@ -13,6 +13,8 @@
 FFMPEGTHUMBNAILER_VERSION = 2.0.8
 FFMPEGTHUMBNAILER_SOURCE = ffmpegthumbnailer-$(FFMPEGTHUMBNAILER_VERSION).tar.gz
 FFMPEGTHUMBNAILER_SITE = http://ffmpegthumbnailer.googlecode.com/files
+# moved to github, but downloaded archive is NOT the 2.0.8 tarball!!!
+#FFMPEGTHUMBNAILER_SITE = https://github.com/dirkvdb/ffmpegthumbnailer/archive
 
 FFMPEGTHUMBNAILER_AUTORECONF = NO
 FFMPEGTHUMBNAILER_LIBTOOL_PATCH = NO
@@ -23,6 +25,6 @@ FFMPEGTHUMBNAILER_INSTALL_TARGET = YES
 FFMPEGTHUMBNAILER_DEPENDENCIES = uclibc ffmpeg jpeg libpng
 
 FFMPEGTHUMBNAILER_CONF_OPT = --disable-static --enable-png --enable-jpeg
-FFMPEGTHUMBNAILER_CONF_ENV = PKG_CONFIG_PATH=$(STAGING_DIR)/usr/lib/pkgconfig/
+FFMPEGTHUMBNAILER_CONF_ENV = PKG_CONFIG_PATH=$(STAGING_DIR)/usr/lib/pkgconfig/ LIBS="-lpthread"
 
 $(eval $(call AUTOTARGETS,package/multimedia,ffmpegthumbnailer))
