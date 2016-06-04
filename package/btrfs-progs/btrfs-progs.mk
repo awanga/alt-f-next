3ccf1074585c38323f5b713d86b28bf6e03876ad
João Cardoso <joao.fs.cardoso@gmail.com>
2016-06-04 18:54:07 +0000

    btrfs-progs: new package

--- package/btrfs-progs/btrfs-progs.mk
+++ package/btrfs-progs/btrfs-progs.mk
@@ -4,22 +4,26 @@
 #
 #############################################################
 
-BTRFS_PROGS_VERSION = v3.14.2
-BTRFS_PROGS_SOURCE = btrfs-progs-$(BTRFS_PROGS_VERSION).tar.xz
-BTRFS_PROGS_SITE = https://www.kernel.org/pub/linux/kernel/people/mason/btrfs-progs
+BTRFS_PROGS_VERSION = 4.5.3
+BTRFS_PROGS_SOURCE = btrfs-progs-v$(BTRFS_PROGS_VERSION).tar.xz
+BTRFS_PROGS_SITE = $(BR2_KERNEL_MIRROR)/linux/kernel/people/kdave/btrfs-progs
 
 BTRFS_PROGS_INSTALL_STAGING = NO
 BTRFS_PROGS_INSTALL_TARGET = YES
-BTRFS_PROGS_DEPENDENCIES = uclibc attr acl zlib lzo e2fsprogs
+BTRFS_PROGS_DEPENDENCIES = uclibc attr acl zlib lzo e2fsprogs util-linux
 
-BTRFS_PROGS_MAKE_ENV = $(TARGET_CONFIGURE_OPTS) $(TARGET_CONFIGURE_ENV)
+BTRFS_PROGS_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
+
+BTRFS_PROGS_OPTI=-I$(BTRFS_PROGS_DIR)/util-linux/include/
+BTRFS_PROGS_OPTL=-L$(BTRFS_PROGS_DIR)/util-linux/lib/
+
+BTRFS_PROGS_CONF_OPT = --disable-backtrace --disable-documentation
+BTRFS_PROGS_CONF_ENV = ZLIB_CFLAGS=" " ZLIB_LIBS="-lz" \
+	BLKID_CFLAGS="$(BTRFS_PROGS_OPTI)" \
+	BLKID_LIBS="$(BTRFS_PROGS_OPTL)  -lblkid" \
+	UUID_CFLAGS=$(BTRFS_PROGS_OPTI) \
+	UUID_LIBS="$(BTRFS_PROGS_OPTL)  -luuid" \
+	CC="$(TARGET_CC) $(BTRFS_PROGS_OPTI) $(TARGET_CFLAGS)" \
+	LDFLAGS="$(BTRFS_PROGS_OPTL) $(TARGET_LDFLAGS)" 
 
 $(eval $(call AUTOTARGETS,package,btrfs-progs))
-
-$(BTRFS_PROGS_TARGET_CONFIGURE):
-	touch $@
-
-$(BTRFS_PROGS_HOOK_POST_CONFIGURE):
-	$(SED) 's/build-Documentation//' -e 's/install-Documentation//' \
-		-e 's/^CC = .*//' -e 's/^CFLAGS = .*//' $(BTRFS_PROGS_DIR)/Makefile
-	touch $@
