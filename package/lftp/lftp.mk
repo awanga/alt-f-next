ff61842df6df90915c636c1802fa6a962e8788f8
João Cardoso <joao.fs.cardoso@gmail.com>
2016-06-11 18:43:06 +0000

    lftp: update download URL

--- package/lftp/lftp.mk
+++ package/lftp/lftp.mk
@@ -6,7 +6,7 @@
 
 LFTP_VERSION:=4.6.1
 LFTP_SOURCE:=lftp-$(LFTP_VERSION).tar.gz
-LFTP_SITE:=http://lftp.yar.ru/ftp/old
+LFTP_SITE:=http://lftp.yar.ru/ftp
 
 LFTP_LIBTOOL_PATCH = NO
 LFTP_DEPENDENCIES = readline expat libiconv gettext openssl
