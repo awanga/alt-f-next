85a41b92bc955c3bbde31b817956e49604c313dc
João Cardoso <joao.fs.cardoso@gmail.com>
2017-02-21 19:18:17 +0000

    mc: add support for Cyrillic. Thanks to Konstantin Slipenko for contributing

--- package/mc/mc.mk
+++ package/mc/mc.mk
@@ -13,21 +13,19 @@ MC_INSTALL_STAGING = NO
 MC_INSTALL_TARGET = YES
 MC_LIBTOOL_PATCH = NO
 
-MC_DEPENDENCIES = libglib2 ncurses
+MC_DEPENDENCIES = libglib2 slang
 
 MC_CONF_ENV = fu_cv_sys_stat_statfs2_bsize=yes
-MC_CONF_OPT = --disable-doxygen-doc --without-x --with-screen=ncurses \
-	--with-ncurses-includes=$(STAGING_DIR)/usr/include \
-	--with-ncurses-libs=$(STAGING_DIR)/usr/lib
+MC_CONF_OPT = --disable-doxygen-doc --without-x --with-screen=slang
 
 $(eval $(call AUTOTARGETS,package,mc))
 
 # mc requires ncurses.h under ncurses include directory, so fool it! 
-$(MC_HOOK_POST_EXTRACT):
-	( \
-	cd $(STAGING_DIR)/usr/include; \
-	mkdir ncurses; cd ncurses; \
-	ln -sf ../ncurses.h ncurses.h; \
-	ln -sf ../term.h term.h; \
-	)
-	touch $@
+# $(MC_HOOK_POST_EXTRACT):
+# 	( \
+# 	cd $(STAGING_DIR)/usr/include; \
+# 	mkdir ncurses; cd ncurses; \
+# 	ln -sf ../ncurses.h ncurses.h; \
+# 	ln -sf ../term.h term.h; \
+# 	)
+# 	touch $@
