993116ba8d98e0a0c7ac9a2d77f0725dea41ef8e
João Cardoso <joao.fs.cardoso@gmail.com>
2017-02-22 16:28:10 +0000

    openssl: bump to 1.0.2i

--- package/openssl/openssl.mk
+++ package/openssl/openssl.mk
@@ -2,18 +2,12 @@
 #
 # openssl
 #
-#############################################################
+############################################################
 
-OPENSSL_VERSION:=1.0.2g
+OPENSSL_VERSION:=1.0.2i
 OPENSSL_SITE:=http://www.openssl.org/source
 
 OPENSSL_MAKE = $(MAKE1)
-
-# specific compiler optimization
-OPENSSL_CFLAGS = $(TARGET_CFLAGS)
-ifeq ($(BR2_PACKAGE_OPENSSL_SIZEOPTIM),y)
-OPENSSL_CFLAGS = $(TARGET_CFLAGS) -Os
-endif
 
 # Some architectures are optimized in OpenSSL
 OPENSSL_TARGET_ARCH=generic32
@@ -36,35 +30,59 @@ OPENSSL_INSTALL_TARGET_OPT = INSTALL_PREFIX=$(TARGET_DIR) install_sw
 
 OPENSSL_DEPENDENCIES = zlib
 
-ifeq ($(BR2_PACKAGE_CRYPTODEV),y)
-	OPENSSL_DEPENDENCIES += cryptodev
-	OPENSSL_CRYPTO_OPT = -DHAVE_CRYPTODEV -DUSE_CRYPTODEV_DIGESTS -DHASH_MAX_LEN=64
-	OPENSSL_CFLAGS += $(OPENSSL_CRYPTO_OPT)
+# specific compiler optimization
+OPENSSL_CFLAGS = $(TARGET_OPTIMIZATION)
+ifneq ($(BR2_PACKAGE_OPENSSL_SIZEOPTIM),)
+	OPENSSL_CFLAGS += $(BR2_PACKAGE_OPENSSL_SIZEOPTIM)
 endif
 
+OPENSSL_CONF_OPT = -DOPENSSL_SMALL_FOOTPRINT 
+ifeq ($(BR2_PACKAGE_CRYPTODEV),y)
+	OPENSSL_DEPENDENCIES += cryptodev
+	OPENSSL_CONF_OPT += -DHAVE_CRYPTODEV -DUSE_CRYPTODEV_DIGESTS -DHASH_MAX_LEN=64
+endif
+
+OPENSSL_CFLAGS += $(OPENSSL_CONF_OPT)
+
 $(eval $(call AUTOTARGETS,package,openssl))
+
+# load cryptodev.ko mv_cesa.ko
+# MV-CESA:Could not register sha1 driver FIXME
+# MV-CESA:Could not register hmac-sha1 driver FIXME
+# 
+# / # openssl engine cryptodev
+# (cryptodev) cryptodev engine
+# 
+# /# openssl speed -evp aes-128-cbc
+# type                    16 bytes        64 bytes     256 bytes    1024 bytes        8192 bytes
+# aes-128-cbc       3462.90k      4104.75k      4306.43k       4356.28k       4205.59k (no mv_cesa)
+# aes-128-cbc       3675.66k    14573.71k    43141.69k   286182.40k   375778.74k (mv_cesa)
+# 
+# / # openssl speed -evp sha1
+# sha1               575.41k     1823.25k     4580.09k     7323.65k     8909.83k (no mv_cesa)
+# sha1               541.06k     1754.84k     4469.86k     7254.65k     8866.47k (mv_cesa)
 
 $(OPENSSL_TARGET_CONFIGURE):
 	(cd $(OPENSSL_DIR); \
 		$(TARGET_CONFIGURE_ARGS) \
 		$(TARGET_CONFIGURE_OPTS) \
 		./Configure \
-			$(OPENSSL_CRYPTO_OPT) \
-			-DOPENSSL_SMALL_FOOTPRINT \
 			--prefix=/usr \
 			--openssldir=/etc/ssl \
 			threads shared \
 			no-idea no-md2 no-mdc2 no-rc5 no-camellia no-seed \
-			no-krb5 no-jpake no-store no-hw no-zlib \
-			no-ssl2 no-ssl3 no-comp no-err no-engines \
+			no-krb5 no-jpake no-store no-err \
+ 			no-zlib no-comp no-ssl2 no-ssl3 \
+			$(OPENSSL_CONF_OPT) \
 			linux-$(OPENSSL_TARGET_ARCH) \
 	)
+	$(SED) "s/build_tests //" $(OPENSSL_DIR)/Makefile
 	touch $@
 
 $(OPENSSL_TARGET_BUILD):
 	# libs compiled with chosen optimization
-	$(SED) "s:-O[0-9]:$(OPENSSL_CFLAGS):" $(OPENSSL_DIR)/Makefile
-	$(OPENSSL_MAKE) CC=$(TARGET_CC) MAKEDEPPROG=$(TARGET_CC) -C $(OPENSSL_DIR) depend build_crypto build_ssl build_engines
+	$(SED) "s:-O[s0-9]:$(OPENSSL_CFLAGS):" $(OPENSSL_DIR)/Makefile
+	$(OPENSSL_MAKE) CC=$(TARGET_CC) MAKEDEPPROG=$(TARGET_CC) -C $(OPENSSL_DIR) depend build_libs
 	# openssl program compiled with -Os, saves 27KB
 	$(SED) "s:-O[0-9]:-Os:" $(OPENSSL_DIR)/Makefile
 	$(OPENSSL_MAKE) CC=$(TARGET_CC) MAKEDEPPROG=$(TARGET_CC) -C $(OPENSSL_DIR) build_apps
