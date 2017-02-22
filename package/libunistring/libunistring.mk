8eac9c0a38ddd9ac9f555bb9038b60f0d4c008c7
João Cardoso <joao.fs.cardoso@gmail.com>
2017-02-22 16:42:29 +0000

    libunistring: disable static compile

--- package/libunistring/libunistring.mk
+++ package/libunistring/libunistring.mk
@@ -2,16 +2,15 @@
 #
 # libunistring
 #
-#############################################################
+############################################################
 
 LIBUNISTRING_VERSION:=0.9.3
 LIBUNISTRING_SITE:=$(BR2_GNU_MIRROR)/libunistring
 LIBUNISTRING_SOURCE=libunistring-$(LIBUNISTRING_VERSION).tar.gz
 
-LIBUNISTRING_DIR=$(BUILD_DIR)/libavl-$(LIBUNISTRING_VERSION)
 LIBUNISTRING_LIBTOOL_PATCH = NO
 LIBUNISTRING_INSTALL_STAGING = YES
 
-#LIBUNISTRING_CONF_OPT = --disable-static --program-prefix=""
+LIBUNISTRING_CONF_OPT = --disable-static
 
 $(eval $(call AUTOTARGETS,package,libunistring))
