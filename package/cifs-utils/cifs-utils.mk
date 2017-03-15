4d10baf021f71e08efe265e0c0643757a52d958b
João Cardoso <joao.fs.cardoso@gmail.com>
2017-03-15 18:52:55 +0000

    cifs-utils: add EOF

--- package/cifs-utils/cifs-utils.mk
+++ package/cifs-utils/cifs-utils.mk
@@ -8,10 +8,6 @@ CIFS_UTILS_VERSION:=6.6
 CIFS_UTILS_SOURCE:=cifs-utils-$(CIFS_UTILS_VERSION).tar.bz2
 CIFS_UTILS_SITE:=https://download.samba.org/pub/linux-cifs/cifs-utils
 
-#CIFS_UTILS_DEPENDENCIES = gettext 
-#CIFS_UTILS_CONF_OPT = --disable-nls
-#CIFS_UTILS_CONF_ENV = LIBS="-liconv -lintl"
-
 $(eval $(call AUTOTARGETS,package,cifs-utils))
 
 $(CIFS_UTILS_HOOK_POST_INSTALL):
