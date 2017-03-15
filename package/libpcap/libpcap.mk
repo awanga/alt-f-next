855bfbd4686ef7ad77c4116b8528f68d031ff976
João Cardoso <joao.fs.cardoso@gmail.com>
2017-03-15 18:54:28 +0000

    libpcap: configure --without-libnl

--- package/libpcap/libpcap.mk
+++ package/libpcap/libpcap.mk
@@ -2,15 +2,15 @@
 #
 # libpcap
 #
-#############################################################
-#LIBPCAP_VERSION:=1.1.1
+############################################################
+
 LIBPCAP_VERSION:=1.4.0
 LIBPCAP_SOURCE:=libpcap-$(LIBPCAP_VERSION).tar.gz
 LIBPCAP_SITE:=http://www.tcpdump.org/release
 LIBPCAP_DIR:=$(BUILD_DIR)/libpcap-$(LIBPCAP_VERSION)
 LIBPCAP_INSTALL_STAGING = YES
 
-LIBPCAP_CONF_OPT = --disable-yydebug --with-pcap=linux
+LIBPCAP_CONF_OPT = --disable-yydebug --with-pcap=linux --without-libnl
 LIBPCAP_CONF_ENV = ac_cv_linux_vers=$(BR2_DEFAULT_KERNEL_HEADERS)
 LIBPCAP_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
