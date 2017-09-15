440d55ff79faffce8245a969b90bd4ba6325356a
João Cardoso <jcard@gmail.com>
2017-09-15 17:46:42 +0000

    wget: bump to 1.19.1
    git-svn-id: https://svn.code.sf.net/p/alt-f/code/trunk/alt-f@3747 cd80dc97-fa67-4d1f-9ead-3176a861659d

--- package/wget/wget.mk
+++ package/wget/wget.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-WGET_VERSION:=1.18
+WGET_VERSION:=1.19.1
 WGET_SOURCE:=wget-$(WGET_VERSION).tar.gz
 WGET_SITE:=$(BR2_GNU_MIRROR)/wget
 
