ece4c98cb121994094881a1b123afddd80ef8d0c
João Cardoso <jcard@gmail.com>
2018-03-14 17:49:24 +0000

    cadaver: update download URL
    git-svn-id: https://svn.code.sf.net/p/alt-f/code/trunk/alt-f@3843 cd80dc97-fa67-4d1f-9ead-3176a861659d

--- package/cadaver/cadaver.mk
+++ package/cadaver/cadaver.mk
@@ -1,12 +1,12 @@
-#############################################################
+###########################################################
 #
 # cadaver
 #
-#############################################################
+###########################################################
 
 CADAVER_VERSION:=0.23.3
-CADAVER_SOURCE:=cadaver-$(CADAVER_VERSION).tar.gz
-CADAVER_SITE:=http://www.webdav.org/cadaver
+CADAVER_SOURCE:=cadaver_$(CADAVER_VERSION).orig.tar.gz
+CADAVER_SITE:=$(BR2_DEBIAN_MIRROR)/debian/pool/main/c/cadaver
 
 CADAVER_DEPENDENCIES = ncurses readline libiconv neon uclibc
 CADAVER_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
