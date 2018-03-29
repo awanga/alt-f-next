7382f257d42440c39bda7a80bfd889ef5707c68e
João Cardoso <jcard@gmail.com>
2018-03-29 16:43:59 +0000

    transmission: bump to 2.93; remove 2.92 patch; configure with --enable-lightweight
    git-svn-id: https://svn.code.sf.net/p/alt-f/code/trunk/alt-f@3845 cd80dc97-fa67-4d1f-9ead-3176a861659d

--- package/transmission/transmission.mk
+++ package/transmission/transmission.mk
@@ -1,18 +1,19 @@
-#############################################################
+###########################################################
 #
 # transmission
 #
-#############################################################
+###########################################################
 
-TRANSMISSION_VERSION = 2.92
+TRANSMISSION_VERSION = 2.93
 TRANSMISSION_SOURCE = transmission-$(TRANSMISSION_VERSION).tar.xz
-TRANSMISSION_SITE = http://download.m0k.org/transmission/files
+TRANSMISSION_SITE = https://github.com/transmission/transmission-releases/raw/master
 
 TRANSMISSION_AUTORECONF = NO
 TRANSMISSION_LIBTOOL_PATCH = NO
 
 TRANSMISSION_DEPENDENCIES = uclibc libcurl openssl libevent2 pkg-config
-TRANSMISSION_CONF_OPT = --disable-nls --disable-gtk --disable-gconf2 --enable-utp --enable-cli
+TRANSMISSION_CONF_OPT = --disable-nls --disable-gtk --disable-gconf2 \
+--enable-utp --enable-cli --enable-lightweight
 TRANSMISSION_CONF_ENV = ZLIB_LIBS="-L$(STAGING_DIR)/usr/lib -lz" ZLIB_CFLAGS="-I$(STAGING_DIR)/usr/include"
 # FIXME: zlib: install zlib.pc
 
