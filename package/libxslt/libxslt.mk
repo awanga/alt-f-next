afd85c793b8f5b437fb18ed933404f24678b10a5
João Cardoso <jcard@gmail.com>
2020-03-31 18:10:27 +0000

    libxslt new package. Used to transform XML files to other XML files, designed for use as part of XSL.
    git-svn-id: https://svn.code.sf.net/p/alt-f/code/trunk/alt-f@3882 cd80dc97-fa67-4d1f-9ead-3176a861659d

--- package/libxslt/libxslt.mk
+++ package/libxslt/libxslt.mk
@@ -2,16 +2,21 @@
 #
 # libxslt
 #
-#############################################################
-LIBXSLT_VERSION = 1.1.24
+############################################################
+
+#LIBXSLT_VERSION = 1.1.24
+LIBXSLT_VERSION = 1.1.33
 LIBXSLT_SOURCE = libxslt-$(LIBXSLT_VERSION).tar.gz
 LIBXSLT_SITE = ftp://xmlsoft.org/libxslt
+
+LIBXSLT_LIBTOOL_PATCH = NO
 LIBXSLT_INSTALL_STAGING = YES
 LIBXSLT_INSTALL_TARGET = YES
+LIBXSLT_DEPENDENCIES = libxml2
 
 # If we have enabled libgcrypt then use it, else disable crypto support.
 ifeq ($(BR2_PACKAGE_LIBGCRYPT),y)
-LIBXSLT_DEPENDENCIES_EXTRA=libgcrypt
+LIBXSLT_DEPENDENCIES += libgcrypt
 else
 LIBXSLT_XTRA_CONF_OPT = --without-crypto
 endif
@@ -21,13 +26,13 @@ LIBXSLT_CONF_OPT = --with-gnu-ld --enable-shared \
 		$(DISABLE_NLS) $(DISABLE_IPV6) \
 		--without-debugging --without-python \
 		--without-threads \
-		--with-libxml-prefix=$(STAGING_DIR)/usr/
 
-LIBXSLT_DEPENDENCIES = uclibc $(LIBXSLT_DEPENDENCIES_EXTRA)
-
+LIBXSLT_CONF_ENV = XSLTPROC=/usr/bin/xsltproc LIBXML_CFLAGS=-I$(STAGING_DIR)/usr/include/libxml2
+		
 $(eval $(call AUTOTARGETS,package,libxslt))
 
 $(LIBXSLT_HOOK_POST_INSTALL):
+	rm -f $(TARGET_DIR)/usr/bin/xslt-config $(TARGET_DIR)/usr/lib/xsltConf.sh
 	$(SED) "s,^prefix=.*,prefix=\'$(STAGING_DIR)/usr\',g" $(STAGING_DIR)/usr/bin/xslt-config
 	$(SED) "s,^exec_prefix=.*,exec_prefix=\'$(STAGING_DIR)/usr\',g" $(STAGING_DIR)/usr/bin/xslt-config
 	$(SED) "s,^includedir=.*,includedir=\'$(STAGING_DIR)/usr/include\',g" $(STAGING_DIR)/usr/bin/xslt-config
