d1b705654ea89c2f562cbef3f93256af24f7450c
João Cardoso <jcard@gmail.com>
2020-03-31 17:55:11 +0000

    lzo: bump to 2.10
    git-svn-id: https://svn.code.sf.net/p/alt-f/code/trunk/alt-f@3879 cd80dc97-fa67-4d1f-9ead-3176a861659d

--- package/lzo/lzo.mk
+++ package/lzo/lzo.mk
@@ -2,8 +2,9 @@
 #
 # lzo
 #
-#############################################################
-LZO_VERSION:=2.06
+############################################################
+
+LZO_VERSION:=2.10
 LZO_SOURCE:=lzo-$(LZO_VERSION).tar.gz
 LZO_SITE:=http://www.oberhumer.com/opensource/lzo/download
 LZO_AUTORECONF = NO
@@ -13,48 +14,8 @@ LZO_LIBTOOL_PATCH = NO
 LZO_INSTALL_STAGING_OPT = CC="$(TARGET_CC)" DESTDIR=$(STAGING_DIR) install
 LZO_CONF_ENV = 
 LZO_CONF_OPT = --disable-static --enable-shared
-LZO_DEPENDENCIES = uclibc
+LZO_DEPENDENCIES = uclibc attr-host
+LZO_HOST_DEPENDENCIES = attr-host
 
 $(eval $(call AUTOTARGETS,package,lzo))
-
-# lzo for the host
-LZO_HOST_DIR:=$(BUILD_DIR)/lzo-$(LZO_VERSION)-host
-
-$(DL_DIR)/$(LZO_SOURCE):
-	$(call DOWNLOAD,$(LZO_SITE),$(LZO_SOURCE))
-
-$(STAMP_DIR)/host_lzo_unpacked: $(DL_DIR)/$(LZO_SOURCE)
-	mkdir -p $(LZO_HOST_DIR)
-	$(INFLATE$(suffix $(LZO_SOURCE))) $< | \
-		$(TAR) $(TAR_STRIP_COMPONENTS)=1 -C $(LZO_HOST_DIR) $(TAR_OPTIONS) -
-	toolchain/patch-kernel.sh $(LZO_HOST_DIR) package/lzo/ \*.patch
-	touch $@
-
-$(STAMP_DIR)/host_lzo_configured: $(STAMP_DIR)/host_lzo_unpacked
-	(cd $(LZO_HOST_DIR); rm -rf config.cache; \
-		$(HOST_CONFIGURE_OPTS) \
-		CFLAGS="$(HOST_CFLAGS)" \
-		LDFLAGS="$(HOST_LDFLAGS)" \
-		./configure \
-		--prefix="$(HOST_DIR)/usr" \
-		--sysconfdir="$(HOST_DIR)/etc" \
-	)
-	touch $@
-
-$(STAMP_DIR)/host_lzo_compiled: $(STAMP_DIR)/host_lzo_configured
-	$(MAKE) -C $(LZO_HOST_DIR)
-	touch $@
-
-$(STAMP_DIR)/host_lzo_installed: $(STAMP_DIR)/host_lzo_compiled
-	$(MAKE) -C $(LZO_HOST_DIR) install
-	touch $@
-
-host-lzo: $(STAMP_DIR)/host_lzo_installed
-
-host-lzo-clean:
-	rm -f $(addprefix $(STAMP_DIR)/host_lzo_,unpacked configured compiled installed)
-	-$(MAKE) -C $(LZO_HOST_DIR) uninstall
-	-$(MAKE) -C $(LZO_HOST_DIR) clean
-
-host-lzo-dirclean:
-	rm -rf $(LZO_HOST_DIR)
+$(eval $(call AUTOTARGETS_HOST,package,lzo))
