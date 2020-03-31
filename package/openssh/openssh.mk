855afe56353176dfc7d2ba55ce13906743945aac
João Cardoso <jcard@gmail.com>
2020-03-31 16:42:31 +0000

    openssh: bump to 8.1p1. Use update-alternatives so ssh and scp do not conflict with dropbear binaries
    git-svn-id: https://svn.code.sf.net/p/alt-f/code/trunk/alt-f@3869 cd80dc97-fa67-4d1f-9ead-3176a861659d

--- package/openssh/openssh.mk
+++ package/openssh/openssh.mk
@@ -4,36 +4,37 @@
 #
 #############################################################
 
-#OPENSSH_VERSION=6.9p1
-OPENSSH_VERSION=7.1p2
+#OPENSSH_VERSION=7.1p2
+OPENSSH_VERSION=8.1p1
 
-OPENSSH_SITE=ftp://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
+OPENSSH_SITE=https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
 
 OPENSSH_CONF_ENV = LD=$(TARGET_CC)
 OPENSSH_CONF_OPT = --sysconfdir=/etc/ssh --with-privsep-path=/var/run/vsftpd \
-	-disable-lastlog --disable-utmp --disable-utmpx --disable-wtmp --disable-wtmpx
+	--disable-lastlog --disable-utmp --disable-utmpx --disable-wtmp --disable-wtmpx \
+	--without-ssl-engine --without-pam --without-selinux --with-md5-passwords
 
 OPENSSH_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
 OPENSSH_SFTP_STAMP = $(PROJECT_BUILD_DIR)/autotools-stamps/openssh-sftp_target_installed
 
-# The bellow dependency on dropbear is a fake. Dropbear installs ssh and scp as a link
-# to dropbear, so if it is installed after openssh it will override those binaries
-
-OPENSSH_DEPENDENCIES = zlib openssl dropbear
+OPENSSH_DEPENDENCIES = zlib openssl
 
 $(eval $(call AUTOTARGETS,package,openssh))
 
-# this is a hack. Use AUTOTARGETS to do everything but install.
+# is there other way? Use AUTOTARGETS to do everything but install.
 # At the build end, the post build hook runs, which installs just sftp-server
 
 ifneq ($(BR2_PACKAGE_OPENSSH_SFTP),y)
 
 $(OPENSSH_HOOK_POST_INSTALL):
-	sed -i 's/#.*Ciphers /Ciphers aes128-cbc,/' $(TARGET_DIR)/etc/ssh/ssh_config
+	#sed -i 's/#.*Ciphers /Ciphers aes128-cbc,/' $(TARGET_DIR)/etc/ssh/ssh_config
 	sed -i -e '/Subsystem/s/libexec/lib/' \
-		-e 's/#PermitRootLogin no/PermitRootLogin yes/' \
+		-e 's/#PermitRootLogin.*$$/PermitRootLogin yes/' \
+		-e 's/#PasswordAuthentication/PasswordAuthentication/' \
 		$(TARGET_DIR)/etc/ssh/sshd_config
+	mv $(TARGET_DIR)/usr/bin/ssh $(TARGET_DIR)/usr/bin/ssh-openssh
+	mv $(TARGET_DIR)/usr/bin/scp $(TARGET_DIR)/usr/bin/scp-openssh
 	touch $@
 
 else
