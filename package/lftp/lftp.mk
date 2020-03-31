6fd78b0e989856049a8f8ebdcd0ef4929df7dfa2
João Cardoso <jcard@gmail.com>
2020-03-31 15:30:44 +0000

    lftp: bump to 4.9.1
    git-svn-id: https://svn.code.sf.net/p/alt-f/code/trunk/alt-f@3853 cd80dc97-fa67-4d1f-9ead-3176a861659d

--- package/lftp/lftp.mk
+++ package/lftp/lftp.mk
@@ -4,13 +4,21 @@
 #
 #############################################################
 
-LFTP_VERSION:=4.6.1
-LFTP_SOURCE:=lftp-$(LFTP_VERSION).tar.gz
+LFTP_VERSION:=4.9.1
+LFTP_SOURCE:=lftp-$(LFTP_VERSION).tar.xz
 LFTP_SITE:=http://lftp.yar.ru/ftp
 
 LFTP_LIBTOOL_PATCH = NO
 LFTP_DEPENDENCIES = readline expat libiconv gettext openssl
 
-LFTP_CONF_OPT = --without-gnutls --with-openssl
+LFTP_CONF_OPT = --without-gnutls --with-openssl \
+	--with-readline=$(STAGING_DIR)/usr \
+	--with-zlib=$(STAGING_DIR)/usr \
+	--with-expat=$(STAGING_DIR)/usr
 
 $(eval $(call AUTOTARGETS,package,lftp))
+
+$(LFTP_HOOK_POST_INSTALL):
+	rm -f $(TARGET_DIR)/usr/share/applications/lftp.desktop \
+		$(TARGET_DIR)/usr/share/icons/hicolor/48x48/apps/lftp-icon.png
+	#rmdir $(TARGET_DIR)/usr/share/applications
