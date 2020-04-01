2570cea715bba16106b81b056a9475049cefe7ce
João Cardoso <jcard@gmail.com>
2020-04-01 14:46:19 +0000

    popt: minor build adjustments
    git-svn-id: https://svn.code.sf.net/p/alt-f/code/trunk/alt-f@3886 cd80dc97-fa67-4d1f-9ead-3176a861659d

--- package/popt/popt.mk
+++ package/popt/popt.mk
@@ -5,18 +5,21 @@
 #############################################################
 
 POPT_VERSION:=1.16
-POPT_SITE:=http://rpm5.org/files/popt
+#POPT_SITE:=http://rpm5.org/files/popt
+POPT_SITE:=https://ftp.openbsd.org/pub/OpenBSD/distfiles
 POPT_INSTALL_STAGING = YES
 POPT_INSTALL_TARGET = YES
 POPT_LIBTOOL_PATCH = NO
 POPT_CONF_OPT = --libdir=/usr/lib
 POPT_CONF_ENV = ac_cv_va_copy=yes
 
+POPT_DEPENDENCIES:=uclibc
+
 ifeq ($(BR2_PACKAGE_LIBICONV),y)
-POPT_CONF_ENV += am_cv_lib_iconv=yes
+POPT_CONF_ENV += am_cv_lib_iconv=yes CFLAGS="$(TARGET_CFLAGS) $(BR2_PACKAGE_POPT_OPTIM)"
 POPT_CONF_OPT += --with-libiconv-prefix=$(STAGING_DIR)/usr
+POPT_DEPENDENCIES+=libiconv
 endif
 
-POPT_DEPENDENCIES:=uclibc libiconv
 
 $(eval $(call AUTOTARGETS,package,popt))
