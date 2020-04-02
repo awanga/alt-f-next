fe41a9e833e181cd798fef4c8042f58576a479ee
João Cardoso <jcard@gmail.com>
2020-04-02 18:31:28 +0000

    acl: add host-build
    git-svn-id: https://svn.code.sf.net/p/alt-f/code/trunk/alt-f@3891 cd80dc97-fa67-4d1f-9ead-3176a861659d

--- package/acl/acl.mk
+++ package/acl/acl.mk
@@ -10,11 +10,14 @@ ACL_SITE:=http://download.savannah.gnu.org/releases/acl
 ACL_INSTALL_STAGING = YES
 ACL_LIBTOOL_PATCH = NO
 ACL_DEPENDENCIES = uclibc attr libintl
+ACL_DEPENDENCIES_HOST = attr-host
 ACL_CONF_OPT = --disable-gettext
 ACL_CONF_ENV = LDFLAGS=-lintl
 ACL_INSTALL_STAGING=YES
 
 ACL_INSTALL_STAGING_OPT = DESTDIR="$(STAGING_DIR)" install install-dev install-lib
+ACL_HOST_INSTALL_OPT = DESTDIR="$(HOST_DIR)" install install-dev install-lib
 ACL_INSTALL_TARGET_OPT = DESTDIR="$(TARGET_DIR)" install install-lib
 
 $(eval $(call AUTOTARGETS,package,acl))
+$(eval $(call AUTOTARGETS_HOST,package,acl))
