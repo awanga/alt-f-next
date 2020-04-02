401335b37ddd65267be47fd4cebcde5a4c12215e
João Cardoso <jcard@gmail.com>
2020-04-02 18:44:58 +0000

    attr: add host-build
    git-svn-id: https://svn.code.sf.net/p/alt-f/code/trunk/alt-f@3896 cd80dc97-fa67-4d1f-9ead-3176a861659d

--- package/attr/attr.mk
+++ package/attr/attr.mk
@@ -11,6 +11,9 @@ ATTR_INSTALL_STAGING=YES
 
 ATTR_INSTALL_STAGING_OPT = DESTDIR="$(STAGING_DIR)" install install-dev install-lib
 ATTR_INSTALL_TARGET_OPT = DESTDIR="$(TARGET_DIR)" install install-lib
+ATTR_HOST_INSTALL_OPT = DESTDIR="$(HOST_DIR)" install install-dev install-lib
+
 ATTR_CONF_OPT = --disable-gettext
 
 $(eval $(call AUTOTARGETS,package,attr))
+$(eval $(call AUTOTARGETS_HOST,package,attr))
