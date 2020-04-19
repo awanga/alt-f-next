db79d119e5836ba81313cf82fbbb7fb76f1c0c8e
João Cardoso <jcard@gmail.com>
2020-04-19 18:00:34 +0000

    expat: bump to 2.2.5
    git-svn-id: https://svn.code.sf.net/p/alt-f/code/trunk/alt-f@3905 cd80dc97-fa67-4d1f-9ead-3176a861659d

--- package/expat/expat.mk
+++ package/expat/expat.mk
@@ -2,19 +2,15 @@
 #
 # expat
 #
-#############################################################
+############################################################
 
-EXPAT_VERSION = 2.0.1
-EXPAT_SOURCE = expat-$(EXPAT_VERSION).tar.gz
+EXPAT_VERSION = 2.2.5
+EXPAT_SOURCE = expat-$(EXPAT_VERSION).tar.bz2
 EXPAT_SITE = $(BR2_SOURCEFORGE_MIRROR)/project/expat/expat/$(EXPAT_VERSION)
 
 EXPAT_LIBTOOL_PATCH = NO
 EXPAT_INSTALL_STAGING = YES
 EXPAT_INSTALL_TARGET = YES
-
-EXPAT_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) installlib
-EXPAT_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) installlib
-EXPAT_HOST_INSTALL_OPT = DESTDIR=$(HOST_DIR) installlib
 
 EXPAT_CONF_OPT = --enable-shared
 EXPAT_DEPENDENCIES = uclibc host-pkgconfig
