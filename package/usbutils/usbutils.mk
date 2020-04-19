ad53eaf8d2ef7e860c6fc7127e500da7a2bf95f5
João Cardoso <jcard@gmail.com>
2020-04-19 17:53:41 +0000

    usbutils: update usb.ids; use compressed usb.ids; add S31usbid initcript to cron-update usb.ids; add proper update_usbid.sh
    git-svn-id: https://svn.code.sf.net/p/alt-f/code/trunk/alt-f@3903 cd80dc97-fa67-4d1f-9ead-3176a861659d

--- package/usbutils/usbutils.mk
+++ package/usbutils/usbutils.mk
@@ -4,64 +4,19 @@
 #
 #############################################################
 
-USBUTILS_VERSION:=0.87
+USBUTILS_VERSION:=0.87-1
 USBUTILS_SOURCE:=usbutils_$(USBUTILS_VERSION).orig.tar.gz
 USBUTILS_SITE:=http://archive.debian.org/debian/pool/main/u/usbutils
 
-USBUTILS_DIR:=$(BUILD_DIR)/usbutils-$(USBUTILS_VERSION)
-USBUTILS_CAT:=$(ZCAT)
-USBUTILS_BINARY:=lsusb
-USBUTILS_TARGET_BINARY:=usr/sbin/lsusb
+USBUTILS_LIBTOOL_PATCH = NO
+USBUTILS_DEPENDENCIES = libusb
+USBID_SITE="http://www.linux-usb.org/usb.ids.gz"
 
-$(DL_DIR)/$(USBUTILS_SOURCE):
-	$(call DOWNLOAD,$(USBUTILS_SITE),$(USBUTILS_SOURCE))
+$(eval $(call AUTOTARGETS,package,usbutils))
 
-usbutils-source: $(DL_DIR)/$(USBUTILS_SOURCE)
-
-usbutils-unpacked: $(USBUTILS_DIR)/.unpacked
-$(USBUTILS_DIR)/.unpacked: $(DL_DIR)/$(USBUTILS_SOURCE)
-	$(USBUTILS_CAT) $(DL_DIR)/$(USBUTILS_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	touch $(USBUTILS_DIR)/.unpacked
-
-$(USBUTILS_DIR)/.configured: $(USBUTILS_DIR)/.unpacked
-	(cd $(USBUTILS_DIR); rm -rf config.cache; \
-		$(TARGET_CONFIGURE_OPTS) \
-		$(TARGET_CONFIGURE_ARGS) \
-		ac_cv_func_malloc_0_nonnull=yes \
-		./configure \
-		--target=$(GNU_TARGET_NAME) \
-		--host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) \
-		--prefix=/usr \
-	)
-	touch $(USBUTILS_DIR)/.configured
-
-$(USBUTILS_DIR)/$(USBUTILS_BINARY): $(USBUTILS_DIR)/.configured
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(USBUTILS_DIR)
-
-$(TARGET_DIR)/$(USBUTILS_TARGET_BINARY): $(USBUTILS_DIR)/$(USBUTILS_BINARY)
-	$(USBUTILS_DIR)/update-usbids.sh
-	$(MAKE) -C $(USBUTILS_DIR) DESTDIR=$(TARGET_DIR) install
-	rm -rf $(TARGET_DIR)/usr/share/pkgconfig/usbutils.pc \
-		$(TARGET_DIR)/usr/man $(TARGET_DIR)/usr/share/usb.ids.gz \
-		$(TARGET_DIR)/usr/bin/usb-devices
-	rmdir $(TARGET_DIR)/usr/share/pkgconfig
-
-usbutils: uclibc libusb $(TARGET_DIR)/$(USBUTILS_TARGET_BINARY)
-
-usbutils-clean:
-	rm -f $(TARGET_DIR)/$(USBUTILS_TARGET_BINARY)
-	rm -f $(TARGET_DIR)/usr/share/usb.ids
-	rmdir --ignore-fail-on-non-empty $(TARGET_DIR)/usr/share
-	-$(MAKE) -C $(USBUTILS_DIR) clean
-
-usbutils-dirclean:
-	rm -rf $(USBUTILS_DIR)
-#############################################################
-#
-# Toplevel Makefile options
-#
-#############################################################
-ifeq ($(BR2_PACKAGE_USBUTILS),y)
-TARGETS+=usbutils
-endif
+$(USBUTILS_HOOK_POST_INSTALL):
+	wget -N -P $(TARGET_DIR)/usr/share $(USBID_SITE)
+	rm -rf $(TARGET_DIR)/usr/share/pkgconfig \
+		$(TARGET_DIR)/usr/bin/usb-devices \
+		$(TARGET_DIR)/usr/sbin/update-usbids.sh
+	touch $@
