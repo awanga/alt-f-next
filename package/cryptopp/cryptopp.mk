22bc5505271ec0c9bcb2f813dc1ffa18eb13b056
João Cardoso <jcard@gmail.com>
2020-05-12 17:18:12 +0000

    cryptopp: bump to 5.6.5
    git-svn-id: https://svn.code.sf.net/p/alt-f/code/trunk/alt-f@3908 cd80dc97-fa67-4d1f-9ead-3176a861659d

--- package/cryptopp/cryptopp.mk
+++ package/cryptopp/cryptopp.mk
@@ -4,20 +4,21 @@
 #
 #############################################################
 
-CRYPTOPP_VERSION:=562
-CRYPTOPP_VERSION2:=5.6.2
-CRYPTOPP_SOURCE:=cryptopp$(CRYPTOPP_VERSION).zip
-CRYPTOPP_SITE:=$(BR2_SOURCEFORGE_MIRROR)/project/cryptopp/cryptopp/$(CRYPTOPP_VERSION2)
+CRYPTOPP_VERSION:=5.6.5
+CRYPTOPP_VERSION2:=5_6_5
+
+CRYPTOPP_SOURCE:=CRYPTOPP_$(CRYPTOPP_VERSION2).tar.gz
+CRYPTOPP_SITE:=https://github.com/weidai11/cryptopp/archive
 
 CRYPTOPP_LIBTOOL_PATCH = NO
 CRYPTOPP_INSTALL_STAGING = YES
 
-CRYPTOPP_MAKE_OPT = dynamic
+CRYPTOPP_MAKE_OPT = dynamic # cryptest.exe
 
 CRYPTOPP_MAKE_ENV = \
 	CC="$(TARGET_CC)" \
 	CXX="$(TARGET_CXX)" \
-	CXXFLAGS="$(TARGET_CXXFLAGS) -fPIC" \
+	CXXFLAGS="$(TARGET_CXXFLAGS) -DNDEBUG -fPIC" \
 	AR=$(TARGET_AR) \
 	LD=$(TARGET_LD) \
 	STRIP=$(TARGET_STRIP)
@@ -28,12 +29,8 @@ CRYPTOPP_INSTALL_STAGING_OPT = PREFIX=$(STAGING_DIR)/usr install
 
 $(eval $(call AUTOTARGETS,package,cryptopp))
 
-$(CRYPTOPP_TARGET_EXTRACT):
-	if ! which unzip; then /bin/echo -e "\n\nYou must install 'unzip' on your build machine\n"; fi
-	unzip -a $(DL_DIR)/$(CRYPTOPP_SOURCE) -d $(CRYPTOPP_DIR)
-	sed -i 's/\(.*march=native\)/#\1/' $(CRYPTOPP_DIR)/GNUmakefile
-	touch $@
-
 $(CRYPTOPP_TARGET_CONFIGURE):
+	$(SED) 's/\(.*march=native\)/#\1/' \
+	-e 's|./libcryptopp.a|-L. -lcryptopp|' $(CRYPTOPP_DIR)/GNUmakefile
 	touch $@
 
