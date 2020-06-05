ff8e6f37f56b0616b1e6096ad11bceb25d46a52b
Alfred Wanga <awanga@gmail.com>
2020-06-05 19:31:36 +0000

    alt-f-utils: update package to properly build for host

--- package/alt-f-utils/alt-f-utils.mk
+++ package/alt-f-utils/alt-f-utils.mk
@@ -15,7 +15,7 @@ ALT_F_UTILS_INSTALL_STAGING := NO
 ALT_F_UTILS_INSTALL_TARGET := YES
 
 ALT_F_UTILS_MAKE_ENV := CC="$(TARGET_CC) $(TARGET_CFLAGS) -D_GNU_SOURCE" STRIP="$(TARGET_STRIP)"
-ALT_F_UTILS_HOST_MAKE_ENV := STRIP=true
+HOST_ALT_F_UTILS_MAKE_ENV := STRIP=true DESTDIR="$(HOST_DIR)"
 
 $(DL_DIR)/$(ALT_F_UTILS_SOURCE): package/alt-f-utils/alt-f-utils-$(ALT_F_UTILS_VERSION)/sysctrl.c \
 		package/alt-f-utils/alt-f-utils-$(ALT_F_UTILS_VERSION)/dns323-fw.c \
@@ -29,10 +29,9 @@ $(DL_DIR)/$(ALT_F_UTILS_SOURCE): package/alt-f-utils/alt-f-utils-$(ALT_F_UTILS_V
 	touch $@
 
 $(eval $(autotools-package))
-
 $(eval $(host-autotools-package))
 
-$(ALT_F_UTILS_HOST_HOOK_POST_INSTALL):
+$(HOST_ALT_F_UTILS_POST_INSTALL_HOOKS):
 	mkdir -p $(HOST_DIR)/usr/bin
 	( cd $(HOST_DIR)/usr/sbin/; \
 		rm rpcinfo sysctrl dns320l-daemon; \
@@ -40,7 +39,7 @@ $(ALT_F_UTILS_HOST_HOOK_POST_INSTALL):
 	)
 	touch $@
 
-$(ALT_F_UTILS_TARGET_CONFIGURE) $(ALT_F_UTILS_HOST_CONFIGURE):
+$(ALT_F_UTILS_TARGET_CONFIGURE) $(HOST_ALT_F_UTILS_TARGET_CONFIGURE):
 	touch $@
 
-$(ALT_F_UTILS_TARGET_SOURCE) $(ALT_F_UTILS_HOST_SOURCE): $(DL_DIR)/$(ALT_F_UTILS_SOURCE)
+$(ALT_F_UTILS_TARGET_SOURCE) $(HOST_ALT_F_UTILS_TARGET_SOURCE): $(DL_DIR)/$(ALT_F_UTILS_SOURCE)
